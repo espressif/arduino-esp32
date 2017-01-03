23202683db913494f34d9cf28367d262a7e2cfa8
Joren Six <joren.six@ugent.be>
2017-01-03 14:10:53 +0100

    Added a basic WiFi UDP client example: a sketch that sends random bytes over udp.

---
+++ libraries/WiFi/examples/WiFiUDPClient/WiFiUDPClient.ino
@@ -0,0 +1,77 @@
+/*
+ *  This sketch sends random data over UDP on a ESP32 device
+ *
+ */
+#include <WiFi.h>
+#include <WiFiUdp.h>
+
+// WiFi network name and password:
+const char * networkName = "network_name";
+const char * networkPswd = "password";
+
+//IP address to send UDP data to:
+const char * udpAddress = "192.168.1.255";
+const int udpPort = 3333;
+
+//Are we currently connected?
+boolean connected = false;
+
+//The udp library class
+WiFiUDP udp;
+
+void setup(){
+  // Initilize hardware serial:
+  Serial.begin(115200);
+  
+  //Connect to the WiFi network
+  connectToWiFi(networkName, networkPswd);
+}
+
+void loop(){
+  //only send data when connected
+  if(connected){
+
+    //send one random byte
+    byte randomData = (byte) random(255);
+
+    //Send a packet
+    udp.beginPacket(udpAddress,udpPort);
+    udp.write(randomData);
+    udp.endPacket();
+  }
+  //Wait for 1.5 seconds
+  delay(1500);
+}
+
+void connectToWiFi(const char * ssid, const char * pwd){
+  Serial.println("Connecting to WiFi network: " + String(ssid));
+
+  // delete old config
+  WiFi.disconnect(true);
+  //register event handler
+  WiFi.onEvent(WiFiEvent);
+  
+  //Initiate connection
+  WiFi.begin(ssid, pwd);
+
+  Serial.println("Waiting for WIFI connection...");
+}
+
+//wifi event handler
+void WiFiEvent(WiFiEvent_t event){
+    switch(event) {
+      case SYSTEM_EVENT_STA_GOT_IP:
+          //When connected set 
+          Serial.print("WiFi connected! IP address: ");
+          Serial.println(WiFi.localIP());  
+          //initializes the UDP state
+          //This initializes the transfer buffer
+          port.begin(WiFi.localIP(),udpPort);
+          connected = true;
+          break;
+      case SYSTEM_EVENT_STA_DISCONNECTED:
+          Serial.println("WiFi lost connection");
+          connected = false;
+          break;
+    }
+}
