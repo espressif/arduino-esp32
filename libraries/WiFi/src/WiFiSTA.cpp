98414817cd8573a0a9db7ed8b56449170c85c637
seopyoon <heeseop.y@gmail.com>
2017-01-18 21:47:43 +0900

    added _smartConfigDone = ture

--- libraries/WiFi/src/WiFiSTA.cpp
+++ libraries/WiFi/src/WiFiSTA.cpp
@@ -554,6 +554,8 @@ void WiFiSTAClass::_smartConfigCallback(uint32_t st, void* result) {
         esp_wifi_set_config(WIFI_IF_AP, (wifi_config_t *)sta_conf);
         esp_wifi_disconnect();
         esp_wifi_connect();
+
+        _smartConfigDone = true;
     } else if (status == SC_STATUS_LINK_OVER) {
         WiFi.stopSmartConfig();
     }
