8ff7004f3e25a28b6a2f066f316ffbe794a1b9a3
me-no-dev <hristo@espressif.com>
2017-02-06 01:56:12 +0200

    Fix wrong definition of SS pin

--- variants/esp32thing/pins_arduino.h
+++ variants/esp32thing/pins_arduino.h
@@ -2,14 +2,12 @@
 #define Pins_Arduino_h
 
 #define EXTERNAL_NUM_INTERRUPTS 16
-#define NUM_DIGITAL_PINS        38
+#define NUM_DIGITAL_PINS        40
 #define NUM_ANALOG_INPUTS       16
 
 #define analogInputToDigitalPin(p)  (((p)<20)?(esp32_adc2gpio[(p)]):-1)
 #define digitalPinToInterrupt(p)    (((p)<40)?(p):-1)
 #define digitalPinHasPWM(p)         (p < 34)
-
-#define LED_BUILTIN 16
 
 static const uint8_t TX = 1;
 static const uint8_t RX = 3;
@@ -17,7 +15,7 @@ static const uint8_t RX = 3;
 static const uint8_t SDA = 21;
 static const uint8_t SCL = 22;
 
-static const uint8_t SS    = 5;
+static const uint8_t SS    = 2;
 static const uint8_t MOSI  = 23;
 static const uint8_t MISO  = 19;
 static const uint8_t SCK   = 18;
