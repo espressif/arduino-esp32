e8192082f3f2b002edd25c40c24eab48123ff706
Ion <ionciubotaru@users.noreply.github.com>
2017-02-06 16:17:49 +0200

    Update TouchRead.ino

--- libraries/ESP32/examples/Touch/TouchRead/TouchRead.ino
+++ libraries/ESP32/examples/Touch/TouchRead/TouchRead.ino
@@ -5,7 +5,7 @@ void setup()
 {
   Serial.begin(115200);
   delay(1000); // give me time to bring up serial monitor
-  Serial.println(("ESP32 Touch Test");
+  Serial.println("ESP32 Touch Test");
 }
 
 void loop()
