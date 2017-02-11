f9ba8e9ccfd80763a4be90403bf711eff1329ef4
Martin Sloup <arcao@arcao.com>
2017-02-11 23:08:22 +0100

    Refactoring EPS32NVS library to Preferences library (#199)
    * Refactoring EPS32NVS library to Preferences library
    
    * Renaming all set* functions to put
    * Added functions for float, double and bool
    
    * Small redesign of Preferences API
    
    * added type to put* function names
    * for (u)int64_t used functions name with *(U)Long64
    * added functions for long and unsigned long (same like int a unsigned int)

--- libraries/Preferences/src/Preferences.cpp
+++ libraries/Preferences/src/Preferences.cpp
@@ -11,24 +11,24 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "ESP32NVS.h"
+#include "Preferences.h"
 
 #include "nvs.h"
 
 const char * nvs_errors[] = { "OTHER", "NOT_INITIALIZED", "NOT_FOUND", "TYPE_MISMATCH", "READ_ONLY", "NOT_ENOUGH_SPACE", "INVALID_NAME", "INVALID_HANDLE", "REMOVE_FAILED", "KEY_TOO_LONG", "PAGE_FULL", "INVALID_STATE", "INVALID_LENGHT"};
 #define nvs_error(e) (((e)>ESP_ERR_NVS_BASE)?nvs_errors[(e)&~(ESP_ERR_NVS_BASE)]:nvs_errors[0])
 
-NVSClass::NVSClass()
+Preferences::Preferences()
     :_handle(0)
     ,_started(false)
     ,_readOnly(false)
 {}
 
-NVSClass::~NVSClass(){
+Preferences::~Preferences(){
     end();
 }
 
-bool NVSClass::begin(const char * name, bool readOnly){
+bool Preferences::begin(const char * name, bool readOnly){
     if(_started){
         return false;
     }
@@ -42,7 +42,7 @@ bool NVSClass::begin(const char * name, bool readOnly){
     return true;
 }
 
-void NVSClass::end(){
+void Preferences::end(){
     if(!_started){
         return;
     }
@@ -51,10 +51,10 @@ void NVSClass::end(){
 }
 
 /*
- * Clear the storage
+ * Clear all keys in opened preferences
  * */
 
-bool NVSClass::clear(){
+bool Preferences::clear(){
     if(!_started || _readOnly){
         return false;
     }
@@ -67,10 +67,10 @@ bool NVSClass::clear(){
 }
 
 /*
- * Remove the key
+ * Remove a key
  * */
 
-bool NVSClass::remove(const char * key){
+bool Preferences::remove(const char * key){
     if(!_started || !key || _readOnly){
         return false;
     }
@@ -83,10 +83,10 @@ bool NVSClass::remove(const char * key){
 }
 
 /*
- * Write
+ * Put a key value
  * */
 
-size_t NVSClass::setChar(const char* key, int8_t value){
+size_t Preferences::putChar(const char* key, int8_t value){
     if(!_started || !key || _readOnly){
         return 0;
     }
@@ -103,7 +103,7 @@ size_t NVSClass::setChar(const char* key, int8_t value){
     return 1;
 }
 
-size_t NVSClass::setUChar(const char* key, uint8_t value){
+size_t Preferences::putUChar(const char* key, uint8_t value){
     if(!_started || !key || _readOnly){
         return 0;
     }
@@ -120,7 +120,7 @@ size_t NVSClass::setUChar(const char* key, uint8_t value){
     return 1;
 }
 
-size_t NVSClass::setShort(const char* key, int16_t value){
+size_t Preferences::putShort(const char* key, int16_t value){
     if(!_started || !key || _readOnly){
         return 0;
     }
@@ -137,7 +137,7 @@ size_t NVSClass::setShort(const char* key, int16_t value){
     return 2;
 }
 
-size_t NVSClass::setUShort(const char* key, uint16_t value){
+size_t Preferences::putUShort(const char* key, uint16_t value){
     if(!_started || !key || _readOnly){
         return 0;
     }
@@ -154,7 +154,7 @@ size_t NVSClass::setUShort(const char* key, uint16_t value){
     return 2;
 }
 
-size_t NVSClass::setInt(const char* key, int32_t value){
+size_t Preferences::putInt(const char* key, int32_t value){
     if(!_started || !key || _readOnly){
         return 0;
     }
@@ -171,7 +171,7 @@ size_t NVSClass::setInt(const char* key, int32_t value){
     return 4;
 }
 
-size_t NVSClass::setUInt(const char* key, uint32_t value){
+size_t Preferences::putUInt(const char* key, uint32_t value){
     if(!_started || !key || _readOnly){
         return 0;
     }
@@ -188,7 +188,15 @@ size_t NVSClass::setUInt(const char* key, uint32_t value){
     return 4;
 }
 
-size_t NVSClass::setLong(const char* key, int64_t value){
+size_t Preferences::putLong(const char* key, int32_t value){
+    return putInt(key, value);
+}
+
+size_t Preferences::putULong(const char* key, uint32_t value){
+    return putUInt(key, value);
+}
+
+size_t Preferences::putLong64(const char* key, int64_t value){
     if(!_started || !key || _readOnly){
         return 0;
     }
@@ -205,7 +213,7 @@ size_t NVSClass::setLong(const char* key, int64_t value){
     return 8;
 }
 
-size_t NVSClass::setULong(const char* key, uint64_t value){
+size_t Preferences::putULong64(const char* key, uint64_t value){
     if(!_started || !key || _readOnly){
         return 0;
     }
@@ -222,7 +230,19 @@ size_t NVSClass::setULong(const char* key, uint64_t value){
     return 8;
 }
 
-size_t NVSClass::setString(const char* key, const char* value){
+size_t Preferences::putFloat(const char* key, const float_t value){
+    return putBytes(key, (void*)&value, sizeof(float_t));
+}
+
+size_t Preferences::putDouble(const char* key, const double_t value){
+    return putBytes(key, (void*)&value, sizeof(double_t));
+}
+
+size_t Preferences::putBool(const char* key, const bool value){
+    return putUChar(key, (uint8_t) (value ? 1 : 0));
+}
+
+size_t Preferences::putString(const char* key, const char* value){
     if(!_started || !key || !value || _readOnly){
         return 0;
     }
@@ -239,11 +259,11 @@ size_t NVSClass::setString(const char* key, const char* value){
     return strlen(value);
 }
 
-size_t NVSClass::setString(const char* key, const String value){
-    return setString(key, value.c_str());
+size_t Preferences::putString(const char* key, const String value){
+    return putString(key, value.c_str());
 }
 
-size_t NVSClass::setBytes(const char* key, const void* value, size_t len){
+size_t Preferences::putBytes(const char* key, const void* value, size_t len){
     if(!_started || !key || !value || !len || _readOnly){
         return 0;
     }
@@ -261,10 +281,10 @@ size_t NVSClass::setBytes(const char* key, const void* value, size_t len){
 }
 
 /*
- * Read
+ * Get a key value
  * */
 
-int8_t NVSClass::getChar(const char* key, const int8_t defaultValue){
+int8_t Preferences::getChar(const char* key, const int8_t defaultValue){
     int8_t value = defaultValue;
     if(!_started || !key){
         return value;
@@ -276,7 +296,7 @@ int8_t NVSClass::getChar(const char* key, const int8_t defaultValue){
     return value;
 }
 
-uint8_t NVSClass::getUChar(const char* key, const uint8_t defaultValue){
+uint8_t Preferences::getUChar(const char* key, const uint8_t defaultValue){
     uint8_t value = defaultValue;
     if(!_started || !key){
         return value;
@@ -288,7 +308,7 @@ uint8_t NVSClass::getUChar(const char* key, const uint8_t defaultValue){
     return value;
 }
 
-int16_t NVSClass::getShort(const char* key, const int16_t defaultValue){
+int16_t Preferences::getShort(const char* key, const int16_t defaultValue){
     int16_t value = defaultValue;
     if(!_started || !key){
         return value;
@@ -300,7 +320,7 @@ int16_t NVSClass::getShort(const char* key, const int16_t defaultValue){
     return value;
 }
 
-uint16_t NVSClass::getUShort(const char* key, const uint16_t defaultValue){
+uint16_t Preferences::getUShort(const char* key, const uint16_t defaultValue){
     uint16_t value = defaultValue;
     if(!_started || !key){
         return value;
@@ -312,7 +332,7 @@ uint16_t NVSClass::getUShort(const char* key, const uint16_t defaultValue){
     return value;
 }
 
-int32_t NVSClass::getInt(const char* key, const int32_t defaultValue){
+int32_t Preferences::getInt(const char* key, const int32_t defaultValue){
     int32_t value = defaultValue;
     if(!_started || !key){
         return value;
@@ -324,7 +344,7 @@ int32_t NVSClass::getInt(const char* key, const int32_t defaultValue){
     return value;
 }
 
-uint32_t NVSClass::getUInt(const char* key, const uint32_t defaultValue){
+uint32_t Preferences::getUInt(const char* key, const uint32_t defaultValue){
     uint32_t value = defaultValue;
     if(!_started || !key){
         return value;
@@ -336,7 +356,15 @@ uint32_t NVSClass::getUInt(const char* key, const uint32_t defaultValue){
     return value;
 }
 
-int64_t NVSClass::getLong(const char* key, const int64_t defaultValue){
+int32_t Preferences::getLong(const char* key, const int32_t defaultValue){
+    return getInt(key, defaultValue);
+}
+
+uint32_t Preferences::getULong(const char* key, const uint32_t defaultValue){
+    return getUInt(key, defaultValue);
+}
+
+int64_t Preferences::getLong64(const char* key, const int64_t defaultValue){
     int64_t value = defaultValue;
     if(!_started || !key){
         return value;
@@ -348,7 +376,7 @@ int64_t NVSClass::getLong(const char* key, const int64_t defaultValue){
     return value;
 }
 
-uint64_t NVSClass::getULong(const char* key, const uint64_t defaultValue){
+uint64_t Preferences::getULong64(const char* key, const uint64_t defaultValue){
     uint64_t value = defaultValue;
     if(!_started || !key){
         return value;
@@ -360,7 +388,23 @@ uint64_t NVSClass::getULong(const char* key, const uint64_t defaultValue){
     return value;
 }
 
-size_t NVSClass::getString(const char* key, char* value, const size_t maxLen){
+float_t Preferences::getFloat(const char* key, const float_t defaultValue) {
+    float_t value = defaultValue;
+    getBytes(key, (void*) &value, sizeof(float_t));
+    return value;
+}
+
+double_t Preferences::getDouble(const char* key, const double_t defaultValue) {
+    double_t value = defaultValue;
+    getBytes(key, (void*) &value, sizeof(double_t));
+    return value;
+}
+
+bool Preferences::getBool(const char* key, const bool defaultValue) {
+    return getUChar(key, defaultValue ? 1 : 0) == 1;
+}
+
+size_t Preferences::getString(const char* key, char* value, const size_t maxLen){
     size_t len = 0;
     if(!_started || !key || !value || !maxLen){
         return 0;
@@ -382,7 +426,7 @@ size_t NVSClass::getString(const char* key, char* value, const size_t maxLen){
     return len;
 }
 
-String NVSClass::getString(const char* key, const String defaultValue){
+String Preferences::getString(const char* key, const String defaultValue){
     char * value = NULL;
     size_t len = 0;
     if(!_started || !key){
@@ -403,7 +447,7 @@ String NVSClass::getString(const char* key, const String defaultValue){
     return String(buf);
 }
 
-size_t NVSClass::getBytes(const char* key, void * buf, size_t maxLen){
+size_t Preferences::getBytes(const char* key, void * buf, size_t maxLen){
     size_t len = 0;
     if(!_started || !key || !buf || !maxLen){
         return 0;
@@ -424,4 +468,3 @@ size_t NVSClass::getBytes(const char* key, void * buf, size_t maxLen){
     }
     return len;
 }
-
