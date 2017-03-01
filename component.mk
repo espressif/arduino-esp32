72ce040785c4c69f35ce10c48e912fdd67347891
me-no-dev <hristo@espressif.com>
2017-03-02 01:26:41 +0200

    Use wildcard for core includes in IDF
    thanks @Curclamas for pointing it out :)

--- component.mk
+++ component.mk
@@ -1,6 +1,6 @@
-ARDUINO_LIB_DIRS := libraries/WiFi/src libraries/SPI/src libraries/Wire/src libraries/Preferences/src libraries/SimpleBLE/src libraries/WiFiClientSecure/src
+ARDUINO_CORE_LIBS := $(wildcard libraries/*/src)
 
-COMPONENT_ADD_INCLUDEDIRS := cores/esp32 variants/esp32 $(ARDUINO_LIB_DIRS)
+COMPONENT_ADD_INCLUDEDIRS := cores/esp32 variants/esp32 $(ARDUINO_CORE_LIBS)
 COMPONENT_PRIV_INCLUDEDIRS := cores/esp32/libb64
-COMPONENT_SRCDIRS := cores/esp32/libb64 cores/esp32 variants/esp32 $(ARDUINO_LIB_DIRS)
+COMPONENT_SRCDIRS := cores/esp32/libb64 cores/esp32 variants/esp32 $(ARDUINO_CORE_LIBS)
 CXXFLAGS += -fno-rtti
