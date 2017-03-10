2f5efed220a16835b749c0c1599080d97f835dc8
me-no-dev <hristo@espressif.com>
2017-03-10 17:35:39 +0200

    Add pin mapping for SD and SD_MMC examples

--- libraries/SD/examples/SD_Test/SD_Test.ino
+++ libraries/SD/examples/SD_Test/SD_Test.ino
@@ -1,3 +1,17 @@
+/*
+ * Connect the SD card to the following pins:
+ *
+ * SD Card | ESP32
+ *    D2       -
+ *    D3       SS
+ *    CMD      MOSI
+ *    VSS      GND
+ *    VDD      3.3V
+ *    CLK      SCK
+ *    VSS      GND
+ *    D0       MISO
+ *    D1       -
+ */
 #include "FS.h"
 #include "SD.h"
 #include "SPI.h"
--- libraries/SD_MMC/examples/SDMMC_Test/SDMMC_Test.ino
+++ libraries/SD_MMC/examples/SDMMC_Test/SDMMC_Test.ino
@@ -1,3 +1,18 @@
+/*
+ * Connect the SD card to the following pins:
+ *
+ * SD Card | ESP32
+ *    D2       12
+ *    D3       13
+ *    CMD      15
+ *    VSS      GND
+ *    VDD      3.3V
+ *    CLK      14
+ *    VSS      GND
+ *    D0       2  (add 1K pull up after flashing)
+ *    D1       4
+ */
+
 #include "FS.h"
 #include "SD_MMC.h"
 
