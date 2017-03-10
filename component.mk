161a224543bceb276edc5d71b33546943c859508
me-no-dev <hristo@espressif.com>
2017-03-10 15:00:15 +0200

    fix IDF library includes

--- component.mk
+++ component.mk
@@ -1,4 +1,4 @@
-ARDUINO_CORE_LIBS := $(wildcard libraries/*/src)
+ARDUINO_CORE_LIBS := $(patsubst $(COMPONENT_PATH)/%,%,$(wildcard $(COMPONENT_PATH)/libraries/*/src))
 
 COMPONENT_ADD_INCLUDEDIRS := cores/esp32 variants/esp32 $(ARDUINO_CORE_LIBS)
 COMPONENT_PRIV_INCLUDEDIRS := cores/esp32/libb64
