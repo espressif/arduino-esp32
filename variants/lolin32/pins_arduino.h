7920f769770bfae23891e89df028c7402fdee014
wemos <support@wemos.cc>
2017-04-11 11:51:02 +0800

    Change board's name to "LOLIN32" Change BUILTIN_LED to GPIO5

--- variants/lolin32/pins_arduino.h
+++ variants/lolin32/pins_arduino.h
@@ -11,7 +11,7 @@
 #define digitalPinToInterrupt(p)    (((p)<40)?(p):-1)
 #define digitalPinHasPWM(p)         (p < 34)
 
-static const uint8_t LED_BUILTIN = 2;
+static const uint8_t LED_BUILTIN = 5;
 #define BUILTIN_LED  LED_BUILTIN // backward compatibility
 
 static const uint8_t KEY_BUILTIN = 0;
