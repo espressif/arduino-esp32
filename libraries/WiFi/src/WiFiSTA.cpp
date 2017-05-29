0e513f3a4bf20089774ae91b82debe36651487c8
me-no-dev <hristo@espressif.com>
2017-05-29 13:12:42 +0300

    Make sure WiFi is initialized in disconnect.
    Fixes: https://github.com/espressif/arduino-esp32/issues/400

--- libraries/WiFi/src/WiFiSTA.cpp
+++ libraries/WiFi/src/WiFiSTA.cpp
@@ -259,6 +259,7 @@ bool WiFiSTAClass::disconnect(bool wifioff)
     *conf.sta.ssid = 0;
     *conf.sta.password = 0;
 
+    WiFi.getMode();
     esp_wifi_set_config(WIFI_IF_STA, &conf);
     ret = esp_wifi_set_config(WIFI_IF_STA, &conf) == ESP_OK;
 
