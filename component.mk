89b4e37c95c8c170415b9bada9e6d06294d0c5e8
Andreas Pokorny <andreas.pokorny@siemens.com>
2017-07-05 17:32:04 +0200

    Disable SimpleBLE when BT not enabled
    SimpleBLE would otherwise require a bt header file to in the include paths.
    Forcing people to enable bt even when not needed. Which is a pity because that also disables an memory region for BT exclusive use.
    
    Signed-off-by: Andreas Pokorny <andreas.pokorny@siemens.com>

--- component.mk
+++ component.mk
@@ -1,4 +1,25 @@
-ARDUINO_CORE_LIBS := $(patsubst $(COMPONENT_PATH)/%,%,$(wildcard $(COMPONENT_PATH)/libraries/*/src))
+ARDUINO_CORE_LIBS := libraries/ArduinoOTA/src  \
+    libraries/ESP32/src \
+    libraries/ESPmDNS/src \
+    libraries/FS/src  \
+    libraries/HTTPClient/src \
+    libraries/Preferences/src \
+    libraries/SD/src \
+    libraries/SD_MMC/src \
+    libraries/SPI/src \
+    libraries/Update/src \
+    libraries/Wire/src
+
+ifdef CONFIG_BT_ENABLED
+ifdef CONFIG_BLUEDROID_ENABLED
+ARDUINO_CORE_LIBS += libraries/SimpleBLE/src
+endif
+endif
+
+ifdef CONFIG_WIFI_ENABLED
+ARDUINO_CORE_LIBS += libraries/WiFi/src \
+		     libraries/WiFiClientSecure/src
+endif
 
 COMPONENT_ADD_INCLUDEDIRS := cores/esp32 variants/esp32 $(ARDUINO_CORE_LIBS)
 COMPONENT_PRIV_INCLUDEDIRS := cores/esp32/libb64
