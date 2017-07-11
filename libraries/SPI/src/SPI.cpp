d4f7f55959f5db880f5c45913d278fd2038a1c2b
Glen McGillan <glen@automatic.com>
2017-07-11 11:17:29 -0700

    Add files via upload
    Remove white space changes.

--- libraries/SPI/src/SPI.cpp
+++ libraries/SPI/src/SPI.cpp
@@ -1,9 +1,9 @@
-/*
+/* 
  SPI.cpp - SPI library for esp8266
 
  Copyright (c) 2015 Hristo Gochkov. All rights reserved.
  This file is part of the esp8266 core for Arduino environment.
-
+ 
  This library is free software; you can redistribute it and/or
  modify it under the terms of the GNU Lesser General Public
  License as published by the Free Software Foundation; either
