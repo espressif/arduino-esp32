7e2e9bec079c7d5bb78195436ab94eb443142eb7
Andreas Pokorny <andreas.pokorny@siemens.com>
2017-07-17 08:56:45 +0200

    Use gnu make filter function
    With that arbitrary libraries can still be dropped into arduino-esp32 and built with the rest.
    
    Signed-off-by: Andreas Pokorny <andreas.pokorny@siemens.com>

--- component.mk
+++ component.mk
@@ -1,26 +1,18 @@
-ARDUINO_CORE_LIBS := libraries/ArduinoOTA/src  \
-    libraries/ESP32/src \
-    libraries/ESPmDNS/src \
-    libraries/FS/src  \
-    libraries/HTTPClient/src \
-    libraries/Preferences/src \
-    libraries/SD/src \
-    libraries/SD_MMC/src \
-    libraries/SPI/src \
-    libraries/Update/src \
-    libraries/Wire/src
+ARDUINO_CORE_LIBS := $(patsubst $(COMPONENT_PATH)/%,%,$(wildcard $(COMPONENT_PATH)/libraries/*/src))
+FILTER_LIBS :=
 
-ifdef CONFIG_BT_ENABLED
-ifdef CONFIG_BLUEDROID_ENABLED
-ARDUINO_CORE_LIBS += libraries/SimpleBLE/src
+ifndef CONFIG_BT_ENABLED
+ifndef CONFIG_BLUEDROID_ENABLED
+FILTER_LIBS += libraries/SimpleBLE/src
 endif
 endif
 
-ifdef CONFIG_WIFI_ENABLED
-ARDUINO_CORE_LIBS += libraries/WiFi/src \
-		     libraries/WiFiClientSecure/src
+ifndef CONFIG_WIFI_ENABLED
+FILTER_LIBS += libraries/WiFi/src  \
+	       libraries/WiFiClientSecure/src
 endif
 
+ARDUINO_CORE_LIBS := $(filter-out $(FILTER_LIBS),$(ARDUINO_CORE_LIBS))
 COMPONENT_ADD_INCLUDEDIRS := cores/esp32 variants/esp32 $(ARDUINO_CORE_LIBS)
 COMPONENT_PRIV_INCLUDEDIRS := cores/esp32/libb64
 COMPONENT_SRCDIRS := cores/esp32/libb64 cores/esp32 variants/esp32 $(ARDUINO_CORE_LIBS)
