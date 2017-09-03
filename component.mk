fd9cc002e8f0b774861a13759570315fc374ecd8
Xie Yi <imxieyi@users.noreply.github.com>
2017-09-04 00:18:20 +0800

    Fix support for some arduino libraries which don't put source files under a src directory (#605)
    * fix support for some arduino libraries
    
    * no longer need line 1

--- component.mk
+++ component.mk
@@ -1,4 +1,4 @@
-ARDUINO_CORE_LIBS := $(patsubst $(COMPONENT_PATH)/%,%,$(wildcard $(COMPONENT_PATH)/libraries/*/src))
+ARDUINO_CORE_LIBS := $(patsubst $(COMPONENT_PATH)/%,%,$(sort $(dir $(wildcard $(COMPONENT_PATH)/libraries/*/*/))))
 
 COMPONENT_ADD_INCLUDEDIRS := cores/esp32 variants/esp32 $(ARDUINO_CORE_LIBS)
 COMPONENT_PRIV_INCLUDEDIRS := cores/esp32/libb64
