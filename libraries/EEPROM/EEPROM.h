af8f198d7b00c33339c3b0a738fe080fe7c81a80
fedy0 <ifedioraelochukwuc@gmail.com>
2017-12-30 03:10:39 +0100

    enchanced EEPROMClass

--- libraries/EEPROM/EEPROM.h
+++ libraries/EEPROM/EEPROM.h
@@ -1,13 +1,13 @@
-/* 
-   EEPROM.h -ported by Paolo Becchi to Esp32 
-
+/*
+   EEPROM.h -ported by Paolo Becchi to Esp32
+            -Modified by Ifediora Elochukwu C. <eloifediora@fedironics.com>
    use a one sector flash partition defined in partition table
-  
-  from esp8266 EEPROM 
+
+  from esp8266 EEPROM
 
   Copyright (c) 2014 Ivan Grokhotkov. All rights reserved.
   This file is part of the esp8266 core for Arduino environment.
- 
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -44,7 +44,9 @@ extern "C" {
 class EEPROMClass {
 public:
   EEPROMClass(uint32_t sector);
+  EEPROMClass(const char* name);
   EEPROMClass(void);
+  ~EEPROMClass(void);
 
   bool begin(size_t size);
   uint8_t read(int address);
@@ -54,7 +56,7 @@ public:
 
   uint8_t * getDataPtr();
 
-  template<typename T> 
+  template<typename T>
   T &get(int address, T &t) {
     if (address < 0 || address + sizeof(T) > _size)
       return t;
@@ -63,7 +65,7 @@ public:
     return t;
   }
 
-  template<typename T> 
+  template<typename T>
   const T &put(int address, const T &t) {
     if (address < 0 || address + sizeof(T) > _size)
       return t;
@@ -73,17 +75,58 @@ public:
     return t;
   }
 
+    uint16_t length();
+
+    uint8_t readByte(int address);
+    int8_t readChar(int address);
+    uint8_t readUChar(int address);
+    int16_t readShort(int address);
+    uint16_t readUShort(int address);
+    int32_t readInt(int address);
+    uint32_t readUInt(int address);
+    int32_t readLong(int address);
+    uint32_t readULong(int address);
+    int64_t readLong64(int address);
+    uint64_t readULong64(int address);
+    float_t readFloat(int address);
+    double_t readDouble(int address);
+    bool readBool(int address);
+    size_t readString(int address, char* value, size_t maxLen);
+    String readString(int address);
+    size_t readBytes(int address, void * value, size_t maxLen);
+    template <class T> T readAll (int address, T &);
+
+    size_t writeByte(int address, uint8_t value);
+    size_t writeChar(int address, int8_t value);
+    size_t writeUChar(int address, uint8_t value);
+    size_t writeShort(int address, int16_t value);
+    size_t writeUShort(int address, uint16_t value);
+    size_t writeInt(int address, int32_t value);
+    size_t writeUInt(int address, uint32_t value);
+    size_t writeLong(int address, int32_t value);
+    size_t writeULong(int address, uint32_t value);
+    size_t writeLong64(int address, int64_t value);
+    size_t writeULong64(int address, uint64_t value);
+    size_t writeFloat(int address, float_t value);
+    size_t writeDouble(int address, double_t value);
+    size_t writeBool(int address, bool value);
+    size_t writeString(int address, const char* value);
+    size_t writeString(int address, String value);
+    size_t writeBytes(int address, const void* value, size_t len);
+    template <class T> T writeAll (int address, const T &);
+
 protected:
   uint32_t _sector;
   uint8_t* _data;
   size_t _size;
   bool _dirty;
   const esp_partition_t * _mypart;
+  const char* _name;
 };
 
-#if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_EEPROM)
-extern EEPROMClass EEPROM;
-#endif
+//#if !defined(NO_GLOBAL_INSTANCES) && !defined(NO_GLOBAL_EEPROM)
+//extern EEPROMClass EEPROM;
+//#endif
 
 #endif
 
