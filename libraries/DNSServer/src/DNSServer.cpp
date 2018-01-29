83bfebcb9e5156e9795599078ea0eb9aa5622369
Shawn A <spam@rumental.com>
2018-01-29 10:26:29 -0600

    adds debugging to dnsserver

--- libraries/DNSServer/src/DNSServer.cpp
+++ libraries/DNSServer/src/DNSServer.cpp
@@ -2,6 +2,12 @@
 #include <lwip/def.h>
 #include <Arduino.h>
 
+// #define DEBUG_ESP_DNS
+#ifdef DEBUG_ESP_PORT
+#define DEBUG_OUTPUT DEBUG_ESP_PORT
+#else
+#define DEBUG_OUTPUT Serial
+#endif
 
 DNSServer::DNSServer()
 {
@@ -132,6 +138,11 @@ void DNSServer::replyWithIP()
   _udp.write((uint8_t)4);
   _udp.write(_resolvedIP, 4);
   _udp.endPacket();
+
+  #ifdef DEBUG_ESP_DNS
+    DEBUG_OUTPUT.printf("DNS responds: %s for %s\n",
+            IPAddress(_resolvedIP).toString().c_str(), getDomainNameWithoutWwwPrefix().c_str() );
+  #endif  
 }
 
 void DNSServer::replyWithCustomCode()
