90174189003e5f02ca06d0e05de15a94807e0257
copercini <evandrocopercini@yahoo.com>
2018-02-23 07:45:01 -0300

    Replace deprecated header and small fixes

--- libraries/BluetoothSerial/examples/SerialToSerialBT/SerialToSerialBT.ino
+++ libraries/BluetoothSerial/examples/SerialToSerialBT/SerialToSerialBT.ino
@@ -15,6 +15,7 @@ BluetoothSerial SerialBT;
 void setup() {
   Serial.begin(115200);
   SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!")
 }
 
 void loop() {
