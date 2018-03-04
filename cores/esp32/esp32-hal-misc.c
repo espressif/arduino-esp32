835268c326e0a6c95a36e66f5b6a6d24c2c51f8f
me-no-dev <hristo@espressif.com>
2018-03-04 21:23:18 +0100

    fix thread storage

--- cores/esp32/esp32-hal-misc.c
+++ cores/esp32/esp32-hal-misc.c
@@ -21,6 +21,7 @@
 #include "nvs.h"
 #include "esp_partition.h"
 #include "esp_log.h"
+#include "pthread.h"
 #include <sys/time.h>
 
 //Undocumented!!! Get chip temperature in Farenheit
@@ -40,7 +41,7 @@ void yield()
 portMUX_TYPE microsMux = portMUX_INITIALIZER_UNLOCKED;
 static pthread_key_t microsStore=NULL; // Thread Local Storage Handle
 
-void* microsStoreDelete(void * storage) {  // release thread local data when task is delete.
+void microsStoreDelete(void * storage) {  // release thread local data when task is delete.
     if(storage) free(storage);
 }
 
