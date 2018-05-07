d72e8137b3933470bfa8292e801389064fd8d001
timpur <timothy@purchas.com>
2018-05-08 08:29:33 +1000

    Exampl of Wifi Events

--- libraries/WiFi/src/WiFiType.h
+++ libraries/WiFi/src/WiFiType.h
@@ -33,7 +33,8 @@
 #define WIFI_AP_STA  WIFI_MODE_APSTA
 
 #define WiFiEvent_t  system_event_id_t
-#define WiFiEvent_id_t wifi_event_id_t;
+#define WiFiEventInfo_t system_event_info_t
+#define WiFiEventId_t wifi_event_id_t
 
 
 typedef enum {
