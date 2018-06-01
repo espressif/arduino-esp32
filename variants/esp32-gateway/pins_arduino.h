d157e8b63276a68d934bb17dfe5727bd0f989165
timkoers <timkoers@users.noreply.github.com>
2018-06-01 22:14:24 +0200

    Added SPI bus pins
    Added SPI bus pins and tested them as well

--- variants/esp32-gateway/pins_arduino.h
+++ variants/esp32-gateway/pins_arduino.h
@@ -20,6 +20,11 @@ static const uint8_t KEY_BUILTIN = 34;
 static const uint8_t SCL = 16; // This is pin 11
 static const uint8_t SDA = 17; // This is pin 12
 
+static const uint8_t SS    = 5;
+static const uint8_t MOSI  = 23;
+static const uint8_t MISO  = 19;
+static const uint8_t SCK   = 18;
+
 static const uint8_t TX = 1;
 static const uint8_t RX = 3;
 
