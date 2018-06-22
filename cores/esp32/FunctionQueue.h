46e1ae82b17b5e189956ba40d4621c2db7ab7db6
hreintke <hreintke@tauri.nl>
2018-06-22 12:34:28 +0200

    Update slashes in includes

--- cores/esp32/FunctionQueue.h
+++ cores/esp32/FunctionQueue.h
@@ -12,11 +12,11 @@
 
 extern "C"
 {
-#include "freertos\FreeRTOS.h"
-#include "freertos\task.h"
-#include "freertos\queue.h"
-#include "freertos\semphr.h"
-#include "freertos\event_groups.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+#include "freertos/event_groups.h"
 }
 
 class FunctionQueue {
