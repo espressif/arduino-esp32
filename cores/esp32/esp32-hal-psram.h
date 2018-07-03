5e46c9bae6741196130a41d3d5e8dba316153b1e
me-no-dev <hristo@espressif.com>
2018-07-03 23:03:50 +0200

    Add Wrover Support and Option to enable SPIRAM

--- cores/esp32/esp32-hal-psram.h
+++ cores/esp32/esp32-hal-psram.h
@@ -15,11 +15,19 @@
 #ifndef _ESP32_HAL_PSRAM_H_
 #define _ESP32_HAL_PSRAM_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bool psramInit();
 bool psramFound();
 
 void *ps_malloc(size_t size);
 void *ps_calloc(size_t n, size_t size);
 void *ps_realloc(void *ptr, size_t size);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _ESP32_HAL_PSRAM_H_ */
