2d5af6232c91efadabe9a02038bcd18469c041c6
hreintke <hreintke@tauri.nl>
2018-08-20 16:13:49 +0200

    Add IRAM_ATTR

--- cores/esp32/FunctionalInterrupt.cpp
+++ cores/esp32/FunctionalInterrupt.cpp
@@ -16,7 +16,7 @@ extern "C"
 	extern void __attachInterruptFunctionalArg(uint8_t pin, voidFuncPtrArg userFunc, void * arg, int intr_type, bool functional);
 }
 
-void interruptFunctional(void* arg)
+void IRAM_ATTR interruptFunctional(void* arg)
 {
     InterruptArgStructure* localArg = (InterruptArgStructure*)arg;
 	if (localArg->interruptFunction)
