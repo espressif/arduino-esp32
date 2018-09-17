4e96bffe0ef0e3929ba0ecf540c9c6f299b31e14
david-cermak <38914379+david-cermak@users.noreply.github.com>
2018-09-17 23:19:27 +0200

    Initial version of rmt driver (#1525)
    * rmt driver initial version
    
    * supporting conti mode plus interrupts
    
    * using conitnous mode for sending more data
    
    * working continous mode
    
    * rmt driver cleanup after conti mode
    
    * initial version of rmt driver
    
    * adding a simple example
    
    * adding channel and block locks
    
    * modified of rmt interface for simpler/easier usage
    
    * adding header sentinels, split interface to common and additional settings
    
    * Fixes per code review + support for rx callback mode
    
    * renamed internal structures and enums, fixed formatting
    
    * cmake support for rmt
    
    * refactored tx-conti interrupts to function to make it more readable
    
    * added Tx and Rx examples
    
    * added license headers
    
    * minor updates per review
    
    * used struct access, renamed defines, corrected diagram

--- cores/esp32/esp32-hal.h
+++ cores/esp32/esp32-hal.h
@@ -57,6 +57,7 @@ void yield(void);
 #include "esp32-hal-spi.h"
 #include "esp32-hal-i2c.h"
 #include "esp32-hal-ledc.h"
+#include "esp32-hal-rmt.h"
 #include "esp32-hal-sigmadelta.h"
 #include "esp32-hal-timer.h"
 #include "esp32-hal-bt.h"
