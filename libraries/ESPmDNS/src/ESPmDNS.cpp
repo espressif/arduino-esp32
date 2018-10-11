601a094fb5a24a4c90901cd1ab0e299c671bbb6e
Bascy <bas@basbex.com>
2018-10-11 23:05:53 +0200

    Replaced ARDUINO_VARIANT with const char

--- libraries/ESPmDNS/src/ESPmDNS.cpp
+++ libraries/ESPmDNS/src/ESPmDNS.cpp
@@ -82,7 +82,7 @@ void MDNSResponder::setInstanceName(String name) {
 
 void MDNSResponder::enableArduino(uint16_t port, bool auth){
     mdns_txt_item_t arduTxtData[4] = {
-        {(char*)"board"         ,(char*)ARDUINO_VARIANT},
+        {(char*)"board"         ,(char*)"esp32"},
         {(char*)"tcp_check"     ,(char*)"no"},
         {(char*)"ssh_upload"    ,(char*)"no"},
         {(char*)"auth_upload"   ,(char*)"no"}
