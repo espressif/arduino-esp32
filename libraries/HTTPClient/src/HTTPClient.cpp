79724d689392805e347667197ffc61c8f29805cd
Jeroen88 <info@el-mago.nl>
2018-10-26 23:33:05 +0200

    Added small comment because it looked like the Travis build did not finish

--- libraries/HTTPClient/src/HTTPClient.cpp
+++ libraries/HTTPClient/src/HTTPClient.cpp
@@ -22,6 +22,7 @@
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
+ * Adapted in October 2018
  */
 
 #include <Arduino.h>
