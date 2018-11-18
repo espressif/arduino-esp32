e39f0ae5840e7e720afb2aa19c903c8c79089137
Muhammad Lutfi Santoso <m_lutfi_santoso@yahoo.co.id>
2018-11-18 23:40:53 +0700

    Update Code

--- libraries/FreeRTOS/FreeRTOS.ino
+++ libraries/FreeRTOS/FreeRTOS.ino
@@ -5,13 +5,13 @@
 #endif
 
 // define two tasks for Blink & AnalogRead
-void TaskAnalogReadA0( void *pvParameters );
+void TaskBlink( void *pvParameters );
 void TaskAnalogReadA3( void *pvParameters );
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   
-  // initialize serial communication at 9600 bits per second:
+  // initialize serial communication at 115200 bits per second:
   Serial.begin(115200);
   
   while (!Serial) {
@@ -20,8 +20,8 @@ void setup() {
 
   // Now set up two tasks to run independently.
   xTaskCreatePinnedToCore(
-    TaskAnalogReadA0
-    ,  "AnalogReadA0"   // A name just for humans
+    TaskBlink
+    ,  "TaskBlink"   // A name just for humans
     ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
     ,  NULL
     ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
@@ -49,43 +49,26 @@ void loop()
 /*---------------------- Tasks ---------------------*/
 /*--------------------------------------------------*/
 
-void TaskAnalogReadA0(void *pvParameters)  // This is a task.
+void TaskBlink(void *pvParameters)  // This is a task.
 {
   (void) pvParameters;
 
 /*
   Blink
   Turns on an LED on for one second, then off for one second, repeatedly.
-
-  Most Arduinos have an on-board LED you can control. On the UNO, LEONARDO, MEGA, and ZERO 
-  it is attached to digital pin 13, on MKR1000 on pin 6. LED_BUILTIN takes care 
-  of use the correct LED pin whatever is the board used.
-  
-  The MICRO does not have a LED_BUILTIN available. For the MICRO board please substitute
-  the LED_BUILTIN definition with either LED_BUILTIN_RX or LED_BUILTIN_TX.
-  e.g. pinMode(LED_BUILTIN_RX, OUTPUT); etc.
-  
-  If you want to know what pin the on-board LED is connected to on your Arduino model, check
-  the Technical Specs of your board  at https://www.arduino.cc/en/Main/Products
-  
-  This example code is in the public domain.
-
-  modified 8 May 2014
-  by Scott Fitzgerald
-  
-  modified 2 Sep 2016
-  by Arturo Guadalupi
+    
+  If you want to know what pin the on-board LED is connected to on your ESP32 model, check
+  the Technical Specs of your board.
 */
 
   // initialize digital LED_BUILTIN on pin 13 as an output.
-  //pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
 
   for (;;) // A Task shall never return or exit.
   {
-    // read the input on analog pin 0:
-    int sensorValueA0 = analogRead(A0);
-    // print out the value you read:
-    Serial.println(sensorValueA0);
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
     vTaskDelay(100);  // one tick delay (15ms) in between reads for stability
   }
 }
@@ -96,16 +79,16 @@ void TaskAnalogReadA3(void *pvParameters)  // This is a task.
   
 /*
   AnalogReadSerial
-  Reads an analog input on pin 0, prints the result to the serial monitor.
+  Reads an analog input on pin A3, prints the result to the serial monitor.
   Graphical representation is available using serial plotter (Tools > Serial Plotter menu)
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+  Attach the center pin of a potentiometer to pin A#, and the outside pins to +5V and ground.
 
   This example code is in the public domain.
 */
 
   for (;;)
   {
-    // read the input on analog pin 0:
+    // read the input on analog pin A3:
     int sensorValueA3 = analogRead(A3);
     // print out the value you read:
     Serial.println(sensorValueA3);
