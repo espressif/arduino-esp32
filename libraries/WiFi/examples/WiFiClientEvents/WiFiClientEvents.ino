a3ed5118847614388d6188225174e859682c0a57
Junxiao Shi <sunnylandh@gmail.com>
2018-11-19 10:42:33 -0500

    WiFi: improve WiFiEvent list in WiFiClientEvents example (#1917)
    fixes #1875

--- libraries/WiFi/examples/WiFiClientEvents/WiFiClientEvents.ino
+++ libraries/WiFi/examples/WiFiClientEvents/WiFiClientEvents.ino
@@ -3,34 +3,35 @@
  *
 */
 
-/* 
+/*
 * WiFi Events
 
-SYSTEM_EVENT_WIFI_READY               < ESP32 WiFi ready
-SYSTEM_EVENT_SCAN_DONE                < ESP32 finish scanning AP
-SYSTEM_EVENT_STA_START                < ESP32 station start
-SYSTEM_EVENT_STA_STOP                 < ESP32 station stop
-SYSTEM_EVENT_STA_CONNECTED            < ESP32 station connected to AP
-SYSTEM_EVENT_STA_DISCONNECTED         < ESP32 station disconnected from AP
-SYSTEM_EVENT_STA_AUTHMODE_CHANGE      < the auth mode of AP connected by ESP32 station changed
-SYSTEM_EVENT_STA_GOT_IP               < ESP32 station got IP from connected AP
-SYSTEM_EVENT_STA_LOST_IP              < ESP32 station lost IP and the IP is reset to 0
-SYSTEM_EVENT_STA_WPS_ER_SUCCESS       < ESP32 station wps succeeds in enrollee mode
-SYSTEM_EVENT_STA_WPS_ER_FAILED        < ESP32 station wps fails in enrollee mode
-SYSTEM_EVENT_STA_WPS_ER_TIMEOUT       < ESP32 station wps timeout in enrollee mode
-SYSTEM_EVENT_STA_WPS_ER_PIN           < ESP32 station wps pin code in enrollee mode
-SYSTEM_EVENT_AP_START                 < ESP32 soft-AP start
-SYSTEM_EVENT_AP_STOP                  < ESP32 soft-AP stop
-SYSTEM_EVENT_AP_STACONNECTED          < a station connected to ESP32 soft-AP
-SYSTEM_EVENT_AP_STADISCONNECTED       < a station disconnected from ESP32 soft-AP
-SYSTEM_EVENT_AP_PROBEREQRECVED        < Receive probe request packet in soft-AP interface
-SYSTEM_EVENT_GOT_IP6                  < ESP32 station or ap or ethernet interface v6IP addr is preferred
-SYSTEM_EVENT_ETH_START                < ESP32 ethernet start
-SYSTEM_EVENT_ETH_STOP                 < ESP32 ethernet stop
-SYSTEM_EVENT_ETH_CONNECTED            < ESP32 ethernet phy link up
-SYSTEM_EVENT_ETH_DISCONNECTED         < ESP32 ethernet phy link down
-SYSTEM_EVENT_ETH_GOT_IP               < ESP32 ethernet got IP from connected AP
-SYSTEM_EVENT_MAX
+0  SYSTEM_EVENT_WIFI_READY               < ESP32 WiFi ready
+1  SYSTEM_EVENT_SCAN_DONE                < ESP32 finish scanning AP
+2  SYSTEM_EVENT_STA_START                < ESP32 station start
+3  SYSTEM_EVENT_STA_STOP                 < ESP32 station stop
+4  SYSTEM_EVENT_STA_CONNECTED            < ESP32 station connected to AP
+5  SYSTEM_EVENT_STA_DISCONNECTED         < ESP32 station disconnected from AP
+6  SYSTEM_EVENT_STA_AUTHMODE_CHANGE      < the auth mode of AP connected by ESP32 station changed
+7  SYSTEM_EVENT_STA_GOT_IP               < ESP32 station got IP from connected AP
+8  SYSTEM_EVENT_STA_LOST_IP              < ESP32 station lost IP and the IP is reset to 0
+9  SYSTEM_EVENT_STA_WPS_ER_SUCCESS       < ESP32 station wps succeeds in enrollee mode
+10 SYSTEM_EVENT_STA_WPS_ER_FAILED        < ESP32 station wps fails in enrollee mode
+11 SYSTEM_EVENT_STA_WPS_ER_TIMEOUT       < ESP32 station wps timeout in enrollee mode
+12 SYSTEM_EVENT_STA_WPS_ER_PIN           < ESP32 station wps pin code in enrollee mode
+13 SYSTEM_EVENT_AP_START                 < ESP32 soft-AP start
+14 SYSTEM_EVENT_AP_STOP                  < ESP32 soft-AP stop
+15 SYSTEM_EVENT_AP_STACONNECTED          < a station connected to ESP32 soft-AP
+16 SYSTEM_EVENT_AP_STADISCONNECTED       < a station disconnected from ESP32 soft-AP
+17 SYSTEM_EVENT_AP_STAIPASSIGNED         < ESP32 soft-AP assign an IP to a connected station
+18 SYSTEM_EVENT_AP_PROBEREQRECVED        < Receive probe request packet in soft-AP interface
+19 SYSTEM_EVENT_GOT_IP6                  < ESP32 station or ap or ethernet interface v6IP addr is preferred
+20 SYSTEM_EVENT_ETH_START                < ESP32 ethernet start
+21 SYSTEM_EVENT_ETH_STOP                 < ESP32 ethernet stop
+22 SYSTEM_EVENT_ETH_CONNECTED            < ESP32 ethernet phy link up
+23 SYSTEM_EVENT_ETH_DISCONNECTED         < ESP32 ethernet phy link down
+24 SYSTEM_EVENT_ETH_GOT_IP               < ESP32 ethernet got IP from connected AP
+25 SYSTEM_EVENT_MAX
 */
 
 #include <WiFi.h>
@@ -138,7 +139,7 @@ void setup()
 
     delay(1000);
 
-    // Examples of diffrent ways to register wifi events
+    // Examples of different ways to register wifi events
     WiFi.onEvent(WiFiEvent);
     WiFi.onEvent(WiFiGotIP, WiFiEvent_t::SYSTEM_EVENT_STA_GOT_IP);
     WiFiEventId_t eventID = WiFi.onEvent([](WiFiEvent_t event, WiFiEventInfo_t info){
