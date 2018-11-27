bfde8daf75b75c536d3ddeb0d3ac1dc0d9bc211b
Me No Dev <me-no-dev@users.noreply.github.com>
2018-11-27 23:43:34 +0100

    Update build-release.sh
    print out the command

--- tools/build-release.sh
+++ tools/build-release.sh
@@ -350,7 +350,8 @@ if [ $bIsPrerelease -eq 0 ]; then
 		downloadAndMergePackageJSON "https://github.com/$TRAVIS_REPO_SLUG/releases/download/${prev_release}/${PACKAGE_JSON_REL}" "${pkgJsonRel}" "${curlAuth}" "$releaseDir"
 		
 		# Release notes: GIT log comments (prev_release, current_release>
-		cd $srcdir && git log --oneline $prev_release.. > $releaseDir/commits.txt
+		echo " - executing: git log --oneline $prev_release.."
+		git log --oneline $prev_release.. > $releaseDir/commits.txt
 	fi
 fi
 
