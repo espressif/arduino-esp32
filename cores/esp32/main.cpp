b8f8502fcb02bd845ce11de26037827507a25232
me-no-dev <hristo@espressif.com>
2018-12-28 20:32:04 +0200

    Add API to enable/disable WDT for Core 1 and Arduino Loop

--- cores/esp32/main.cpp
+++ cores/esp32/main.cpp
@@ -1,6 +1,9 @@
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "esp_task_wdt.h"
 #include "Arduino.h"
+
+TaskHandle_t loopTaskHandle = NULL;
 
 #if CONFIG_AUTOSTART_ARDUINO
 
@@ -10,18 +13,24 @@
 #define ARDUINO_RUNNING_CORE 1
 #endif
 
+bool loopTaskWDTEnabled;
+
 void loopTask(void *pvParameters)
 {
     setup();
     for(;;) {
+        if(loopTaskWDTEnabled){
+            esp_task_wdt_reset();
+        }
         loop();
     }
 }
 
 extern "C" void app_main()
 {
+    loopTaskWDTEnabled = false;
     initArduino();
-    xTaskCreatePinnedToCore(loopTask, "loopTask", 8192, NULL, 1, NULL, ARDUINO_RUNNING_CORE);
+    xTaskCreatePinnedToCore(loopTask, "loopTask", 8192, NULL, 1, &loopTaskHandle, ARDUINO_RUNNING_CORE);
 }
 
 #endif
