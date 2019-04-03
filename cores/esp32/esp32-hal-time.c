c271d3f4c57e089a16b63b7dc7b5c8b6508cab76
A C SREEDHAR REDDY <acreddysreedharreddy@gmail.com>
2019-04-04 00:09:21 +0530

    timeout logic.

--- cores/esp32/esp32-hal-time.c
+++ cores/esp32/esp32-hal-time.c
@@ -78,7 +78,7 @@ bool getLocalTime(struct tm * info, uint32_t ms)
 {
     uint32_t start = millis();
     time_t now;
-    while((millis()-start) > ms) {
+    while((millis()-start) <= ms) {
         time(&now);
         localtime_r(&now, info);
         if(info->tm_year > (2016 - 1900)){
