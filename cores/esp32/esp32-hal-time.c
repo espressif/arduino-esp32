0906bf580f639c17c4b899b9cb0828088ac239bd
A C SREEDHAR REDDY <acreddysreedharreddy@gmail.com>
2019-04-10 06:09:14 +0530

    update getLocalTime(). (#2629)
    * update getLocalTime().
    
    * change count to millis in getLocalTime
    
    * timeout logic.

--- cores/esp32/esp32-hal-time.c
+++ cores/esp32/esp32-hal-time.c
@@ -76,23 +76,15 @@ void configTzTime(const char* tz, const char* server1, const char* server2, cons
 
 bool getLocalTime(struct tm * info, uint32_t ms)
 {
-    uint32_t count = ms / 10;
+    uint32_t start = millis();
     time_t now;
-
-    time(&now);
-    localtime_r(&now, info);
-
-    if(info->tm_year > (2016 - 1900)){
-        return true;
-    }
-
-    while(count--) {
-        delay(10);
+    while((millis()-start) <= ms) {
         time(&now);
         localtime_r(&now, info);
         if(info->tm_year > (2016 - 1900)){
             return true;
         }
+        delay(10);
     }
     return false;
 }
