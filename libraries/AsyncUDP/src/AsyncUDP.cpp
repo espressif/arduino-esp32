64c465de6558ae68e34b0edcc9e8bd9d0f030eca
Larry Bernstone <lbernstone@gmail.com>
2019-05-09 15:04:53 -0600

    Revert #2607.  Fixes #2685

--- libraries/AsyncUDP/src/AsyncUDP.cpp
+++ libraries/AsyncUDP/src/AsyncUDP.cpp
@@ -682,8 +682,8 @@ void AsyncUDP::_recv(udp_pcb *upcb, pbuf *pb, const ip_addr_t *addr, uint16_t po
         if(_handler) {
             AsyncUDPPacket packet(this, this_pb, addr, port, netif);
             _handler(packet);
-        }
-        pbuf_free(this_pb);
+        } else {
+            pbuf_free(this_pb);
     }
 }
 
