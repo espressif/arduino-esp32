562fbcb37ba4d01f0f149776df789e1a14bf0de3
Dirk O. Kaar <dok@dok-net.net>
2019-06-02 11:29:12 +0200

    Port Schedule to ESP32.
    Same yield behavior as on ESP8266 - scheduler has policies for loop and yield initiated runs.

--- cores/esp32/Schedule.cpp
+++ cores/esp32/Schedule.cpp
@@ -1,78 +1,136 @@
 #include "Schedule.h"
+#include "PolledTimeout.h"
+#include "Ticker.h"
+#ifdef ESP8266
+#include "interrupts.h"
+#include "coredecls.h"
+#else
+#include <mutex>
+#endif
+#include "circular_queue.h"
+
+typedef std::function<bool(void)> mFuncT;
+
+constexpr uint32_t TICKER_MIN_US = 5000;
 
 struct scheduled_fn_t
 {
-    scheduled_fn_t* mNext;
-    std::function<void(void)> mFunc;
+	mFuncT mFunc = nullptr;
+	esp8266::polledTimeout::periodicFastUs callNow;
+	schedule_e policy = SCHEDULE_FUNCTION_FROM_LOOP;
+	scheduled_fn_t() : callNow(esp8266::polledTimeout::periodicFastUs::alwaysExpired) { }
 };
 
-static scheduled_fn_t* sFirst = 0;
-static scheduled_fn_t* sLast = 0;
+namespace {
+	static circular_queue_mp<scheduled_fn_t> schedule_queue(SCHEDULED_FN_MAX_COUNT);
+#ifndef ESP8266
+	std::mutex schedulerMutex;
+#endif
 
-static scheduled_fn_t* sFirstUnused = 0;
-static scheduled_fn_t* sLastUnused = 0;
+	void ticker_scheduled(Ticker* ticker, const std::function<bool(void)>& fn, uint32_t repeat_us, schedule_e policy)
+	{
+		auto repeat_ms = (repeat_us + 500) / 1000;
+		ticker->once_ms(repeat_ms, [ticker, fn, repeat_us, policy]()
+			{
+				if (!schedule_function([ticker, fn, repeat_us, policy]()
+					{
+						if (fn()) ticker_scheduled(ticker, fn, repeat_us, policy);
+						else delete ticker;
+						return false;
+					}, policy))
+				{
+					ticker_scheduled(ticker, fn, repeat_us, policy);
+				}
+			});
+	}
+};
 
-static int sCount = 0;
-
-static scheduled_fn_t* get_fn() {
-    scheduled_fn_t* result = NULL;
-    // try to get an item from unused items list
-    if (sFirstUnused) {
-        result = sFirstUnused;
-        sFirstUnused = result->mNext;
-        if (sFirstUnused == NULL) {
-            sLastUnused = NULL;
-        }
-    }
-    // if no unused items, and count not too high, allocate a new one
-    else if (sCount != SCHEDULED_FN_MAX_COUNT) {
-        result = new scheduled_fn_t;
-        result->mNext = NULL;
-        ++sCount;
-    }
-    return result;
+bool IRAM_ATTR schedule_function_us(std::function<bool(void)>&& fn, uint32_t repeat_us, schedule_e policy)
+{
+	if (repeat_us >= TICKER_MIN_US)
+	{
+		auto ticker = new Ticker;
+		if (!ticker) return false;
+		if (!schedule_function([ticker, fn = std::move(fn), repeat_us, policy]()
+		{
+			ticker_scheduled(ticker, fn, repeat_us, policy);
+			return false;
+		}, SCHEDULE_FUNCTION_WITHOUT_YIELDELAYCALLS))
+		{
+			delete ticker;
+			return false;
+		}
+		return true;
+	}
+	else
+	{
+		scheduled_fn_t item;
+		item.mFunc = std::move(fn);
+		if (repeat_us) item.callNow.reset(repeat_us);
+		item.policy = policy;
+		return schedule_queue.push(std::move(item));
+	}
 }
 
-static void recycle_fn(scheduled_fn_t* fn)
+bool IRAM_ATTR schedule_function_us(const std::function<bool(void)>& fn, uint32_t repeat_us, schedule_e policy)
 {
-    if (!sLastUnused) {
-        sFirstUnused = fn;
-    }
-    else {
-        sLastUnused->mNext = fn;
-    }
-    fn->mNext = NULL;
-    sLastUnused = fn;
+	return schedule_function_us(std::function<bool(void)>(fn), repeat_us, policy);
 }
 
-bool schedule_function(std::function<void(void)> fn)
+bool IRAM_ATTR schedule_function(std::function<void(void)>&& fn, schedule_e policy)
 {
-    scheduled_fn_t* item = get_fn();
-    if (!item) {
-        return false;
-    }
-    item->mFunc = fn;
-    item->mNext = NULL;
-    if (!sFirst) {
-        sFirst = item;
-    }
-    else {
-        sLast->mNext = item;
-    }
-    sLast = item;
-    return true;
+	return schedule_function_us([fn = std::move(fn)]() { fn(); return false; }, 0, policy);
 }
 
-void run_scheduled_functions()
+bool IRAM_ATTR schedule_function(const std::function<void(void)>& fn, schedule_e policy)
 {
-	scheduled_fn_t* rFirst = sFirst;
-	sFirst = NULL;
-	sLast  = NULL;
-    while (rFirst) {
-        scheduled_fn_t* item = rFirst;
-        rFirst = item->mNext;
-        item->mFunc();
-        item->mFunc = std::function<void(void)>();
-        recycle_fn(item);
-    }
+	return schedule_function(std::function<void(void)>(fn), policy);
+}
+
+void run_scheduled_functions(schedule_e policy)
+{
+	// Note to the reader:
+	// There is no exposed API to remove a scheduled function:
+	// Scheduled functions are removed only from this function, and
+	// its purpose is that it is never called from an interrupt
+	// (always on cont stack).
+
+	static bool fence = false;
+	{
+#ifdef ESP8266
+		InterruptLock lockAllInterruptsInThisScope;
+#else
+		std::lock_guard<std::mutex> lock(schedulerMutex);
+#endif
+		if (fence) {
+			// prevent recursive calls from yield()
+			return;
+		}
+		fence = true;
+	}
+
+	esp8266::polledTimeout::periodicFastMs yieldNow(100); // yield every 100ms
+
+	// run scheduled function:
+	// - when its schedule policy allows it anytime
+	// - or if we are called at loop() time
+	// and
+	// - its time policy allows it
+	schedule_queue.for_each_requeue([policy, &yieldNow](scheduled_fn_t& func)
+		{
+			if (yieldNow) {
+#if defined(ESP8266)
+				cont_yield(g_pcont);
+#elif defined(ESP32)
+				vPortYield();
+#else
+				yield();
+#endif
+			}
+			return
+				(func.policy != SCHEDULE_FUNCTION_WITHOUT_YIELDELAYCALLS && policy != SCHEDULE_FUNCTION_FROM_LOOP)
+				|| !func.callNow
+				|| func.mFunc();
+		});
+	fence = false;
 }
