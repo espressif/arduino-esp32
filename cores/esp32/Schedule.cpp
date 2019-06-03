35018f4027f9aac644884495c68915cbe7d3676c
Dirk O. Kaar <dok@dok-net.net>
2019-06-03 15:44:15 +0200

    Provide own memory management for Ticker instances to conserve heap.
    Insightful source code remark for future refactoring

--- cores/esp32/Schedule.cpp
+++ cores/esp32/Schedule.cpp
@@ -21,13 +21,42 @@ struct scheduled_fn_t
 	scheduled_fn_t() : callNow(esp8266::polledTimeout::periodicFastUs::alwaysExpired) { }
 };
 
+// anonymous namespace provides compilation-unit internal linkage
 namespace {
 	static circular_queue_mp<scheduled_fn_t> schedule_queue(SCHEDULED_FN_MAX_COUNT);
+
+	class SchedulerTicker;
+	// A local heap for Ticker instances to prevent global heap exhaustion
+	class TickerHeap : public circular_queue_mp<SchedulerTicker*> {
+	public:
+		TickerHeap(const size_t capacity) : circular_queue_mp<SchedulerTicker*>(capacity)
+		{
+			heap = new char[capacity * sizeof(Ticker)];
+			for (size_t i = 0; i < capacity; ++i) push(reinterpret_cast<SchedulerTicker*>(heap + i * sizeof(Ticker)));
+		}
+		~TickerHeap()
+		{
+			delete heap;
+		}
+	protected:
+		char* heap;
+	};
+	static TickerHeap tickerHeap(SCHEDULED_FN_MAX_COUNT);
+
+	class SchedulerTicker : public Ticker
+	{
+	public:
+		static void operator delete(void* ptr) {
+			tickerHeap.push(static_cast<SchedulerTicker*>(ptr));
+		}
+	};
+	static_assert(sizeof(SchedulerTicker) == sizeof(Ticker), "sizeof(SchedulerTicker) != sizeof(Ticker)");
+
 #ifndef ESP8266
 	std::mutex schedulerMutex;
 #endif
 
-	void ticker_scheduled(Ticker* ticker, const std::function<bool(void)>& fn, uint32_t repeat_us, schedule_e policy)
+	void ticker_scheduled(SchedulerTicker* ticker, const std::function<bool(void)>& fn, uint32_t repeat_us, schedule_e policy)
 	{
 		auto repeat_ms = (repeat_us + 500) / 1000;
 		ticker->once_ms(repeat_ms, [ticker, fn, repeat_us, policy]()
@@ -49,8 +78,12 @@ bool IRAM_ATTR schedule_function_us(std::function<bool(void)>&& fn, uint32_t rep
 {
 	if (repeat_us >= TICKER_MIN_US)
 	{
-		auto ticker = new Ticker;
-		if (!ticker) return false;
+		// failure to aquire a Ticker must be returned to caller now. Specifically, allocating
+		// Tickers from inside the scheduled function doesn't work, any exhaustion of the scheduler
+		// can dead-lock it forever.
+		auto tickerPlace = tickerHeap.pop();
+		if (!tickerPlace) return false;
+		auto ticker = new(tickerPlace) SchedulerTicker;
 		if (!schedule_function([ticker, fn = std::move(fn), repeat_us, policy]()
 		{
 			ticker_scheduled(ticker, fn, repeat_us, policy);
