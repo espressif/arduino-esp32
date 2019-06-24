5be2cb94c91741f1bf731f7f0a74c69e31f3805c
A C SREEDHAR REDDY <acreddysreedharreddy@gmail.com>
2019-06-24 15:30:00 +0530

    set connection timeout of a http client.

--- libraries/HTTPClient/examples/BasicHttpClient/BasicHttpClient.ino
+++ libraries/HTTPClient/examples/BasicHttpClient/BasicHttpClient.ino
@@ -70,7 +70,8 @@ void loop() {
     if((wifiMulti.run() == WL_CONNECTED)) {
 
         HTTPClient http;
-
+        // Set connection Timeout seconds
+        //http.setConnectTimeout(10)
         USE_SERIAL.print("[HTTP] begin...\n");
         // configure traged server and url
         //http.begin("https://www.howsmyssl.com/a/check", ca); //HTTPS
