6aba3a8920e00505800620fdc180fdf5db2424e5
me-no-dev <hristo@espressif.com>
2019-08-20 00:08:17 +0300

    split build steps

--- tools/build-tests.sh
+++ tools/build-tests.sh
@@ -1,56 +1,31 @@
 #!/bin/bash
 
-#- set -e
-
 if [ ! -z "$TRAVIS_TAG" ]; then
 	echo "No sketch builds & tests required for tagged TravisCI builds, exiting"
 	exit 0
 fi
 
 echo -e "travis_fold:start:sketch_test_env_prepare"
-pip install pyserial
-wget -O arduino.tar.xz https://www.arduino.cc/download.php?f=/arduino-nightly-linux64.tar.xz
-tar xf arduino.tar.xz
-mv arduino-nightly $HOME/arduino_ide
-mkdir -p $HOME/Arduino/libraries
-cd $HOME/arduino_ide/hardware
-mkdir espressif
-cd espressif
-ln -s $TRAVIS_BUILD_DIR esp32
-cd esp32
-git submodule update --init --recursive
-cd tools
-python get.py
+tools/prep-arduino-ide.sh
 echo -e "travis_fold:end:sketch_test_env_prepare"
 
 echo -e "travis_fold:start:platformio_test_env_prepare"
-pip install -U https://github.com/platformio/platformio/archive/develop.zip && \
-python -m platformio platform install https://github.com/platformio/platform-espressif32.git#feature/stage && \
-sed -i 's/https:\/\/github\.com\/espressif\/arduino-esp32\.git/*/' ~/.platformio/platforms/espressif32/platform.json && \
-ln -s $TRAVIS_BUILD_DIR ~/.platformio/packages/framework-arduinoespressif32
+tools/prep-platformio.sh
 if [ $? -ne 0 ]; then exit 1; fi
 echo -e "travis_fold:end:platformio_test_env_prepare"
 
-echo "PATH=$PATH"
 cd $TRAVIS_BUILD_DIR
 
-# echo -e "travis_fold:start:sketch_test"
-# export PATH="$HOME/arduino_ide:$TRAVIS_BUILD_DIR/tools/xtensa-esp32-elf/bin:$PATH"
-# source tools/common.sh
-# build_sketches $HOME/arduino_ide $TRAVIS_BUILD_DIR/libraries "-l $HOME/Arduino/libraries"
-# if [ $? -ne 0 ]; then exit 1; fi
-# echo -e "travis_fold:end:sketch_test"
+echo -e "travis_fold:start:sketch_test"
+tools/test-arduino-ide.sh
+if [ $? -ne 0 ]; then exit 1; fi
+echo -e "travis_fold:end:sketch_test"
 
-# echo -e "travis_fold:start:size_report"
-# cat size.log
-# echo -e "travis_fold:end:size_report"
+echo -e "travis_fold:start:size_report"
+cat size.log
+echo -e "travis_fold:end:size_report"
 
 echo -e "travis_fold:start:platformio_test"
-python -m platformio ci  --board esp32dev libraries/WiFi/examples/WiFiClient && \
-python -m platformio ci  --board esp32dev libraries/WiFiClientSecure/examples/WiFiClientSecure && \
-python -m platformio ci  --board esp32dev libraries/BluetoothSerial/examples/SerialToSerialBT && \
-python -m platformio ci  --board esp32dev libraries/BLE/examples/BLE_server && \
-python -m platformio ci  --board esp32dev libraries/AzureIoT/examples/GetStarted && \
-python -m platformio ci  --board esp32dev libraries/ESP32/examples/Camera/CameraWebServer --project-option="board_build.partitions = huge_app.csv"
+tools/test-platformio.sh
 if [ $? -ne 0 ]; then exit 1; fi
 echo -e "travis_fold:end:platformio_test"
---
+++ tools/test-arduino-ide.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export PATH="$HOME/arduino_ide:$TRAVIS_BUILD_DIR/tools/xtensa-esp32-elf/bin:$PATH"
+source tools/common.sh
+build_sketches $HOME/arduino_ide $TRAVIS_BUILD_DIR/libraries "-l $HOME/Arduino/libraries"
+if [ $? -ne 0 ]; then exit 1; fi
