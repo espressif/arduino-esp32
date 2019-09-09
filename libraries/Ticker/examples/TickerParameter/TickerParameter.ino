25f2eeac42de3664c70d684ed2ca1bb515c8b443
Dirk O. Kaar <dok@dok-net.net>
2019-09-09 14:32:40 +0200

    Test case same as ESP8266

--- libraries/Ticker/examples/TickerParameter/TickerParameter.ino
+++ libraries/Ticker/examples/TickerParameter/TickerParameter.ino
@@ -24,15 +24,19 @@ void setPin(int state) {
   digitalWrite(LED_BUILTIN, state);
 }
 
+void setPinChar(char state) {
+  digitalWrite(LED_BUILTIN, state);
+}
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(1, LOW);
 
   // every 25 ms, call setPin(0)
   tickerSetLow.attach_ms(25, setPin, 0);
 
-  // every 26 ms, call setPin(1)
-  tickerSetHigh.attach_ms(26, setPin, 1);
+  // every 26 ms, call setPinChar(1)
+  tickerSetHigh.attach_ms(26, setPinChar, (char)1);
+
 }
 
 void loop() {
