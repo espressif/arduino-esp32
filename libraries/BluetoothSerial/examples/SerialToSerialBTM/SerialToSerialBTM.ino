9eb5e3a147de0353edb52079829fd0b359254b21
Victor Tchistiak <vtchistiak@boomi.com>
2019-09-16 22:25:14 -0400

    20190916 - initial: Force another build

--- libraries/BluetoothSerial/examples/SerialToSerialBTM/SerialToSerialBTM.ino
+++ libraries/BluetoothSerial/examples/SerialToSerialBTM/SerialToSerialBTM.ino
@@ -12,8 +12,8 @@
 
 BluetoothSerial SerialBT;
 
-uint8_t address[6]  = {0xAA, 0xBB, 0xCC, 0x11, 0x22, 0x33};
 String MACadd = "AA:BB:CC:11:22:33";
+uint8_t address[6]  = {0xAA, 0xBB, 0xCC, 0x11, 0x22, 0x33};
 String name = "OBDII";
 char *pin = "1234"; //<- standard pin would be provided by default
 
