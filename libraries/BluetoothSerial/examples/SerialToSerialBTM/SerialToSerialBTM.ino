1b36dad0655d3308ec7ba03dbb4d8289cbce4c74
Victor Tchistiak <vtchistiak@boomi.com>
2019-09-19 11:11:11 -0400

    20190916 - update example comments

--- libraries/BluetoothSerial/examples/SerialToSerialBTM/SerialToSerialBTM.ino
+++ libraries/BluetoothSerial/examples/SerialToSerialBTM/SerialToSerialBTM.ino
@@ -8,10 +8,6 @@
 
 #include "BluetoothSerial.h"
 
-#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
-#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
-#endif
-
 BluetoothSerial SerialBT;
 
 String MACadd = "AA:BB:CC:11:22:33";
