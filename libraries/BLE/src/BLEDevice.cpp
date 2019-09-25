b2d88d57a906ce1ad973c118448c23c773a912ea
billprozac <billprozac@gmail.com>
2019-09-25 01:05:54 -0400

    Update BLEDevice.cpp
    Pretty sure this was a typo as the deinit doesn't actually allow for reinit if that is an ifndef.  Changed in my local copy and can now deinit and reinit just fine.  Also, not sure why we are checking for the architecture here.  Just curious.

--- libraries/BLE/src/BLEDevice.cpp
+++ libraries/BLE/src/BLEDevice.cpp
@@ -625,7 +625,7 @@ void BLEDevice::removePeerDevice(uint16_t conn_id, bool _client) {
     esp_bluedroid_deinit();
     esp_bt_controller_disable();
     esp_bt_controller_deinit();
-#ifndef ARDUINO_ARCH_ESP32
+#ifdef ARDUINO_ARCH_ESP32
     if (release_memory) {
         esp_bt_controller_mem_release(ESP_BT_MODE_BTDM);  // <-- require tests because we released classic BT memory and this can cause crash (most likely not, esp-idf takes care of it)
     } else {
