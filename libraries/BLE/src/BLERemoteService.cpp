7d1e85099ff21d3269e2ae82d7d0bd8dc79195b8
root <akiraneko@gmail.com>
2019-09-30 10:02:07 +0900

    Delete log output

--- libraries/BLE/src/BLERemoteService.cpp
+++ libraries/BLE/src/BLERemoteService.cpp
@@ -231,14 +231,12 @@ std::map<std::string, BLERemoteCharacteristic*>* BLERemoteService::getCharacteri
  * @return A map of all the characteristics of this service.
  */
 std::map<uint16_t, BLERemoteCharacteristic*>* BLERemoteService::getCharacteristicsByHandle() {
-	log_v(">> getCharacteristicsByHandle() for service: %s", getUUID().toString().c_str());
 	// If is possible that we have not read the characteristics associated with the service so do that
 	// now.  The request to retrieve the characteristics by calling "retrieveCharacteristics" is a blocking
 	// call and does not return until all the characteristics are available.
 	if (!m_haveCharacteristics) {
 		retrieveCharacteristics();
 	}
-	log_v("<< getCharacteristicsByHandle() for service: %s", getUUID().toString().c_str());
 	return &m_characteristicMapByHandle;
 } // getCharacteristicsByHandle
 
