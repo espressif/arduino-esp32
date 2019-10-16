3a1acf91e1c0ffa1c9a9c3aee1b085b4dfe74a73
Mjohncecil <michaeljohn.cecil@calbaptist.edu>
2019-10-16 16:09:24 -0700

    updated sht31 code for readability.

--- ESP_SHT3x/ESP_SHT3x.ino
+++ ESP_SHT3x/ESP_SHT3x.ino
@@ -3,46 +3,41 @@
 #include "Adafruit_SHT31.h"
  
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
+
  
- 
-void setup() 
-{
+void setup() {
   Serial.begin(74880);
-  if (! sht31.begin(0x44)) 
-  {
+
+  // initializes sht31
+  if (! sht31.begin(0x44)) {
     Serial.println("Couldn't find SHT31");
-    while (1) delay(1);
-  }else {
+  } else {
     Serial.println("SHT31 Found");
-  }
+  }  
 }
- 
- 
-void loop() 
-{
-  float t = sht31.readTemperature();
-  float h = sht31.readHumidity();
- 
-  if (! isnan(t)) 
-  {
-    Serial.print("Temp *C = "); Serial.println(t);
-  } 
-  else 
-  { 
+
+// Outputs measured temperature and humidity to serial monitor
+void printShtMeasurments(float temp, float hum) {
+   if (! isnan(temp)) {
+    Serial.print("Temp *C = "); Serial.println(temp);
+  } else { 
     Serial.println("Failed to read temperature");
   }
  
-  if (! isnan(h)) 
-  {
-    Serial.print("Hum. % = "); Serial.println(h);
-  } 
-  else 
-  { 
+  if (! isnan(hum)) {
+    Serial.print("Hum. % = "); Serial.println(hum);
+  } else { 
     Serial.println("Failed to read humidity");
   }
-  if(h > 60) {
-    Serial.println("H high");
-  }
+}
+ 
+void loop() {
+  float t = sht31.readTemperature();
+  float h = sht31.readHumidity();
+
+  printShtMeasurments(t, h);
+  
   Serial.println();
+  
   delay(1000);
 }
