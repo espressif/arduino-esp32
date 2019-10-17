45947de0fc19186dd210f4605f962fb35870b485
Mjohncecil <michaeljohn.cecil@calbaptist.edu>
2019-10-17 10:05:54 -0700

    Seperated sht initialization to its own method, which is called every loop. (got rid of old, comented out code.)

--- ESP_SHT3x/ESP_SHT3x.ino
+++ ESP_SHT3x/ESP_SHT3x.ino
@@ -7,18 +7,10 @@ Adafruit_SHT31 sht31 = Adafruit_SHT31();
  
 void setup() {
   Serial.begin(74880);
-  /*
-  // initializes sht31
-  if (! sht31.begin(0x44)) {
-    Serial.println("Couldn't find SHT31");
-  } else {
-    Serial.println("SHT31 Found");
-  }
-  */
 }
 
+// initializes sht31
 void shtInit() {
-  // initializes sht31
   if (! sht31.begin(0x44)) {
     Serial.println("Couldn't find SHT31");
   } else {
