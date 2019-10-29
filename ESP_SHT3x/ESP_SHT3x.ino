b242d993f826bbfdaa503306cc160575f420cf7b
Mjohncecil <michaeljohn.cecil@calbaptist.edu>
2019-10-29 13:42:13 -0700

    Adds a timer using epoch time from a NTP client.

--- ESP_SHT3x/ESP_SHT3x.ino
+++ ESP_SHT3x/ESP_SHT3x.ino
@@ -1,21 +1,120 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 #include <Arduino.h>
 #include <Wire.h>
+#include <DallasTemperature.h>
 #include "Adafruit_SHT31.h"
+#include <NTPClient.h>
+
+//const char* ssid = "TP-Link_4346";
+//const char* password = "84850034";
+
+const char* ssid = "inHouseTest1";
+const char* password = "nasturtium";
+
+// Enables ntp server for time and date
+WiFiUDP ntpUDP;
+
+NTPClient timeClient(ntpUDP);
+
+int currentTime = 0;
  
 Adafruit_SHT31 sht31 = Adafruit_SHT31();
 
+// The pins which will be connected to the 2 relay
+int humidPin = 2;
+int tempPin = 14;
+
+// GPIO where the DS18B20 is connected to
+const int oneWireBus = 0;     
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensors(&oneWire);
+
+// the temperature (Cecius) and humidity (%) threshholds to trigger relay
+float tempLimit = 22.78;
+float humidLimit = 97.0;
+
+// variables to influence clock controll behavior
+// records time of last change in pin voltage
+int humLastTime = 0;
+int temLastTime = 0;
+// {timeOn, timeOff} in seconds
+int humTimeDuration[2] = {10, 20};
+int temTimeDuration[2] = {5, 15};
  
 void setup() {
-  Serial.begin(74880);
+  Serial.begin(115200);
+  // Enables DS18B20
+  sensors.begin();
+
+  // Sets the digital pins as output
+  pinMode(humidPin, OUTPUT);
+  pinMode(tempPin, OUTPUT);
+
+  // Initializes current epoch time.
+  timeClient.begin();
+  currentTime = timeClient.getEpochTime();
+
+  OTAinit();
 }
 
-// initializes sht31
-void shtInit() {
-  if (! sht31.begin(0x44)) {
-    Serial.println("Couldn't find SHT31");
-  } else {
-    Serial.println("SHT31 Found");
-  }  
+// Initializes OTA functionality for the 8266
+void OTAinit() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    switch (error) {
+      case OTA_AUTH_ERROR:
+        Serial.println("Auth Failed");
+        break;
+      case OTA_BEGIN_ERROR:
+        Serial.println("Begin Failed");
+        break;
+      case OTA_CONNECT_ERROR:
+        Serial.println("Connect Failed");
+        break;
+      case OTA_RECEIVE_ERROR:
+        Serial.println("Receive Failed");
+        break;
+      case OTA_END_ERROR:
+        Serial.println("End Failed");
+        break;
+    }
+  });
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 }
 
 // Outputs measured temperature and humidity to serial monitor
@@ -32,13 +131,76 @@ void printShtMeasurments(float temp, float hum) {
     Serial.println("Failed to read humidity");
   }
 }
- 
-void loop() {
-  shtInit();
+
+// Sets relay pins to high or low depending on measurements
+void relayPower(int pin, float current, float limit, int *lastTime) {
+  int oldPinPow = digitalRead(pin);
+  
+  if(current < limit) {
+    digitalWrite(pin, LOW);
+  } else {
+    digitalWrite(pin, HIGH);
+  }
+
+  if(oldPinPow != digitalRead(pin)) {
+    *lastTime = currentTime;
+  }
+}
+
+void shtAlgorithm() {
+  // Reads temperature and humidity from sht31 and stores it to appropriate variables
   float t = sht31.readTemperature();
   float h = sht31.readHumidity();
-
+  // Displays current readings
   printShtMeasurments(t, h);
+
+  // Sets relay relevent pins to appropriate voltage levels
+  relayPower(humidPin, h, humidLimit, &humLastTime);
+  relayPower(tempPin, t, tempLimit, &temLastTime);
+}
+
+void DS18B20Algorithm() {
+  sensors.requestTemperatures(); 
+  float temperatureC = sensors.getTempCByIndex(0);
+
+  if(-127 < temperatureC && temperatureC < 85) {
+    relayPower(tempPin, temperatureC, tempLimit, &temLastTime);
+    Serial.print(temperatureC);
+    Serial.println("ºC");
+  }else {
+    clockManager(tempPin, temTimeDuration, &temLastTime);
+  }
+  clockManager(humidPin, humTimeDuration, &humLastTime);
+}
+
+void clockManager(int pin, int times[], int *lastTime) {
+  if((!digitalRead(pin) && (*lastTime + times[0] <= currentTime)) || (digitalRead(pin) && (*lastTime + times[1] <= currentTime))) {
+    digitalWrite(pin, !digitalRead(pin));
+    *lastTime = currentTime;
+  }
+}
+ 
+void loop() {
+  // Updates and records time to current.
+  timeClient.update();
+  currentTime = timeClient.getEpochTime();
+  
+  if(sht31.begin(0x44)) {
+    shtAlgorithm();
+  } else {
+    DS18B20Algorithm();
+  }
+
+  // Checks for FOTA update
+  ArduinoOTA.handle();
+
+  Serial.println();
+
+  Serial.println(currentTime);
+  Serial.print("Humidity: ");
+  Serial.println(digitalRead(humidPin));
+  Serial.print("Temperature: ");
+  Serial.println(digitalRead(tempPin));
   
   Serial.println();
   
