c8131bd4cc0aa7ae2688339ce29ce13e28b2dca2
Chris Liebman <chris.l@taboola.com>
2020-01-24 17:37:48 -0800

    fix unused parameter warning

--- libraries/ESP32/examples/ESPNow/Basic/Slave/Slave.ino
+++ libraries/ESP32/examples/ESPNow/Basic/Slave/Slave.ino
@@ -78,6 +78,7 @@ void setup() {
 
 // callback when data is recv from Master
 void OnDataRecv(const uint8_t *mac_addr, const uint8_t *data, int data_len) {
+  (void)data_len; // unused
   char macStr[18];
   snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
            mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
