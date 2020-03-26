4aabaa61db062d24a0f8f1ff7ce637586dfe1452
Martijn Scheepers <ms@SDNengineering.nl>
2020-03-26 11:08:01 +0100

    Read efuse for chipmodel

--- cores/esp32/Esp.cpp
+++ cores/esp32/Esp.cpp
@@ -220,18 +220,18 @@ uint8_t EspClass::getChipRevision(void)
 
 const char * EspClass::getChipModel(void)
 {
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    switch (chip_info.model) {
-        case 0:
-            return "ESP32D0WDQ6";
-        case 1:
-            return "ESP32D0WDQ5";
-        case 2:
-            return "ESP32D2WDQ5";
-        case 4:
+    uint32_t chip_ver = REG_GET_FIELD(EFUSE_BLK0_RDATA3_REG, EFUSE_RD_CHIP_VER_PKG);
+    uint32_t pkg_ver = chip_ver & 0x7;
+    switch (pkg_ver) {
+        case EFUSE_RD_CHIP_VER_PKG_ESP32D0WDQ6 :
+            return "ESP32-D0WDQ6";
+        case EFUSE_RD_CHIP_VER_PKG_ESP32D0WDQ5 :
+            return "ESP32-D0WDQ5";
+        case EFUSE_RD_CHIP_VER_PKG_ESP32D2WDQ5 :
+            return "ESP32-D2WDQ5";
+        case EFUSE_RD_CHIP_VER_PKG_ESP32PICOD2 :
             return "ESP32-PICO-D2";
-        case 5:
+        case EFUSE_RD_CHIP_VER_PKG_ESP32PICOD4 :
             return "ESP32-PICO-D4";
         default:
             return "Unknown";
