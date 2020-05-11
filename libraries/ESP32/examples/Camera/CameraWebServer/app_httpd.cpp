3735cfe548459d17cc2c2d5be5f2883acedebf98
me-no-dev <hristo@espressif.com>
2020-05-12 00:07:11 +0300

    Update app_httpd.cpp

--- libraries/ESP32/examples/Camera/CameraWebServer/app_httpd.cpp
+++ libraries/ESP32/examples/Camera/CameraWebServer/app_httpd.cpp
@@ -11,6 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+#if 0
 #include "esp_http_server.h"
 #include "esp_timer.h"
 #include "esp_camera.h"
@@ -660,3 +661,4 @@ void startCameraServer(){
         httpd_register_uri_handler(stream_httpd, &stream_uri);
     }
 }
+#endif
