f7a9e3e4021e2fb2535883663ee55494bcaf9f27
sweetymhaiske <sweetymhaiske@gmail.com>
2020-07-13 19:56:19 +0530

    Fixes for 1)When device is already provisioned 2)space issue w.r.t softAP

--- libraries/WiFi/src/WiFiProv.h
+++ libraries/WiFi/src/WiFiProv.h
@@ -1,4 +1,4 @@
-/*
+*
     WiFiProv.h - Base class for provisioning support
     All right reserved.
  
@@ -16,19 +16,21 @@
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
-
-#include "wifi_provisioning/manager.h"
-#include "wifi_provisioning/scheme_ble.h"
-#include "esp_wifi.h"
-#include "nvs_flash.h"
+#include <wifi_provisioning/scheme_ble.h>
+#include <wifi_provisioning/manager.h>
+#include <nvs_flash.h>
 #include "SimpleBLE.h"
+
 //Select the scheme using which you want to provision
-enum scheme
+enum scheme_t
 {
     WIFI_PROV_SCHEME_BLE,
-    WIFI_PROV_SCHEME_SOFTAP,
-    WIFI_PROV_SCHEME_CONSOLE
+    WIFI_PROV_SCHEME_SOFTAP
 };
+
+extern void provSchemeSoftAP();
+extern void provSchemeBLE();
+
 //Provisioning class 
 class WiFiProvClass
 {
@@ -42,7 +44,7 @@ class WiFiProvClass
 
         bool isProvEnabled();
     
-        void beginProvision(scheme prov_scheme = WIFI_PROV_SCHEME_SOFTAP, wifi_prov_event_handler_t scheme_event_handler = WIFI_PROV_EVENT_HANDLER_NONE, wifi_prov_security_t security = WIFI_PROV_SECURITY_1, const char * pop = "abcd1234", const char * service_name = NULL, const char * service_key = NULL, uint8_t *uuid = NULL);
+        void beginProvision(void (*schemecb)() = provSchemeSoftAP, wifi_prov_event_handler_t scheme_event_handler = WIFI_PROV_EVENT_HANDLER_NONE, wifi_prov_security_t security = WIFI_PROV_SECURITY_1, const char * pop = "abcd1234", const char * service_name = NULL, const char * service_key = NULL, uint8_t *uuid = NULL);
 };
 
 /*
@@ -52,4 +54,4 @@ class WiFiProvClass
       - WIFI_PROV_SCHEME_BLE_EVENT_HANDLER_FREE_BT
 Event Handler for SOFTAP
       - WIFI_PROV_EVENT_HANDLER_NONE
-*/
+*/	
