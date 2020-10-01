82670b96f88e25e21dbeb09f7ee0dd9ab4f074d9
Jens Hauke <jens@4k2.de>
2020-10-01 14:43:30 +0200

    Fix for missed scan response data (BLEScan). (#4358)
    This is a fix for missing scan responses after a first successfull scan.
    
    While running the BLE_scan.ino sketch with wantDuplicates=false, i got
    only one result with correct advertising and scan response length (31,26):
    
    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks(), false);
    pBLEScan->start(scanTime, false);
    ...
    [W][BLEScan.cpp:109] handleGAPEvent(): bytes length: 31 + 26, addr type: 1
    
    All following calls to start() just returned the advertising data without
    scan response data:
    
    pBLEScan->start(scanTime, false);
    [W][BLEScan.cpp:109] handleGAPEvent(): bytes length: 31 + 0, addr type: 1
    
    With "wantDuplicates=true" i got:
    
    pBLEScan->setAdvertisedDeviceCallbacks(new MyAdvertisedDeviceCallbacks(), true);
    pBLEScan->start(scanTime, false);
    [W][BLEScan.cpp:109] handleGAPEvent(): bytes length: 31 + 26, addr type: 1
    [W][BLEScan.cpp:109] handleGAPEvent(): bytes length: 31 + 26, addr type: 1
    [W][BLEScan.cpp:73] handleGAPEvent(): ESP_GAP_SEARCH_INQ_CMPL_EVT
    Devices found: 1
    Scan done!
    pBLEScan->start(scanTime, false);
    [W][BLEScan.cpp:109] handleGAPEvent(): bytes length: 31 + 0, addr type: 1
    [W][BLEScan.cpp:109] handleGAPEvent(): bytes length: 0 + 26, addr type: 1
    [W][BLEScan.cpp:109] handleGAPEvent(): bytes length: 31 + 0, addr type: 1
    [W][BLEScan.cpp:109] handleGAPEvent(): bytes length: 0 + 26, addr type: 1
    
    Explicitly initializing m_scan_params.scan_duplicate of BLEScan solves
    this issue (In my case the un-initialized value was
    m_scan_params.scan_duplicate == 1073599044).
    
    Co-authored-by: Me No Dev <me-no-dev@users.noreply.github.com>

--- libraries/BLE/src/BLEScan.cpp
+++ libraries/BLE/src/BLEScan.cpp
@@ -22,6 +22,7 @@
  * Constructor
  */
 BLEScan::BLEScan() {
+	memset(&m_scan_params, 0, sizeof(m_scan_params)); // Initialize all params
 	m_scan_params.scan_type          = BLE_SCAN_TYPE_PASSIVE; // Default is a passive scan.
 	m_scan_params.own_addr_type      = BLE_ADDR_TYPE_PUBLIC;
 	m_scan_params.scan_filter_policy = BLE_SCAN_FILTER_ALLOW_ALL;
