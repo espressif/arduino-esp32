8816bb5505b19d91a9159a62e6e772610904c9ef
lbernstone <lbernstone@gmail.com>
2020-11-10 11:50:35 -0700

    Added #define LED_BUILTIN to all pins_arduino.h that need it. (#4520)

--- variants/d32_pro/pins_arduino.h
+++ variants/d32_pro/pins_arduino.h
@@ -6,6 +6,7 @@
 
 static const uint8_t LED_BUILTIN = 5;
 #define BUILTIN_LED  LED_BUILTIN // backward compatibility
+#define LED_BUILTIN LED_BUILTIN
 static const uint8_t _VBAT = 35; // battery voltage
 
 
