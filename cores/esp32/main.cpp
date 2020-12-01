e0d31a23d25209b59fd568d221f0225ad59e4ee2
coylen <neil@netaccess.me.uk>
2020-12-01 15:08:03 +0000

    Changed variable name
    changed variable name to CONFIG_ARDUINO_LOOP_STACK_SIZE as requested by @me-no-dev

--- cores/esp32/main.cpp
+++ cores/esp32/main.cpp
@@ -3,8 +3,8 @@
 #include "esp_task_wdt.h"
 #include "Arduino.h"
 
-#ifndef LOOP_STACK_SIZE
-#define LOOP_STACK_SIZE 8192
+#ifndef CONFIG_ARDUINO_LOOP_STACK_SIZE
+#define CONFIG_ARDUINO_LOOP_STACK_SIZE 8192
 #endif
 
 TaskHandle_t loopTaskHandle = NULL;
@@ -29,7 +29,7 @@ extern "C" void app_main()
 {
     loopTaskWDTEnabled = false;
     initArduino();
-    xTaskCreateUniversal(loopTask, "loopTask", LOOP_STACK_SIZE, NULL, 1, &loopTaskHandle, CONFIG_ARDUINO_RUNNING_CORE);
+    xTaskCreateUniversal(loopTask, "loopTask", CONFIG_ARDUINO_LOOP_STACK_SIZE, NULL, 1, &loopTaskHandle, CONFIG_ARDUINO_RUNNING_CORE);
 }
 
 #endif
