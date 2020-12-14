6dd73b062ccd51ea41f2d7856fe610e9993d0a46
Richard Li <richard.li@ces.hk>
2020-12-14 22:50:31 +0800

    Updated example comments.

--- libraries/BluetoothSerial/examples/SerialToSerialBT_SSP_pairing/SerialToSerialBT_SSP_pairing.ino
+++ libraries/BluetoothSerial/examples/SerialToSerialBT_SSP_pairing/SerialToSerialBT_SSP_pairing.ino
@@ -1,7 +1,7 @@
 //This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Evandro Copercini - 2018
+//By Richard Li 2020
 //
-//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//This example creates a bridge between Serial and Classical Bluetooth (SPP with authentication)
 //and also demonstrate that SerialBT have the same functionalities of a normal Serial
 
 #include "BluetoothSerial.h"
