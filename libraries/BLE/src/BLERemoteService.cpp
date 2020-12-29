5978ba321303e9b67e00aff2b9dc2094bab0b58c
Verkehrsrot <cyberman@obh.snafu.de>
2020-12-29 14:39:16 +0100

    Sanitize compiler warning in BLERemoteService.cpp (issue #4660)
    Compiler complains on unused parameter pCharacteristicMap:
    
    framework-arduinoespressif32\libraries\BLE\src\BLERemoteService.cpp: In member function 'void BLERemoteService::getCharacteristics(std::map<short unsigned int, BLERemoteCharacteristic*>*)':
    \framework-arduinoespressif32\libraries\BLE\src\BLERemoteService.cpp:246:89: warning: parameter 'pCharacteristicMap' set but not used [-Wunused-but-set-parameter]
     void BLERemoteService::getCharacteristics(std::map<uint16_t, BLERemoteCharacteristic*>* pCharacteristicMap) {

--- libraries/BLE/src/BLERemoteService.cpp
+++ libraries/BLE/src/BLERemoteService.cpp
@@ -245,6 +245,7 @@ std::map<uint16_t, BLERemoteCharacteristic*>* BLERemoteService::getCharacteristi
  */
 void BLERemoteService::getCharacteristics(std::map<uint16_t, BLERemoteCharacteristic*>* pCharacteristicMap) {
 	log_v(">> getCharacteristics() for service: %s", getUUID().toString().c_str());
+	(void)pCharacteristicMap;
 	// If is possible that we have not read the characteristics associated with the service so do that
 	// now.  The request to retrieve the characteristics by calling "retrieveCharacteristics" is a blocking
 	// call and does not return until all the characteristics are available.
