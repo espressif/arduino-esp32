3bfadf8e3134f9fffd766301b3c9be1979c18154
Tim Koers <tim.koers@live.nl>
2021-01-12 17:01:35 +0100

    Changed example to buffer

--- libraries/ESP32/examples/Serial/Interrupt/Interrupt.ino
+++ libraries/ESP32/examples/Serial/Interrupt/Interrupt.ino
@@ -1,11 +1,18 @@
-// Serial, Serial1 and Serial2 are all of the type HardwareSerial.
+#define BUFFER_SIZE 8
 
-static void IRAM_ATTR onSerialRX(uint8_t c, void* user_arg){  
-	Serial.print(c);
+static volatile char inputBuffer[BUFFER_SIZE];
+static volatile size_t inputBufferLength = 0;
+
+// Please keep in mind, since the ESP32 is dual core, 
+// the interrupt will be running on the same core as the setRxInterrupt function was called on.
+static void IRAM_ATTR onSerialRX(uint8_t character, void* user_arg){
 	
-	// Cast the user_arg containing a void* to the Serial device, to HardwareSerial* to be used
-	HardwareSerial* serial = (HardwareSerial*)user_arg;
-	serial->print(c);
+	// Cast the user_arg back to a array
+	char[] buffer = (char*)user_arg;
+	
+	if(inputBufferLength < BUFFER_SIZE){
+		buffer[inputBufferLength++] = (char)character;
+	}
 }
 
 void setup()
@@ -14,11 +21,22 @@ void setup()
   Serial2.begin(115200);
   
   // The user_arg is expected to be a void pointer (void*), 
-  // so take the reference of Serial2, and cast the HardwareSerial* to a void*
-  Serial2.setRxInterrupt(onSerialRX, (void*)&Serial2);  
+  // so take the reference of the variable, and it to a void*
+  Serial2.setRxInterrupt(onSerialRX, (void*)&inputBuffer);
 }
 
 void loop()
-{
-  delay(1);
+{ 
+  if(inputBufferLength == (BUFFER_SIZE - 1)){
+	for(size_t i = 0; i < inputBufferLength; i++){
+	  Serial.write(inputBuffer[i]);
+	  
+	  // Clear the buffer
+	  inputBuffer[i] = '\0';
+	}
+	// Clear the bufferLength
+	inputBufferLength = 0;
+  }
+  
+  delay(1000); // Wait for one second
 }
