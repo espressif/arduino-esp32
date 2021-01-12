f33c3322f34c898b38b7924e92f27aeb80ac013b
Tim Koers <tim.koers@live.nl>
2021-01-12 19:47:50 +0100

    Fixed ISR concurrency problems in example

--- libraries/ESP32/examples/Serial/Interrupt/Interrupt.ino
+++ libraries/ESP32/examples/Serial/Interrupt/Interrupt.ino
@@ -1,5 +1,8 @@
 #define BUFFER_SIZE 8
 
+// This semaphore is here to handle the interruption of the loop when the interrupt is running.
+SemaphoreHandle_t bufferSemaphore;
+
 static volatile char inputBuffer[BUFFER_SIZE];
 static volatile size_t inputBufferLength = 0;
 
@@ -10,13 +13,20 @@ static void IRAM_ATTR onSerialRX(uint8_t character, void* user_arg){
 	// Cast the user_arg back to a array
 	char* buffer = (char*)user_arg;
 	
-	if(inputBufferLength < BUFFER_SIZE){
-		buffer[inputBufferLength++] = (char)character;
+	BaseType_t xHighPriorityTaskWoken;
+	
+	if(xSemaphoreTakeFromISR(bufferSemaphore, &xHighPriorityTaskWoken) == pdTRUE){	
+		if(inputBufferLength < BUFFER_SIZE){
+			buffer[inputBufferLength++] = (char)character;
+		}
+		xSemaphoreGiveFromISR(bufferSemaphore, &xHighPriorityTaskWoken);
 	}
 }
 
 void setup()
 {
+  bufferSemaphore = xSemaphoreCreateBinary();
+	
   Serial.begin(115200);
   Serial2.begin(115200);
   
@@ -27,7 +37,7 @@ void setup()
 
 void loop()
 { 
-  if(inputBufferLength == (BUFFER_SIZE - 1)){
+  if(xSemaphoreTake(bufferSemaphore, portMAX_DELAY) == pdTRUE && inputBufferLength == (BUFFER_SIZE - 1)){
 	for(size_t i = 0; i < inputBufferLength; i++){
 	  Serial.write(inputBuffer[i]);
 	  
@@ -36,6 +46,8 @@ void loop()
 	}
 	// Clear the bufferLength
 	inputBufferLength = 0;
+	
+	xSemaphoreGive(bufferSemaphore);
   }
   
   delay(1000); // Wait for one second
