e3298eae2d518f28ab4bb5258f2f1a3a8408ac57
Tim Koers <tim.koers@live.nl>
2021-01-12 15:01:15 +0100

    Changed serials to Serial and Serial2

--- libraries/ESP32/examples/Serial/Interrupt/Interrupt.ino
+++ libraries/ESP32/examples/Serial/Interrupt/Interrupt.ino
@@ -1,8 +1,7 @@
-HardwareSerial hwSerial(0);
-HardwareSerial hwSerial2(2);
+// Serial, Serial1 and Serial2 are all of the type HardwareSerial.
 
 static void IRAM_ATTR onSerialRX(uint8_t c, void* user_arg){  
-	hwSerial.print(c);
+	Serial.print(c);
 	
 	// Cast the user_arg containing a void* to the Serial device, to HardwareSerial* to be used
 	HardwareSerial* serial = (HardwareSerial*)user_arg;
@@ -11,12 +10,12 @@ static void IRAM_ATTR onSerialRX(uint8_t c, void* user_arg){
 
 void setup()
 {
-  hwSerial.begin(115200);
-  hwSerial2.begin(115200);
+  Serial.begin(115200);
+  Serial2.begin(115200);
   
   // The user_arg is expected to be a void pointer (void*), 
-  // so take the reference of hwSerial2, and cast the HardwareSerial* to a void*
-  hwSerial2.setRxInterrupt(onSerialRX, (void*)&hwSerial2);  
+  // so take the reference of Serial2, and cast the HardwareSerial* to a void*
+  Serial2.setRxInterrupt(onSerialRX, (void*)&Serial2);  
 }
 
 void loop()
