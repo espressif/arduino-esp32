6d7e8ebbd439922424c8fb7514d5439753824a3d
Tim Koers <tim.koers@live.nl>
2021-01-12 14:59:37 +0100

    Cleaned up and commented the Interrupt example

--- libraries/ESP32/examples/Serial/Interrupt/Interrupt.ino
+++ libraries/ESP32/examples/Serial/Interrupt/Interrupt.ino
@@ -1,17 +1,21 @@
 HardwareSerial hwSerial(0);
 HardwareSerial hwSerial2(2);
 
-bool ok = true;
-
 static void IRAM_ATTR onSerialRX(uint8_t c, void* user_arg){  
 	hwSerial.print(c);
-	((HardwareSerial*)user_arg)->print(c);
+	
+	// Cast the user_arg containing a void* to the Serial device, to HardwareSerial* to be used
+	HardwareSerial* serial = (HardwareSerial*)user_arg;
+	serial->print(c);
 }
 
 void setup()
 {
   hwSerial.begin(115200);
   hwSerial2.begin(115200);
+  
+  // The user_arg is expected to be a void pointer (void*), 
+  // so take the reference of hwSerial2, and cast the HardwareSerial* to a void*
   hwSerial2.setRxInterrupt(onSerialRX, (void*)&hwSerial2);  
 }
 
