1a52202d3bc4a0dd9702597b528d5dbb86d0f192
ketri-kjy <jinyoung@ketri.re.kr>
2021-01-27 08:40:26 +0900

    etboard pinmap upload

---
+++ variants/ET_Board/pins_arduino.h
@@ -0,0 +1,20 @@
+// #include "pinmap.h"
+
+#define A0 36   // Potentiometer
+#define A1 39   // CDS
+#define A2 32   // temp
+#define A3 33   // InPut
+#define A4 34   // InPut
+#define A5 35   // InPut
+#define A6 25   // InPut
+#define A7 26   // InPut
+
+
+#define D2 27   // LED_Red
+#define D3 14   // LED_Blue
+#define D4 12   // LED_Green
+#define D5 13   // LED_Yellow
+#define D6 15   // BUTTON_Red
+#define D7 16   // BUTTON_Blue
+#define D8 17   // BUTTON_Green
+#define D9 4    // BUTTON_Yellow
