a299ddc99e79d473caefe04116daef31c0520936
me-no-dev <hristo@espressif.com>
2021-03-18 15:02:37 +0200

    Change send_ssl_data to use size_t instead of uint16_t
    Fixes: https://github.com/espressif/arduino-esp32/issues/4960

--- libraries/WiFiClientSecure/src/ssl_client.h
+++ libraries/WiFiClientSecure/src/ssl_client.h
@@ -32,7 +32,7 @@ void ssl_init(sslclient_context *ssl_client);
 int start_ssl_client(sslclient_context *ssl_client, const char *host, uint32_t port, int timeout, const char *rootCABuff, const char *cli_cert, const char *cli_key, const char *pskIdent, const char *psKey, bool insecure);
 void stop_ssl_socket(sslclient_context *ssl_client, const char *rootCABuff, const char *cli_cert, const char *cli_key);
 int data_to_read(sslclient_context *ssl_client);
-int send_ssl_data(sslclient_context *ssl_client, const uint8_t *data, uint16_t len);
+int send_ssl_data(sslclient_context *ssl_client, const uint8_t *data, size_t len);
 int get_ssl_receive(sslclient_context *ssl_client, uint8_t *data, int length);
 bool verify_ssl_fingerprint(sslclient_context *ssl_client, const char* fp, const char* domain_name);
 bool verify_ssl_dn(sslclient_context *ssl_client, const char* domain_name);
