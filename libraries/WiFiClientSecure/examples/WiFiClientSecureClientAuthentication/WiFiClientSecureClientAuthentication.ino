ccb955f43e86ca75cb83da7e386c11a8c25bf113
Norbert <gnorbi19@gmail.com>
2021-04-11 14:09:01 +0200

    Small modifications on ESP32 to Mosquitto (mqtt) broker TLS encrypted connection example.

--- libraries/WiFiClientSecure/examples/WiFiClientSecureClientAuthentication/WiFiClientSecureClientAuthentication.ino
+++ libraries/WiFiClientSecure/examples/WiFiClientSecureClientAuthentication/WiFiClientSecureClientAuthentication.ino
@@ -21,7 +21,13 @@
   openssl req -out esp.csr -key esp.key -new -subj '/CN=localhost'
   openssl x509 -req -in esp.csr -CA ca.crt -CAkey ca.key -CAcreateserial -out esp.crt -days 365
 
-  2. step - Install and setup you Mosquitto broker
+  2. Open ca.crt, esp.crt and esp.key with text viewer and copy the values to this WiFiClientSecureClientAuthentication.ino source file into 
+  corresponding const char CA_cert[], const char ESP_CA_cert[] and const char ESP_RSA_key[] with escape characters.
+
+  (1-2.) Alternatively you can use the libraries/WiFiClientSecure/examples/WiFiClientSecureClientAuthentication/certificates/certificate_generator.sh script 
+  for generating and formatting the certificates.
+
+  3. step - Install and setup you Mosquitto broker
   Follow the instructions from https://mosquitto.org/ and check the manual for the coniguration.
   For the Mosquito broker you need ca.crt, mosquitto.key and mosquitto.crt files generated in previous step.
   Recommended to put they in /etc/mosquitto/ca_certificates/ and /etc/mosquitto/certs/
@@ -34,11 +40,10 @@
   require_certificate true or false #If you need client authentication set it to true
   log_type all  #for logging in /var/log/mosquitto/
 
-  3. Open ca.crt, esp.crt and esp.key with text viewer and copy the values to this WiFiClientSecureClientAuthentication.ino source file into 
-  corresponding const char CA_cert[], const char ESP_CA_cert[] and const char ESP_RSA_key[] with escape characters.
-
-  (4.) Alternatively you can use the libraries/WiFiClientSecure/examples/WiFiClientSecureClientAuthentication/certificates/certificate_generator.sh script 
-  for generating and formatting the certificates.
+  4.Restart the Mosquitto service or start the broker:
+  sudo service mosquitto restart
+  or
+  mosquitto -c /etc/mosquitto/conf.d/default.conf
   
   2021 - Norbert Gal - Apache 2.0 License.
 */
@@ -68,11 +73,11 @@ const char* ESP_RSA_key= \
 "-----END RSA PRIVATE KEY-----";
 
 
-const char* ssid     = "wifi";     // your network SSID (name of wifi network)
-const char* password = "wifi_pass"; // your network password
+const char* ssid     = "wifi";        // your network SSID (name of wifi network)
+const char* password = "wifi_pass";   // your network password
 
 const char* server = "Mosquitto_borker_adress";
-const char* mqtt_user = "user"; //If it is configured, see Mosquitto config
+const char* mqtt_user = "user";           //If it is configured, see Mosquitto config
 const char* mqtt_pass = "user_password";  //If it is configured, see Mosquitto config
 
 WiFiClientSecure client;
@@ -107,9 +112,9 @@ void loop() {
   
   Serial.println("\nStarting connection to server...");
   //if you use password for Mosquitto broker
-  if (mqtt_client.connect("ESP32", mqtt_user , mqtt_pass)) {
+  //if (mqtt_client.connect("ESP32", mqtt_user , mqtt_pass)) {
   //if you dont use password for Mosquitto broker
-  //if (mqtt_client.connect("ESP32")) {                       
+  if (mqtt_client.connect("ESP32")) {                       
     Serial.print("Connected, mqtt_client state: ");
     Serial.println(mqtt_client.state());
     mqtt_client.publish("LIvingRoom/TEMPERATURE", "25");
