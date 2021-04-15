4f9ff7a56e68f2e5a8c36da115d193c7e5f78e42
me-no-dev <hristo@espressif.com>
2021-04-15 16:22:12 +0300

    Adjust examples

--- libraries/WebServer/examples/SDWebServer/SDWebServer.ino
+++ libraries/WebServer/examples/SDWebServer/SDWebServer.ino
@@ -229,7 +229,7 @@ void printDirectory() {
     output += "{\"type\":\"";
     output += (entry.isDirectory()) ? "dir" : "file";
     output += "\",\"name\":\"";
-    output += entry.name();
+    output += entry.path();
     output += "\"";
     output += "}";
     server.sendContent(output);
