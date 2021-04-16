1b72edc0fcdae45b2362f22dd8a158734d8397f7
me-no-dev <me-no-dev@github.com>
2021-04-16 18:27:36 +0000

    IDF master cf457d412

--- tools/sdk/esp32c3/include/bootloader_support/include/esp_flash_data_types.h
+++
@@ -1,2 +0,0 @@
-#warning esp_flash_data_types.h has been merged into esp_flash_partitions.h, please include esp_flash_partitions.h instead
-#include "esp_flash_partitions.h"
--- tools/sdk/esp32c3/include/json/cJSON/tests/unity/test/testdata/Defs.h
+++
@@ -1,8 +0,0 @@
-#ifndef DEF_H
-#define DEF_H
-
-#define EXTERN_DECL
-
-extern int CounterSuiteSetup;
-
-#endif //DEF_H
