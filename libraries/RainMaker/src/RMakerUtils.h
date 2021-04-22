e62ff6dc375d1e75682ce9d79248134d18600680
Piyush Shah <8659527+shahpiyushv@users.noreply.github.com>
2021-04-22 15:29:27 +0530

    RainMaker library: Minor changes (#5092)
    - Use Serial.print instead of log_i for QR code helper information,
      so that it is always printed by default.
    - Expose the RainMaker factory reset and wifi reset APIs.
    - Simplify example to have only a Switch device. Create another example for custom device.
    - Enable push button based Factory reset and Wi-Fi reset.
    - Added support for the TimeZone service.
    - Moved API doc to RainMaker library's top level README.
    - Other minor doc changes.

---
+++ libraries/RainMaker/src/RMakerUtils.h
@@ -0,0 +1,16 @@
+#include "esp_system.h"
+#if ESP_IDF_VERSION_MAJOR >= 4 && CONFIG_IDF_TARGET_ESP32
+
+#include <esp_rmaker_utils.h>
+
+static void RMakerFactoryReset(int seconds)
+{
+    esp_rmaker_factory_reset(seconds);
+}
+
+static void RMakerWiFiReset(int seconds)
+{
+    esp_rmaker_wifi_reset(seconds);
+}
+
+#endif
