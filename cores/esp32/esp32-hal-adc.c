064cae1d503c025fb9d5b271bae550eddb21dfa9
Larry Bernstone <lbernstone@gmail.com>
2021-06-09 17:06:17 -0600

    Urgh. Right function this time

--- cores/esp32/esp32-hal-adc.c
+++ cores/esp32/esp32-hal-adc.c
@@ -249,7 +249,7 @@ int __hallRead()    //hall sensor using idf read
 {
     pinMode(36, ANALOG);
     pinMode(39, ANALOG);
-    adcSetWidth(12);
+    __analogSetWidth(12);
     return hall_sensor_read();
 }
 #endif
