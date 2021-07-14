51545aae9448d8cc08c0ad5ed0b8a6bfc61af0ac
pedro.minatel <pedro.minatel@espressif.com>
2021-07-14 11:49:37 +0100

    Added the Espressif logo on the docs main menu

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,7 +56,7 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 html_theme = 'default'
-
+html_logo = '_static/logo_espressif.png'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
