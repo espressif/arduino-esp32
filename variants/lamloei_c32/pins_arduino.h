33d3818a2b7730db0215d8a63b59359fdb32f09f
lamloei <lamloei@users.noreply.github.com>
2021-08-12 13:55:26 +0700

    Create pins_arduino.h
    add board LamLoei C32

---
+++ variants/lamloei_c32/pins_arduino.h
@@ -0,0 +1,46 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+#define EXTERNAL_NUM_INTERRUPTS 22
+#define NUM_DIGITAL_PINS        22
+#define NUM_ANALOG_INPUTS       6
+
+#define analogInputToDigitalPin(p)  (((p)<NUM_ANALOG_INPUTS)?(esp32_adc2gpio[(p)]):-1)
+#define digitalPinToInterrupt(p)    (((p)<NUM_DIGITAL_PINS)?(p):-1)
+#define digitalPinHasPWM(p)         (p < EXTERNAL_NUM_INTERRUPTS)
+
+static const uint8_t LED_BUILTIN = 8;
+#define BUILTIN_LED  LED_BUILTIN // backward compatibility
+#define LED_BUILTIN LED_BUILTIN
+
+static const uint8_t KEY_BUILTIN = 9;
+#define BUILTIN_KEY  KEY_BUILTIN // backward compatibility
+#define KEY_BUILTIN KEY_BUILTIN
+
+static const uint8_t TX = 21;
+static const uint8_t RX = 20;
+
+static const uint8_t SDA = 8;
+static const uint8_t SCL = 7;
+
+static const uint8_t SS    = 7;
+static const uint8_t MOSI  = 6;
+static const uint8_t MISO  = 5;
+static const uint8_t SCK   = 4;
+
+static const uint8_t A0 = 0;
+static const uint8_t A1 = 1;
+static const uint8_t A2 = 2;
+static const uint8_t A3 = 3;
+static const uint8_t A4 = 4;
+static const uint8_t A5 = 5;
+
+static const uint8_t DM = 18;
+static const uint8_t DN = 18;
+static const uint8_t DP = 19;
+static const uint8_t USB_DM = 18;
+static const uint8_t USB_DN = 18;
+static const uint8_t USB_DP = 19;
+#endif /* Pins_Arduino_h */
