092577179bbc54e8db575b72830864e665647aa9
Rodrigo Garcia <Rodrigo.Garcia@espressif.com>
2021-10-20 23:49:01 -0300

    wifi dynamic buffers tweak

--- libraries/WiFi/src/WiFiGeneric.cpp
+++ libraries/WiFi/src/WiFiGeneric.cpp
@@ -557,6 +557,16 @@ bool wifiLowLevelInit(bool persistent){
         }
 
         wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+
+	// this code forces WiFi to go with Dynamic Buffers 
+        // it bypasses sdkconfig definitions!
+        cfg.static_tx_buf_num = 0;
+        cfg.dynamic_tx_buf_num = 32;
+        cfg.tx_buf_type = 1;
+        cfg.cache_tx_buf_num = 1;  // can't be zero!
+        cfg.static_rx_buf_num = 4;
+        cfg.dynamic_rx_buf_num = 32;
+
         esp_err_t err = esp_wifi_init(&cfg);
         if(err){
             log_e("esp_wifi_init %d", err);
