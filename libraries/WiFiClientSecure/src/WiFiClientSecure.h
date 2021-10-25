15bbd0a187daa5b41393b2faf5c299bc6139c453
Anthony Elder <torntrousers@gmail.com>
2021-10-25 07:20:47 +0100

    Add ALPN support to WiFiClientSecure  (#5633)
    This adds a function to WiFiClientSecure to set the ALPN protocol.
    
    This is required for an MQTT client to connect to AWS IoT when using an AWS Custom Authorizer, as described here.
    
    Example code snippet:
    
    ...
    WiFiClientSecure wiFiClient;
    
    // ALPN protocol, needed with AWS custom authorizer
    const char *aws_protos[] = {"mqtt", NULL};
    
    void setup() {
      wiFiClient.setCACert(AWSCAPEM);
      wiFiClient.setAlpnProtocols(aws_protos);
    }
    ...

--- libraries/WiFiClientSecure/src/WiFiClientSecure.h
+++ libraries/WiFiClientSecure/src/WiFiClientSecure.h
@@ -39,6 +39,7 @@ protected:
     const char *_private_key;
     const char *_pskIdent; // identity for PSK cipher suites
     const char *_psKey; // key in hex for PSK cipher suites
+    const char **_alpn_protos;
 
 public:
     WiFiClientSecure *next;
@@ -73,6 +74,7 @@ public:
     bool loadPrivateKey(Stream& stream, size_t size);
     bool verify(const char* fingerprint, const char* domain_name);
     void setHandshakeTimeout(unsigned long handshake_timeout);
+    void setAlpnProtocols(const char **alpn_protos);
     const mbedtls_x509_crt* getPeerCertificate() { return mbedtls_ssl_get_peer_cert(&sslclient->ssl_ctx); };
     bool getFingerprintSHA256(uint8_t sha256_result[32]) { return get_peer_fingerprint(sslclient, sha256_result); };
     int setTimeout(uint32_t seconds){ return 0; }
