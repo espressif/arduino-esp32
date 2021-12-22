e8611e223c0da8c0ffe4b3e4bbdb061714126ddf
Rodrigo Garcia <Rodrigo.Garcia@espressif.com>
2021-12-21 21:06:16 -0300

    RMT refactoring based on IDF

--- cores/esp32/esp32-hal-rmt.c
+++ cores/esp32/esp32-hal-rmt.c
@@ -535,6 +535,7 @@ rmt_obj_t* rmtInit(int pin, bool tx_not_rx, rmt_reserve_memsize_t memsize)
         ch_end = RMT_RX_CH_END;
     }
     for (i=ch_start; i<=ch_end; i++) {
+        j = 0;
         for (j=0; j<buffers && i+j <= ch_end; j++) {
             // if the space is ocupied break and continue on other channel
             if (g_rmt_objects[i+j].allocated) {
