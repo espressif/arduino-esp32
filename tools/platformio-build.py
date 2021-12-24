f255281f71affcf8924606f76d7241841753fe24
Mjxkill <michael.jouannigot@outlook.com>
2021-12-24 22:59:17 +0100

    connection 2

--- tools/platformio-build.py
+++ tools/platformio-build.py
@@ -36,7 +36,7 @@ SConscript(
     join(
         DefaultEnvironment()
         .PioPlatform()
-        .get_package_dir("framework-arduinoespressif32"),
+        .get_package_dir("framework-arduinoespressif32-mjxkill"),
         "tools",
         "platformio-build-%s.py" % build_mcu,
     )
