e70a1cac0f14e33d2176a51ea3d5df5bb6f277ec
lady ada <limor@ladyada.net>
2021-12-27 15:54:28 -0500

    enable neopixel

--- variants/adafruit_qtpy_esp32s2/variant.cpp
+++ variants/adafruit_qtpy_esp32s2/variant.cpp
@@ -25,13 +25,18 @@
 
 #include "esp32-hal-gpio.h"
 #include "pins_arduino.h"
+#include "Arduino.h"
+#include "Wire.h"
 
 extern "C" {
 
 // Initialize variant/board, called before setup()
 void initVariant(void)
 {
-
+  // This board has a power control pin, and we must set it to output and high
+  // in order to enable the NeoPixels.
+  pinMode(NEOPIXEL_POWER, OUTPUT);
+  digitalWrite(NEOPIXEL_POWER, HIGH);
 }
 
 }
