afdd64f31486b69447264c304be816f4ac635797
Darek7D <darek7d@gmail.com>
2021-12-31 14:58:41 +0100

    Unnecessary spaces

--- libraries/Ethernet/src/ETH.cpp
+++ libraries/Ethernet/src/ETH.cpp
@@ -294,7 +294,7 @@ bool ETHClass::begin(uint8_t phy_addr, int power, int mdc, int mdio, eth_phy_typ
 #else
             log_e("unsupported ethernet type 'ETH_PHY_KSZ8081'");
 #endif
-            break;            
+            break;
         default:
             break;
     }
