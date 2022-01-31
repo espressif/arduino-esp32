8f3c24388311c0c38ee29bb271bcd96a5990705e
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-01-31 17:47:57 +0100

    Bootloader is at 0x0000 for S3

--- tools/platformio-build-esp32s3.py
+++ tools/platformio-build-esp32s3.py
@@ -324,7 +324,7 @@ env.Append(
     ],
 
     FLASH_EXTRA_IMAGES=[
-        ("0x1000", join(FRAMEWORK_DIR, "tools", "sdk", "esp32s3", "bin", "bootloader_${BOARD_FLASH_MODE}_${__get_board_f_flash(__env__)}.bin")),
+        ("0x0000", join(FRAMEWORK_DIR, "tools", "sdk", "esp32s3", "bin", "bootloader_${BOARD_FLASH_MODE}_${__get_board_f_flash(__env__)}.bin")),
         ("0x8000", join(env.subst("$BUILD_DIR"), "partitions.bin")),
         ("0xe000", join(FRAMEWORK_DIR, "tools", "partitions", "boot_app0.bin"))
     ]
