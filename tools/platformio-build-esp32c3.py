b69b44a93d80deb5f489b8782dd1f9168ab5edd6
me-no-dev <me-no-dev@users.noreply.github.com>
2022-02-06 18:20:43 +0000

    IDF release/v4.4 7c986a64c7

--- tools/platformio-build-esp32c3.py
+++ tools/platformio-build-esp32c3.py
@@ -293,7 +293,7 @@ env.Append(
         "UNITY_INCLUDE_CONFIG_H",
         "WITH_POSIX",
         "_GNU_SOURCE",
-        ("IDF_VER", '\\"v4.4-112-g0a1e309ee4\\"'),
+        ("IDF_VER", '\\"v4.4-114-g7c986a64c7\\"'),
         "ESP_PLATFORM",
         "_POSIX_READER_WRITER_LOCKS",
         "ARDUINO_ARCH_ESP32",
