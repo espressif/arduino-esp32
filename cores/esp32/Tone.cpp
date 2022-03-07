a48b155862ab079eaea3252496182585554e0819
Tomas Pilny <tomas.pilny@espressif.com>
2022-03-07 12:28:24 +0100

    Initial commit for Tone support

---
+++ cores/esp32/Tone.cpp
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+void tone(uint8_t _pin, unsigned int frequency, unsigned long duration = 0, unsigned long duty = 50){
+  // TODO
+}
+
+void noTone(uint8_t _pin){
+  // TODO
+}
