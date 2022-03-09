5a815c908f3afcf10cbf69ce4c4b9b1214376a31
Tomas Pilny <tomas.pilny@espressif.com>
2022-03-09 14:48:53 +0100

    Implemented tone

--- cores/esp32/Arduino.h
+++ cores/esp32/Arduino.h
@@ -195,6 +195,9 @@ extern "C" void configTime(long gmtOffset_sec, int daylightOffset_sec,
 extern "C" void configTzTime(const char* tz,
         const char* server1, const char* server2 = nullptr, const char* server3 = nullptr);
 
+void tone(uint8_t _pin, unsigned int frequency, unsigned long duration = 0, unsigned long duty = 50);
+void noTone(uint8_t _pin);
+
 // WMath prototypes
 long random(long);
 #endif /* __cplusplus */
---
+++ cores/esp32/Tone.cpp
@@ -0,0 +1,23 @@
+#include <Arduino.h>
+#include "esp32-hal-ledc.h"
+
+void noTone(uint8_t _pin){
+  ledcDetachPin(_pin);
+}
+
+// parameters:
+// _pin - pin number which will output the signal
+// frequency - PWM frequency in Hz
+// duration - time in ms - how long will the signal be outputted.
+//   If not provided, or 0 you must manually call noTone to end output
+// duty - PWM duty in % (default 50%)
+void tone(uint8_t _pin, unsigned int frequency, unsigned long duration, unsigned long duty){
+  ledcSetup(0, frequency, 11);
+  ledcAttachPin(_pin, 0);
+
+  ledcWrite(0, duty * 20);
+  if(duration){
+    vTaskDelay(pdMS_TO_TICKS(duration));
+    noTone(_pin);
+  }
+}
