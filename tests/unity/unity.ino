96f8f5e3efe9d4ae1bb92b585059f1b4dfbcee98
Abdelatif Guettouche <abdelatif.guettouche@espressif.com>
2022-03-10 13:45:26 +0100

    Add initial hardware testing support (#6313)
    - Added workflow triggered by cron or label "hil_test"
    - Added examples with both pytest and unity

---
+++ tests/unity/unity.ino
@@ -0,0 +1,33 @@
+#include <unity.h>
+
+
+/* These functions are intended to be called before and after each test. */
+void setUp(void) {
+}
+
+void tearDown(void){
+}
+
+
+void test_pass(void){
+  TEST_ASSERT_EQUAL(1, 1);
+}
+
+void test_fail(void){
+  TEST_ASSERT_EQUAL(1, 1);
+}
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    ;
+  }
+  
+  UNITY_BEGIN();
+  RUN_TEST(test_pass);
+  RUN_TEST(test_fail);
+  UNITY_END();
+}
+
+void loop() {
+}
