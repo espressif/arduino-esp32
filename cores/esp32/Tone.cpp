b784c5ea6eca391df937910ca839d8aceb7fa509
Tomas Pilny <tomas.pilny@espressif.com>
2022-03-10 15:13:09 +0100

    Tone uses queue; implemented setToneChannel

--- cores/esp32/Tone.cpp
+++ cores/esp32/Tone.cpp
@@ -1,8 +1,128 @@
 #include <Arduino.h>
 #include "esp32-hal-ledc.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
+#include "freertos/semphr.h"
+
+static TaskHandle_t _tone_task = NULL;
+static QueueHandle_t _tone_queue = NULL;
+static QueueHandle_t _tone_mutex = NULL;
+static uint8_t _channel = 0;
+
+typedef enum{
+  TONE_START,
+  TONE_END,
+  TONE_SET_CHANNEL
+} tone_cmd_t;
+
+typedef struct{
+  tone_cmd_t tone_cmd;
+  uint8_t pin;
+  unsigned int frequency;
+  unsigned long duration;
+  uint8_t channel;
+} tone_msg_t;
+
+static void tone_task(void*){
+  tone_msg_t tone_msg;
+  while(1){
+    xQueueReceive(_tone_queue, &tone_msg, portMAX_DELAY);
+    switch(tone_msg.tone_cmd){
+      case TONE_START:
+        log_d("Task received from queue TONE_START: _pin=%d, frequency=%u Hz, duration=%u ms", tone_msg.pin, tone_msg.frequency, tone_msg.duration);
+        if(xSemaphoreTake(_tone_mutex, portMAX_DELAY) != pdTRUE ){
+          log_e("Tone mutex take returned with error");
+          break;
+        }
+        log_d("Setup LED controll on channel %d", _channel);
+        ledcSetup(_channel, tone_msg.frequency, 11);
+        ledcAttachPin(tone_msg.pin, _channel);
+        ledcWrite(_channel, 1024);
+        if(xSemaphoreGive(_tone_mutex) != pdTRUE){
+          log_e("Tone mutex give returned with error");
+        }
+
+        if(tone_msg.duration){
+          vTaskDelay(pdMS_TO_TICKS(tone_msg.duration));
+          ledcDetachPin(tone_msg.pin);
+        }
+        break;
+
+      case TONE_END:
+        log_d("Task received from queue TONE_END: pin=%d", tone_msg.pin);
+        ledcDetachPin(tone_msg.pin);
+        break;
+
+      case TONE_SET_CHANNEL:
+        _channel = tone_msg.channel;
+        break;
+
+      default: ; // do nothing
+    } // switch
+  } // infinite loop
+}
+
+static int tone_init(){
+  if(_tone_mutex == NULL){
+    log_v("Creating tone mutex");
+    _tone_mutex = xSemaphoreCreateMutex();
+    if(_tone_mutex == NULL){
+      log_e("Could not create tone mutex");
+      return 0; // ERR
+    }
+    log_v("Tone mutex created");
+  }
+
+
+  if(_tone_queue == NULL){
+    log_v("Creating tone queue");
+    _tone_queue = xQueueCreate(128, sizeof(tone_msg_t));
+    if(_tone_queue == NULL){
+      log_e("Could not create tone queue");
+      return 0; // ERR
+    }
+    log_v("Tone queue created");
+  }
+
+  if(_tone_task == NULL){
+    log_v("Creating tone task");
+    xTaskCreate(
+      tone_task, // Function to implement the task
+      "toneTask", // Name of the task
+      3500,  // Stack size in words
+      NULL,  // Task input parameter
+      1,  // Priority of the task
+      &_tone_task  // Task handle.
+      );
+    if(_tone_task == NULL){
+      log_e("Could not create tone task");
+      return 0; // ERR
+    }
+    log_v("Tone task created");
+  }
+  return 1; // OK
+}
+
+void setToneChannel(uint8_t channel){
+  log_d("channel=%d", channel);
+  if(tone_init()){
+    tone_msg_t tone_msg = {
+      .tone_cmd = TONE_SET_CHANNEL,
+      .channel = channel
+    };
+    xQueueSend(_tone_queue, &tone_msg, portMAX_DELAY);
+  }
+}
 
 void noTone(uint8_t _pin){
-  ledcDetachPin(_pin);
+  log_d("noTone was called");
+  if(tone_init()){
+    tone_msg_t tone_msg = {
+      .tone_cmd = TONE_END,
+      .pin = _pin
+    };
+    xQueueSend(_tone_queue, &tone_msg, portMAX_DELAY);
+  }
 }
 
 // parameters:
@@ -11,12 +131,14 @@ void noTone(uint8_t _pin){
 // duration - time in ms - how long will the signal be outputted.
 //   If not provided, or 0 you must manually call noTone to end output
 void tone(uint8_t _pin, unsigned int frequency, unsigned long duration){
-  ledcSetup(0, frequency, 11);
-  ledcAttachPin(_pin, 0);
-
-  ledcWrite(0, 1024);
-  if(duration){
-    vTaskDelay(pdMS_TO_TICKS(duration));
-    noTone(_pin);
+  log_d("_pin=%d, frequency=%u Hz, duration=%u ms", _pin, frequency, duration);
+  if(tone_init()){
+    tone_msg_t tone_msg = {
+      .tone_cmd = TONE_START,
+      .pin = _pin,
+      .frequency = frequency,
+      .duration = duration
+    };
+    xQueueSend(_tone_queue, &tone_msg, portMAX_DELAY);
   }
 }
