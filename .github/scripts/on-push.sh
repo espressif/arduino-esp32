752d415b0950d4a61e0fb98e2088adc508b95276
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2022-03-29 15:56:00 +0200

    use esptool v3.3 (#6498)
    could be used for all. Nice would be releasing in Platformio registry.

--- .github/scripts/on-push.sh
+++ .github/scripts/on-push.sh
@@ -109,7 +109,7 @@ else
     replace_script+="data['packages']['toolchain-xtensa-esp32']['optional']=True;"
     replace_script+="data['packages']['toolchain-xtensa-esp32s3']['optional']=False;"
     replace_script+="data['packages']['tool-esptoolpy']['owner']='tasmota';"
-    replace_script+="data['packages']['tool-esptoolpy']['version']='https://github.com/tasmota/esptool/releases/download/v3.2.1/esptool-3.2.1.zip';"
+    replace_script+="data['packages']['tool-esptoolpy']['version']='https://github.com/tasmota/esptool/releases/download/v3.3/esptool-3.3.zip';"
     replace_script+="fp.seek(0);fp.truncate();json.dump(data, fp, indent=2);fp.close()"
     python -c "$replace_script"
 
