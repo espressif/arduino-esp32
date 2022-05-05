ec33314bba43a3f2c9d4b38843ca84bdc4c232c9
pedro.minatel <pedro.minatel@espressif.com>
2022-05-05 09:46:49 +0200

    Removed TARGETS and VERSIONS

--- docs/_static/arduino_versions.js
+++ docs/_static/arduino_versions.js
@@ -1,9 +1,4 @@
 var DOCUMENTATION_VERSIONS = {
-    DEFAULTS: { has_targets: false,
-                supported_targets: [ "esp32" ]
+    DEFAULTS: { has_targets: false
               },
-    VERSIONS: [ text: "2.0.3", value: "v2.0.3" ],
-    IDF_TARGETS: [
-       { text: "ESP32", value: "esp32" },
-    ]
 };
