74bf57f13b1d847cdad995ffe2c89bdc6e6d8e4d
Dmytro Korniienko <Dmytro.Korniienko@volo.global>
2022-05-23 14:24:28 +0300

    Update camera example for support LED Intensity

--- libraries/ESP32/examples/Camera/CameraWebServer/CameraWebServer.ino
+++ libraries/ESP32/examples/Camera/CameraWebServer/CameraWebServer.ino
@@ -1,6 +1,5 @@
 #include "esp_camera.h"
 #include <WiFi.h>
-
 //
 // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
 //            Ensure ESP32 Wrover Module or other board with PSRAM is selected
@@ -10,25 +9,20 @@
 //            Face Recognition is DISABLED for ESP32 and ESP32-S2, because it takes up from 15 
 //            seconds to process single frame. Face Detection is ENABLED if PSRAM is enabled as well
 
-// ===================
-// Select camera model
-// ===================
-//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
-//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
-//#define CAMERA_MODEL_ESP32S3_EYE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
-//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
-//#define CAMERA_MODEL_M5STACK_UNITCAM // No PSRAM
-#define CAMERA_MODEL_AI_THINKER // Has PSRAM
-//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
-// ** Espressif Internal Boards **
-//#define CAMERA_MODEL_ESP32_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S2_CAM_BOARD
-//#define CAMERA_MODEL_ESP32S3_CAM_LCD
-
+#include "board.h" // Select camera model here (moved since dependencies)
 #include "camera_pins.h"
+
+#ifdef CAMERA_MODEL_AI_THINKER
+#ifndef CONFIG_LED_ILLUMINATOR_ENABLED
+#define CONFIG_LED_ILLUMINATOR_ENABLED 1
+#endif
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
+#endif
+
+#if defined(CONFIG_LED_ILLUMINATOR_ENABLED)
+void ledc_setup();
+#endif
 
 // ===========================
 // Enter your WiFi credentials
@@ -42,6 +36,10 @@ void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(true);
   Serial.println();
+
+#ifdef CAMERA_MODEL_AI_THINKER
+  WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); //disable brownout detector
+#endif
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -139,6 +137,10 @@ void setup() {
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
+
+#if defined(CONFIG_LED_ILLUMINATOR_ENABLED)
+  ledc_setup();
+#endif  
 }
 
 void loop() {
