6967d8fc46a8fe04e5609cecaab5038e4bb38a1c
Tomas Pilny <tomas.pilny@espressif.com>
2022-05-25 10:41:58 +0200

    Initial implementation of RGB driver via digitalWrite

--- cores/esp32/esp32-hal-gpio.c
+++ cores/esp32/esp32-hal-gpio.c
@@ -125,8 +125,54 @@ extern void ARDUINO_ISR_ATTR __pinMode(uint8_t pin, uint8_t mode)
     }
 }
 
+void RGBLedWrite(uint8_t pin, uint8_t red_val, uint8_t green_val, uint8_t blue_val){
+  static bool initialized = false;
+  rmt_data_t led_data[24];
+  static rmt_obj_t* rmt_send = NULL;
+
+  if(!initialized){
+    if ((rmt_send = rmtInit(pin, RMT_TX_MODE, RMT_MEM_64)) == NULL){
+        log_e("RGB LED driver initialization failed!");
+        rmt_send = NULL;
+        return;
+    }
+    rmtSetTick(rmt_send, 100);
+    initialized = true;
+  }
+
+  int color[] = {green_val, red_val, blue_val};  // Color coding is in order GREEN, RED, BLUE
+  int i = 0;
+  for(int col=0; col<3; col++ ){
+    for(int bit=0; bit<8; bit++){
+      if((color[col] & (1<<(7-bit)))){
+        // HIGH bit
+        led_data[i].level0 = 1; // T1H
+        led_data[i].duration0 = 8; // 0.8us
+        led_data[i].level1 = 0; // T1L
+        led_data[i].duration1 = 4; // 0.4us
+      }else{
+        // LOW bit
+        led_data[i].level0 = 1; // T0H
+        led_data[i].duration0 = 4; // 0.4us
+        led_data[i].level1 = 0; // T0L
+        led_data[i].duration1 = 8; // 0.8us
+      }
+      i++;
+    }
+  }
+  rmtWrite(rmt_send, led_data, 24);
+}
+
 extern void ARDUINO_ISR_ATTR __digitalWrite(uint8_t pin, uint8_t val)
 {
+    #ifdef BOARD_HAS_RGB_LED
+        if(pin == LED_BUILTIN){
+            //use RMT to set all channels on/off
+            const uint8_t comm_val = val != 0 ? 255 : 0;
+            RGBLedWrite(pin, comm_val, comm_val, comm_val);
+            return;
+        }
+    #endif
 	gpio_set_level((gpio_num_t)pin, val);
 }
 
