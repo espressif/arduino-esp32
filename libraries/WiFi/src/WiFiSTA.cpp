7efe3cec5a6e24c5978db37ce2d044bc171e88c0
Xiang Hou <fryefryefrye@foxmail.com>
2022-07-30 22:22:44 +0800

    Update WiFiSTA.cpp

--- libraries/WiFi/src/WiFiSTA.cpp
+++ libraries/WiFi/src/WiFiSTA.cpp
@@ -733,7 +733,7 @@ bool WiFiSTAClass::enableIpV6()
 }
 
 /**
- * Get the station interface IPv6 address.
+* Get the station interface local link IPv6 address.
  * @return IPv6Address
  */
 IPv6Address WiFiSTAClass::localIPv6()
@@ -743,6 +743,22 @@ IPv6Address WiFiSTAClass::localIPv6()
         return IPv6Address();
     }
     if(esp_netif_get_ip6_linklocal(get_esp_interface_netif(ESP_IF_WIFI_STA), &addr)) {
+        return IPv6Address();
+    }
+    return IPv6Address(addr.addr);
+}
+
+/**
+* Get the station interface local link IPv6 address.
+ * @return IPv6Address
+ */
+IPv6Address WiFiSTAClass::globalIPv6()
+{
+	esp_ip6_addr_t addr;
+    if(WiFiGenericClass::getMode() == WIFI_MODE_NULL){
+        return IPv6Address();
+    }
+    if(esp_netif_get_ip6_global(get_esp_interface_netif(ESP_IF_WIFI_STA), &addr)) {
         return IPv6Address();
     }
     return IPv6Address(addr.addr);
