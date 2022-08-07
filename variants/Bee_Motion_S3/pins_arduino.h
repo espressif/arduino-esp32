616947ca555e1c8159119fe1ec934a38a22eab1c
paul <strid3r21@gmail.com>
2022-08-07 15:14:56 -0400

    updated some pin defs

--- variants/Bee_Motion_S3/pins_arduino.h
+++ variants/Bee_Motion_S3/pins_arduino.h
@@ -9,12 +9,12 @@
 #define USB_PRODUCT "Bee Motion S3"
 #define USB_SERIAL ""
 
-#define EXTERNAL_NUM_INTERRUPTS 46
-#define NUM_DIGITAL_PINS        48
-#define NUM_ANALOG_INPUTS       20
+#define EXTERNAL_NUM_INTERRUPTS 38
+#define NUM_DIGITAL_PINS        27
+#define NUM_ANALOG_INPUTS       11
 
-#define analogInputToDigitalPin(p)  (((p)<20)?(analogChannelToDigitalPin(p)):-1)
-#define digitalPinToInterrupt(p)    (((p)<48)?(p):-1)
+#define analogInputToDigitalPin(p)  (((p)<11)?(analogChannelToDigitalPin(p)):-1)
+#define digitalPinToInterrupt(p)    (((p)<27)?(p):-1)
 #define digitalPinHasPWM(p)         (p < 46)
 
 static const uint8_t TX = 43;
@@ -30,17 +30,17 @@ static const uint8_t SDO  = 35;
 static const uint8_t SDI  = 37;
 static const uint8_t SCK   = 17;
 
-static const uint8_t A4 = 5;
-static const uint8_t A5 = 6;
-static const uint8_t A6 = 7;
-static const uint8_t A7 = 8;
-static const uint8_t A8 = 9;
-static const uint8_t A9 = 10;
-static const uint8_t A10 = 11;
-static const uint8_t A11 = 12;
-static const uint8_t A12 = 13;
-static const uint8_t A13 = 14;
-static const uint8_t A14 = 15;
+static const uint8_t A5 = 5;
+static const uint8_t A6 = 6;
+static const uint8_t A7 = 7;
+static const uint8_t A8 = 8;
+static const uint8_t A9 = 9;
+static const uint8_t A10 = 10;
+static const uint8_t A11 = 11;
+static const uint8_t A12 = 12;
+static const uint8_t A13 = 13;
+static const uint8_t A14 = 14;
+static const uint8_t A15 = 15;
 
 static const uint8_t D5 = 5;
 static const uint8_t D6 = 6;
