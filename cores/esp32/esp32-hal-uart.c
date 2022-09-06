72ce063c6173fbe84bc9cc291657f825dbf767c8
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2022-09-06 12:05:35 -0300

    reconfiguration with "uartSetFastReading()"

--- cores/esp32/esp32-hal-uart.c
+++ cores/esp32/esp32-hal-uart.c
@@ -162,13 +162,6 @@ uart_t* uartBegin(uint8_t uart_nr, uint32_t baudrate, uint32_t config, int8_t rx
     uart_config.rx_flow_ctrl_thresh = rxfifo_full_thrhd;
     uart_config.source_clk = UART_SCLK_APB;
 
-    uart_intr_config_t uart_intr = {
-        .intr_enable_mask = UART_INTR_RXFIFO_FULL | UART_INTR_RXFIFO_TOUT,   // only these IRQs - no BREAK, PARITY or OVERFLOW
-        .rx_timeout_thresh = 1,
-        .txfifo_empty_intr_thresh = 10,
-        .rxfifo_full_thresh = rxfifo_full_thrhd,
-    };
-
     ESP_ERROR_CHECK(uart_driver_install(uart_nr, rx_buffer_size, tx_buffer_size, 20, &(uart->uart_event_queue), 0));
     ESP_ERROR_CHECK(uart_param_config(uart_nr, &uart_config));
     ESP_ERROR_CHECK(uart_set_pin(uart_nr, txPin, rxPin, UART_PIN_NO_CHANGE, UART_PIN_NO_CHANGE));
@@ -178,15 +171,33 @@ uart_t* uartBegin(uint8_t uart_nr, uint32_t baudrate, uint32_t config, int8_t rx
         // invert signal for both Rx and Tx
         ESP_ERROR_CHECK(uart_set_line_inverse(uart_nr, UART_SIGNAL_TXD_INV | UART_SIGNAL_RXD_INV));    
     }
-
-    // override default RX IDF Driver Interrupt - no BREAK, PARITY or OVERFLOW
-    ESP_ERROR_CHECK(uart_intr_config(uart_nr, &uart_intr));
     
     UART_MUTEX_UNLOCK();
 
     uartFlush(uart);
     return uart;
 }
+
+// This code is under testing - for now just keep it here
+void uartSetFastReading(uart_t* uart)
+{
+    if(uart == NULL) {
+        return;
+    }
+
+    UART_MUTEX_LOCK();
+    // override default RX IDF Driver Interrupt - no BREAK, PARITY or OVERFLOW
+    uart_intr_config_t uart_intr = {
+        .intr_enable_mask = UART_INTR_RXFIFO_FULL | UART_INTR_RXFIFO_TOUT,   // only these IRQs - no BREAK, PARITY or OVERFLOW
+        .rx_timeout_thresh = 1,
+        .txfifo_empty_intr_thresh = 10,
+        .rxfifo_full_thresh = 2,
+    };
+
+    ESP_ERROR_CHECK(uart_intr_config(uart->num, &uart_intr));
+    UART_MUTEX_UNLOCK();
+}
+
 
 void uartSetRxTimeout(uart_t* uart, uint8_t numSymbTimeout)
 {
