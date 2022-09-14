8510734d8314d3071b4ebad32a3ee2452526bc5d
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2022-09-14 10:29:12 +0200

    Add default SPI pin definitions if not defined in pins_arduino.h (#7161)
    * add default SPI pins ifndef in arduino_pins.h
    
    * Revert "add default SPI pins ifndef in arduino_pins.h"
    
    This reverts commit be35b83ab997a88242269621cccbe1d17a0a844f.
    
    * Add missing default SPI pins
    
    * revert change for d32_pro

--- variants/wt32-eth01/pins_arduino.h
+++ variants/wt32-eth01/pins_arduino.h
@@ -52,4 +52,14 @@ static const uint8_t RXD2 = 5, RXD = 5;
 static const uint8_t TX = 1;
 static const uint8_t RX = 3;
 
+//SPI VSPI default pins
+static const uint8_t SS    = -1;
+static const uint8_t MOSI  = 15;
+static const uint8_t MISO  = 12;
+static const uint8_t SCK   = 14;
+
+//I2C default pins
+static const uint8_t SDA = 2;
+static const uint8_t SCL = 4;
+
 #endif /* Pins_Arduino_h */
