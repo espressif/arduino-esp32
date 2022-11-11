d7b5180542b0bc8c8a53ebedf0f582f65d63d987
Tomas Pilny <tomas.pilny@espressif.com>
2022-11-11 09:06:59 +0100

    Added search for Min frequency

--- libraries/ESP32/examples/AnalogOut/ledcFrequency/ledcFrequency.ino
+++ libraries/ESP32/examples/AnalogOut/ledcFrequency/ledcFrequency.ino
@@ -4,7 +4,7 @@
  * Ignore the error messages from incorrect settings such as these:
  *  "E (4190) ledc: requested frequency and duty resolution can not be achieved, try reducing freq_hz or duty_resolution. div_param=255"
  *
- * Date: 10 Nov 2022
+ * Date: 11 Nov 2022
  * Author: Tomas Pilny
  */
 
@@ -18,30 +18,55 @@ void setup() {
   uint32_t min_frequency;
   uint32_t max_frequency;
   uint32_t frequency;
-  uint32_t freq_array[SOC_LEDC_TIMER_BIT_WIDE_NUM];
-  delay(500);
+  uint32_t successful_frequency;
+  uint32_t max_freq_array[SOC_LEDC_TIMER_BIT_WIDE_NUM];
+  uint32_t min_freq_array[SOC_LEDC_TIMER_BIT_WIDE_NUM];
 
+  // Find Max Frequency
   for(uint8_t resolution = 1; resolution <= SOC_LEDC_TIMER_BIT_WIDE_NUM; ++resolution){
-    freq_array[resolution-1] = 0;
+    max_freq_array[resolution-1] = 0;
     min_frequency = 0;
     max_frequency = UINT32_MAX;
+    successful_frequency = 0;
     while(min_frequency != max_frequency && min_frequency+1 != max_frequency){
       frequency = min_frequency + ((max_frequency-min_frequency)/2);
       if(ledcChangeFrequency(analogGetChannel(PIN), frequency, resolution)){
         min_frequency = frequency;
+        successful_frequency = frequency;
       }else{
         max_frequency = frequency;
-        if(frequency == 1){ // Cannot go lower, report max frequency "0"
-          frequency = 0;
-        }
       }
     } // while not found the maximum
-    freq_array[resolution-1] = frequency;
+    max_freq_array[resolution-1] = successful_frequency;
   } // for all resolutions
 
-  printf("Bit resolution | Max Frequency [Hz]\n");
+  // Find Min Frequency
+  for(uint8_t resolution = 1; resolution <= SOC_LEDC_TIMER_BIT_WIDE_NUM; ++resolution){
+    min_freq_array[resolution-1] = 0;
+    min_frequency = 0;
+    max_frequency = max_freq_array[resolution-1];
+    successful_frequency = max_frequency;
+    while(min_frequency != max_frequency && min_frequency+1 != max_frequency){
+      frequency = min_frequency + ((max_frequency-min_frequency)/2);
+      if(ledcChangeFrequency(analogGetChannel(PIN), frequency, resolution)){
+        max_frequency = frequency;
+        successful_frequency = frequency;
+      }else{
+        min_frequency = frequency;
+      }
+    } // while not found the maximum
+    min_freq_array[resolution-1] = successful_frequency;
+  } // for all resolutions
+
+  printf("Bit resolution | Min Frequency [Hz] | Max Frequency [Hz]\n");
   for(uint8_t r = 1; r <= SOC_LEDC_TIMER_BIT_WIDE_NUM; ++r){
-    printf("             %d | %u\n", r, freq_array[r-1]);
+    size_t max_len = std::to_string(UINT32_MAX).length();
+    printf("            %s%d |         %s%u |         %s%u\n",
+      std::string (2 - std::to_string(r).length(), ' ').c_str(), r,
+      std::string (max_len - std::to_string(min_freq_array[r-1]).length(), ' ').c_str(),
+      min_freq_array[r-1],
+      std::string (max_len - std::to_string(max_freq_array[r-1]).length(), ' ').c_str(),
+      max_freq_array[r-1]);
   }
 }
 
