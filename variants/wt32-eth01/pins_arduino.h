9eb56f763b0c39220c5f188fc8c56b07765eb1d6
Sebastian Bergner <54766531+sebastianbergner@users.noreply.github.com>
2022-11-22 11:27:17 +0100

    Remove define macros

--- variants/wt32-eth01/pins_arduino.h
+++ variants/wt32-eth01/pins_arduino.h
@@ -25,14 +25,6 @@
 #define ETH_PHY_TYPE ETH_PHY_LAN8720
 #define ETH_CLK_MODE ETH_CLOCK_GPIO0_IN
 
-#define SDA 33
-#define SCL 32
-
-#define SS 2
-#define MOSI 14
-#define MISO 15
-#define SCK 12
-
 // general purpose IO pins
 static const uint8_t IO0 = 0;
 static const uint8_t IO1 = 1; // TXD0 / TX0 pin
@@ -62,12 +54,12 @@ static const uint8_t RX = 3;
 
 //SPI VSPI default pins
 static const uint8_t SS    = -1;
-static const uint8_t MOSI  = 15;
-static const uint8_t MISO  = 12;
-static const uint8_t SCK   = 14;
+static const uint8_t MOSI  = 14;
+static const uint8_t MISO  = 15;
+static const uint8_t SCK   = 12;
 
 //I2C default pins
-static const uint8_t SDA = 2;
-static const uint8_t SCL = 4;
+static const uint8_t SDA = 33;
+static const uint8_t SCL = 32;
 
 #endif /* Pins_Arduino_h */
