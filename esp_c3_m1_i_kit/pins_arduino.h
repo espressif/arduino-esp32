4c6d23a1e59b51440d6e42704ab7ddaa4bf97a41
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2022-12-07 16:02:16 -0300

    Fixes GPIO definitions

--- esp_c3_m1_i_kit/pins_arduino.h
+++ esp_c3_m1_i_kit/pins_arduino.h
@@ -1,40 +1,50 @@
+/*
+   For more information about this board:
+   https://docs.ai-thinker.com/_media/esp32/docs/nodemcu-esp-c3-m1-i-kit_v1.2.0_specification.pdf
+*/
+
 #ifndef Pins_Arduino_h
 #define Pins_Arduino_h
 
 #include <stdint.h>
 
-#define EXTERNAL_NUM_INTERRUPTS 30
-#define NUM_DIGITAL_PINS        30
+#define EXTERNAL_NUM_INTERRUPTS 22
+#define NUM_DIGITAL_PINS        22
 #define NUM_ANALOG_INPUTS       6
 
 #define analogInputToDigitalPin(p)  (((p)<NUM_ANALOG_INPUTS)?(analogChannelToDigitalPin(p)):-1)
 #define digitalPinToInterrupt(p)    (((p)<NUM_DIGITAL_PINS)?(p):-1)
 #define digitalPinHasPWM(p)         (p < EXTERNAL_NUM_INTERRUPTS)
 
-static const uint8_t LED_BUILTIN = 18;
-static const uint8_t LED_BUILTIN = 19;
-#define BUILTIN_LED  LED_BUILTIN // backward compatibility
+// User LEDs are also connected to USB D- and D+
+static const uint8_t LED_WARM = 18;
+static const uint8_t LED_COLD = 19;
 
-static const uint8_t LED_GREEN = 4;
+// RGB LED 
 static const uint8_t LED_RED = 3;
+static const uint8_t LED_GREEN = 4;
 static const uint8_t LED_BLUE = 5;
 
-static const uint8_t TX = 18;
-static const uint8_t RX = 19;
+static const uint8_t LED_BUILTIN = LED_WARM;
+#define BUILTIN_LED  LED_BUILTIN // backward compatibility
 
-static const uint8_t SDA = 4;
-static const uint8_t SCL = 5;
+// Standard ESP32-C3 GPIOs
+static const uint8_t TX = 21;
+static const uint8_t RX = 20;
 
-static const uint8_t SS    = 23;
-static const uint8_t MOSI  = 21;
-static const uint8_t MISO  = 22;
-static const uint8_t SCK   = 28;
+static const uint8_t SDA = 8;
+static const uint8_t SCL = 9;
 
-static const uint8_t A0 = 1;
-static const uint8_t A1 = 2;
-static const uint8_t A2 = 4;
-static const uint8_t A3 = 5;
-static const uint8_t A4 = 6;
-static const uint8_t A5 = 13;
+static const uint8_t SS    = 7;
+static const uint8_t MOSI  = 6;
+static const uint8_t MISO  = 5;
+static const uint8_t SCK   = 4;
+
+static const uint8_t A0 = 0;
+static const uint8_t A1 = 1;
+static const uint8_t A2 = 2;
+static const uint8_t A3 = 3;
+static const uint8_t A4 = 4;
+static const uint8_t A5 = 5;
 
 #endif /* Pins_Arduino_h */
