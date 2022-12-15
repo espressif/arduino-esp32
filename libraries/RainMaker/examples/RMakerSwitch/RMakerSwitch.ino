8004a81260006ee0f062c36d54d8358419408792
Sanket Wadekar <67091512+sanketwadekar@users.noreply.github.com>
2022-12-15 16:16:58 +0530

    Rainmaker Example Changes (#7591)
    * Changed OTA type in Rainmaker examples
    
    * fix: Added patch in Rainmaker examples
    to prevent crashes during provisioning

--- libraries/RainMaker/examples/RMakerSwitch/RMakerSwitch.ino
+++ libraries/RainMaker/examples/RMakerSwitch/RMakerSwitch.ino
@@ -28,13 +28,19 @@ void sysProvEvent(arduino_event_t *sys_event)
     switch (sys_event->event_id) {
         case ARDUINO_EVENT_PROV_START:
 #if CONFIG_IDF_TARGET_ESP32S2
-        Serial.printf("\nProvisioning Started with name \"%s\" and PoP \"%s\" on SoftAP\n", service_name, pop);
-        printQR(service_name, pop, "softap");
+            Serial.printf("\nProvisioning Started with name \"%s\" and PoP \"%s\" on SoftAP\n", service_name, pop);
+            printQR(service_name, pop, "softap");
 #else
-        Serial.printf("\nProvisioning Started with name \"%s\" and PoP \"%s\" on BLE\n", service_name, pop);
-        printQR(service_name, pop, "ble");
+            Serial.printf("\nProvisioning Started with name \"%s\" and PoP \"%s\" on BLE\n", service_name, pop);
+            printQR(service_name, pop, "ble");
 #endif
-        break;
+            break;
+        case ARDUINO_EVENT_PROV_INIT:
+            wifi_prov_mgr_disable_auto_stop(10000);
+            break;
+        case ARDUINO_EVENT_PROV_CRED_SUCCESS:
+            wifi_prov_mgr_stop_provisioning();
+            break;
         default:;
     }
 }
@@ -72,7 +78,7 @@ void setup()
     my_node.addDevice(my_switch);
 
     //This is optional
-    RMaker.enableOTA(OTA_USING_PARAMS);
+    RMaker.enableOTA(OTA_USING_TOPICS);
     //If you want to enable scheduling, set time zone for your region using setTimeZone().
     //The list of available values are provided here https://rainmaker.espressif.com/docs/time-service.html
     // RMaker.setTimeZone("Asia/Shanghai");
