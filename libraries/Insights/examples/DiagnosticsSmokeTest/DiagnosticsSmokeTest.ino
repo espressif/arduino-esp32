227b36aec16c4be9e60497c45650a83b380ed67e
sanket.wadekar <sanket.wadekar@espressif.com>
2022-12-21 11:36:56 +0530

    Updated ESP Insights examples.

--- libraries/Insights/examples/DiagnosticsSmokeTest/DiagnosticsSmokeTest.ino
+++ libraries/Insights/examples/DiagnosticsSmokeTest/DiagnosticsSmokeTest.ino
@@ -36,7 +36,7 @@ static void smoke_test()
             if (s_reset_count > MAX_CRASHES) {
                 ESP_DIAG_EVENT(TAG, "[count][%d]", count);
             } else {
-               log_e("[count][%d] [crash_count][%"PRIu32"] [excvaddr][0x0f] Crashing...", count, s_reset_count);
+               log_e("[count][%d] [crash_count][%" PRIu32 "] [excvaddr][0x0f] Crashing...", count, s_reset_count);
                *(int *)0x0F = 0x10;
            }
         }
@@ -51,7 +51,7 @@ static void smoke_test()
             void *p = malloc(size);
             if (p) {
                 memset(p, size, 'A' + (esp_random() % 26));
-                log_i("Allocated %"PRIu32" bytes", size);
+                log_i("Allocated %" PRIu32 " bytes", size);
             }
             s_ptrs[count % MAX_PTRS] = p;
         } else {
@@ -70,10 +70,13 @@ void setup()
     Serial.begin(115200);
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASSPHRASE);
-    if(!WiFi.isConnected()) {
-        delay(2000);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
     }
-
+    Serial.println("");
+    Serial.println("WiFi connected");
+    
     esp_err_t ret = Insights.init(insights_auth_key, ESP_DIAG_LOG_TYPE_ERROR | ESP_DIAG_LOG_TYPE_WARNING | ESP_DIAG_LOG_TYPE_EVENT);
     if(ret != ESP_OK) {
         ESP.restart();
--- libraries/Insights/examples/MinimalDiagnostics/MinimalDiagnostics.ino
+++ libraries/Insights/examples/MinimalDiagnostics/MinimalDiagnostics.ino
@@ -12,9 +12,12 @@ void setup()
     Serial.begin(115200);
     WiFi.mode(WIFI_STA);
     WiFi.begin(WIFI_SSID, WIFI_PASSPHRASE);
-    if(!WiFi.isConnected()) {
-        delay(2000);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
     }
+    Serial.println("");
+    Serial.println("WiFi connected");
     esp_err_t ret = Insights.init(insights_auth_key, ESP_DIAG_LOG_TYPE_ERROR | ESP_DIAG_LOG_TYPE_WARNING | ESP_DIAG_LOG_TYPE_EVENT);
     if(ret != ESP_OK) {
         ESP.restart();
