a26c05e837a8074f5586624cfc0fafb5e83b0924
Gonzalo Brusco <gonzabrusco@gmail.com>
2022-12-23 13:41:39 -0300

    Added Bandwith setting method

--- libraries/WiFi/src/WiFiGeneric.h
+++ libraries/WiFi/src/WiFiGeneric.h
@@ -168,6 +168,9 @@ class WiFiGenericClass
 
     int32_t channel(void);
 
+    bool bandwidthSTA(wifi_bandwidth_t bandwidth);
+    bool bandwidthAP(wifi_bandwidth_t bandwidth);
+
     void persistent(bool persistent);
     void enableLongRange(bool enable);
 
