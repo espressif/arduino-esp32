a8df4613ee6b410ffc114bd4160542fedb89550b
me-no-dev <me-no-dev@users.noreply.github.com>
2022-12-25 07:11:25 +0000

    IDF release/v4.4 9ee3c8337d

--- tools/sdk/esp32s2/include/arduino_tinyusb/tinyusb/src/common/tusb_types.h
+++ tools/sdk/esp32s2/include/arduino_tinyusb/tinyusb/src/common/tusb_types.h
@@ -69,6 +69,15 @@ typedef enum
   TUSB_DIR_IN_MASK = 0x80
 }tusb_dir_t;
 
+enum
+{
+  TUSB_EPSIZE_BULK_FS = 64,
+  TUSB_EPSIZE_BULK_HS= 512,
+
+  TUSB_EPSIZE_ISO_FS_MAX = 1023,
+  TUSB_EPSIZE_ISO_HS_MAX = 1024,
+};
+
 /// Isochronous End Point Attributes
 typedef enum
 {
@@ -243,7 +252,6 @@ enum
   INTERFACE_INVALID_NUMBER = 0xff
 };
 
-
 typedef enum
 {
   MS_OS_20_SET_HEADER_DESCRIPTOR       = 0x00,
@@ -263,6 +271,11 @@ enum
   CONTROL_STAGE_SETUP,
   CONTROL_STAGE_DATA,
   CONTROL_STAGE_ACK
+};
+
+enum
+{
+  TUSB_INDEX_INVALID = 0xff
 };
 
 //--------------------------------------------------------------------+
--- tools/sdk/esp32s2/include/freemodbus/freemodbus/common/include/esp_modbus_common.h
+++ tools/sdk/esp32s2/include/freemodbus/freemodbus/common/include/esp_modbus_common.h
@@ -1,16 +1,7 @@
-/* Copyright 2018 Espressif Systems (Shanghai) PTE LTD
+/*
+ * SPDX-FileCopyrightText: 2016-2022 Espressif Systems (Shanghai) CO LTD
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 #ifndef _MB_IFACE_COMMON_H
@@ -24,6 +15,7 @@ extern "C" {
 
 #if __has_include("esp_check.h")
 #include "esp_check.h"
+#include "esp_log.h"
 
 #define MB_RETURN_ON_FALSE(a, err_code, tag, format, ...) ESP_RETURN_ON_FALSE(a, err_code, tag, format __VA_OPT__(,) __VA_ARGS__)
 
@@ -44,10 +36,10 @@ extern "C" {
 #define MB_CONTROLLER_PRIORITY              (CONFIG_FMB_PORT_TASK_PRIO - 1)    // priority of MB controller task
 
 // Default port defines
-#define MB_DEVICE_ADDRESS   (1)             // Default slave device address in Modbus
-#define MB_DEVICE_SPEED     (115200)        // Default Modbus speed for now hard defined
+#define MB_DEVICE_ADDRESS   (1)                 // Default slave device address in Modbus
+#define MB_DEVICE_SPEED     (115200)            // Default Modbus speed for now hard defined
 #define MB_UART_PORT        (UART_NUM_MAX - 1)  // Default UART port number
-#define MB_PAR_INFO_TOUT    (10)            // Timeout for get parameter info
+#define MB_PAR_INFO_TOUT    (10)                // Timeout for get parameter info
 #define MB_PARITY_NONE      (UART_PARITY_DISABLE)
 
 // The Macros below handle the endianness while transfer N byte data into buffer
