c46ab8c8d6f4746f3580246bf862de3cc2f2af2f
gayathri <gaya3.2507@gmail.com>
2023-01-06 11:26:15 +0100

    Updated the pins

--- variants/roboheart_hercules/pins_arduino.h
+++ variants/roboheart_hercules/pins_arduino.h
@@ -11,6 +11,38 @@
 #define digitalPinToInterrupt(p)    (((p)<40)?(p):-1)
 #define digitalPinHasPWM(p)         (p < 34)
 
+
+// Motor driver pins 
+#define MOTOR_A_IN1 25  //  PHASE/IN1
+#define MOTOR_A_IN2 26  //  ENABLE/IN2
+
+#define MOTOR_B_IN1 27  //  PHASE/IN1
+#define MOTOR_B_IN2 32  //  ENABLE/IN2
+
+#define MOTOR_C_IN1 33  //  PHASE/IN1
+#define MOTOR_C_IN2 4   //  ENABLE/IN2
+
+#define SLEEP_MOTOR_ABC 2  //  nSLEEP 
+
+#define LED_ROBOHEART 14  // Built in LED
+#define BUTTON_ROBOHEART 0 // Button
+
+// I2C IMU sensor
+#define IMU_SDA 21
+#define IMU_SCL 22
+
+#define RXD1 16
+#define TXD1 17
+
+// GSM Vela connector board pins
+#define GSM_PWRKEY 12
+#define GSM_DTR 13
+#define GSM_CTS 15
+#define GSM_RTS 14
+#define GSM_TX TXD1
+#define GSM_RX RXD1
+#define BATTERY_PIN 36  // Battery ADC pin 
+
 static const uint8_t TX = 35;
 static const uint8_t RX = 34;
 
