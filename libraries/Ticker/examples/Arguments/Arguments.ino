9faf13fee8bc1c99fd0c2331e1eb342b43ba6467
Tomas Pilny <tomas.pilny@espressif.com>
2023-01-09 12:28:34 +0100

    Fixed Ticker example

--- libraries/Ticker/examples/Arguments/Arguments.ino
+++ libraries/Ticker/examples/Arguments/Arguments.ino
@@ -7,8 +7,9 @@
 Ticker tickerSetHigh;
 Ticker tickerSetLow;
 
-void setPin(int state) {
-  digitalWrite(LED_PIN, state);
+// Argument to callback must always be passed a reference
+void setPin(int *state) {
+  digitalWrite(LED_PIN, *state);
 }
 
 void setup() {
@@ -16,10 +17,12 @@ void setup() {
   digitalWrite(1, LOW);
   
   // every 25 ms, call setPin(0) 
-  tickerSetLow.attach_ms(25, setPin, 0);
+  int state = 0;
+  tickerSetLow.attach_ms(25, setPin, &state);
   
   // every 26 ms, call setPin(1)
-  tickerSetHigh.attach_ms(26, setPin, 1);
+  state = 1;
+  tickerSetHigh.attach_ms(26, setPin, &state);
 }
 
 void loop() {
