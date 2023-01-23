3f2b231dbd028beaa8e1c461457ec029b2afe2b4
Tomas Pilny <tomas.pilny@espressif.com>
2023-01-23 14:17:10 +0100

    Added headers into .ino files

--- libraries/MultiThreading/examples/Mutex/Mutex.ino
+++ libraries/MultiThreading/examples/Mutex/Mutex.ino
@@ -1,3 +1,9 @@
+/* Basic Multi Threading Arduino Example
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
 // Please read file README.md in the folder containing this example.
 
 #define USE_MUTEX
