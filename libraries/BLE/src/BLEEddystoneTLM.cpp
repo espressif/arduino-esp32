50e1c7eaa95a9488abea8399ec2432137113dc4d
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2023-02-12 22:38:14 -0300

    Adds MACROs to convert beacon temperature
    2 Macros
    EDDYSTONE_TEMP_U16_TO_FLOAT(tempU16)  - takes the TLM BigEndian 8.8 fixed point representation and returns its float value 
    EDDYSTONE_TEMP_FLOAT_TO_U16(tempFloat)  - takes a float (temperature) and returns its BigEndian 8.8 fixed point representation

--- libraries/BLE/src/BLEEddystoneTLM.cpp
+++ libraries/BLE/src/BLEEddystoneTLM.cpp
@@ -19,6 +19,8 @@
 static const char LOG_TAG[] = "BLEEddystoneTLM";
 #define ENDIAN_CHANGE_U16(x) ((((x)&0xFF00)>>8) + (((x)&0xFF)<<8))
 #define ENDIAN_CHANGE_U32(x) ((((x)&0xFF000000)>>24) + (((x)&0x00FF0000)>>8)) + ((((x)&0xFF00)<<8) + (((x)&0xFF)<<24))
+#define EDDYSTONE_TEMP_U16_TO_FLOAT(tempU16)   (((int16_t)ENDIAN_CHANGE_U16(tempU16)) / 256.0f)
+#define EDDYSTONE_TEMP_FLOAT_TO_U16(tempFloat) (ENDIAN_CHANGE_U16(((int)(tempFloat * 256))))
 
 BLEEddystoneTLM::BLEEddystoneTLM() {
 	beaconUUID = 0xFEAA;
