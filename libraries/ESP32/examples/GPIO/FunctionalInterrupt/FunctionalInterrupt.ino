387e562abee9992118d7662b3ba2704f049fca94
Tomas Pilny <tomas.pilny@espressif.com>
2023-02-15 14:10:21 +0100

    Returned FunctionalInterrupts to original state + added modified version as new example

--- libraries/ESP32/examples/GPIO/FunctionalInterrupt/FunctionalInterrupt.ino
+++ libraries/ESP32/examples/GPIO/FunctionalInterrupt/FunctionalInterrupt.ino
@@ -1,39 +1,47 @@
 #include <Arduino.h>
+#include <FunctionalInterrupt.h>
 
 #define BUTTON1 16
 #define BUTTON2 17
 
-struct Button {
-  uint8_t PIN;
-  volatile uint32_t numberKeyPresses;
-  volatile int pressed;
+class Button
+{
+public:
+  Button(uint8_t reqPin) : PIN(reqPin){
+    pinMode(PIN, INPUT_PULLUP);
+    attachInterrupt(PIN, std::bind(&Button::isr,this), FALLING);
+  };
+  ~Button() {
+    detachInterrupt(PIN);
+  }
+
+  void ARDUINO_ISR_ATTR isr() {
+    numberKeyPresses += 1;
+    pressed = true;
+  }
+
+  void checkPressed() {
+    if (pressed) {
+      Serial.printf("Button on pin %u has been pressed %u times\n", PIN, numberKeyPresses);
+      pressed = false;
+    }
+  }
+
+private:
+  const uint8_t PIN;
+    volatile uint32_t numberKeyPresses;
+    volatile bool pressed;
 };
 
-void isr(void* param) {
-  struct Button *button = (struct Button*) param;
-  button->numberKeyPresses += 1;
-  button->pressed = 1;
-}
+Button button1(BUTTON1);
+Button button2(BUTTON2);
 
-void checkPressed(struct Button* button) {
-  if(button->pressed) {
-    Serial.printf("Button on pin %u has been pressed %u times\n", button->PIN, button->numberKeyPresses);
-    button->pressed = 0;
-  }
-}
-
-struct Button button1 = {BUTTON1, 0, 0};
-struct Button button2 = {BUTTON2, 0, 0};
 
 void setup() {
-  Serial.begin(115200);
-  pinMode(button1.PIN, INPUT_PULLUP);
-  pinMode(button2.PIN, INPUT_PULLUP);
-  attachInterruptArg(button1.PIN, isr, (void*)&button1, FALLING);
-  attachInterruptArg(button2.PIN, isr, (void*)&button2, FALLING);
+    Serial.begin(115200);
 }
 
 void loop() {
-  checkPressed(&button1);
-  checkPressed(&button2);
+  button1.checkPressed();
+  button2.checkPressed();
 }
