c6d97ed2d9b69c6f0847ecb35fd36d92f2bc1055
Gustavo Guerrero <13749076+gguerreromx@users.noreply.github.com>
2023-02-24 18:39:10 -0600

    UPD: heltec wsl v3 pinout
    ADD: proper versioning to the definition (added v3)
    FIX: TX/RX inversion.
    UPD: analog pins to be defined as a consecutive numeration.

--- variants/heltec_wireless_stick_lite_v3/pins_arduino.h
+++ variants/heltec_wireless_stick_lite_v3/pins_arduino.h
@@ -3,7 +3,7 @@
 
 #include <stdint.h>
 
-#define Wireless_Stick_Lite true
+#define Wireless_Stick_Lite_V3 true
 #define DISPLAY_HEIGHT 0
 #define DISPLAY_WIDTH  0
 
@@ -19,8 +19,8 @@ static const uint8_t LED_BUILTIN = 35;
 #define BUILTIN_LED  LED_BUILTIN // backward compatibility
 #define LED_BUILTIN LED_BUILTIN
 
-static const uint8_t TX = 44;
-static const uint8_t RX = 43;
+static const uint8_t TX = 43;
+static const uint8_t RX = 44;
 
 static const uint8_t SDA = 2;
 static const uint8_t SCL = 3;
@@ -37,14 +37,14 @@ static const uint8_t A3 = 4;
 static const uint8_t A4 = 5;
 static const uint8_t A5 = 6;
 static const uint8_t A6 = 7;
-static const uint8_t A10 = 12;
-static const uint8_t A12 = 14;
-static const uint8_t A13 = 15;
-static const uint8_t A14 = 16;
-static const uint8_t A15 = 17;
-static const uint8_t A16 = 18;
-static const uint8_t A17 = 19;
-static const uint8_t A18 = 20;
+static const uint8_t A7 = 12;
+static const uint8_t A8 = 14;
+static const uint8_t A9 = 15;
+static const uint8_t A10 = 16;
+static const uint8_t A11 = 17;
+static const uint8_t A12 = 18;
+static const uint8_t A13 = 19;
+static const uint8_t A14 = 20;
 
 static const uint8_t T0 = 1;
 static const uint8_t T1 = 2;
