acd86722fdd16073d269891905a59a9e21968f8f
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-02-28 13:22:00 +0100

    Updated timer HW test

--- tests/timer/timer.ino
+++ tests/timer/timer.ino
@@ -1,15 +1,23 @@
 /* HW Timer test */
 #include <unity.h>
 
-#define TIMER_DIVIDER  16
-#define TIMER_SCALE    (APB_CLK_FREQ / TIMER_DIVIDER)
+#define TIMER_FREQUENCY           5000000
+#define TIMER_FREQUENCY_XTAL_CLK  1000
 
-hw_timer_t * timer = NULL;
+/*
+ * ESP32 - APB clk only (1kHz not possible)
+ * C3 - APB + XTAL clk
+ * S2 - APB + XTAL clk
+ * S3 - APB + XTAL clk
+ */
+
+hw_timer_t timer = NULL;
+hw_timer_t timer_XTAL = NULL;
 static volatile bool alarm_flag;
 
 /* These functions are intended to be called before and after each test. */
 void setUp(void) {
-  timer = timerBegin(0, TIMER_DIVIDER, true);
+  timer = timerBegin(TIMER_FREQUENCY, true);
   timerStop(timer);
   timerRestart(timer);
 }
@@ -28,9 +36,8 @@ void ARDUINO_ISR_ATTR onTimer(){
 void timer_interrupt_test(void){
   
   alarm_flag = false;
-  timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, (1.2 * TIMER_SCALE), true);
-  timerAlarmEnable(timer);
+  timerAttachInterrupt(timer, &onTimer);
+  timerAlarmWrite(timer, (1.2 * TIMER_FREQUENCY), true, 0);
   timerStart(timer);
   
   delay(2000);
@@ -40,7 +47,7 @@ void timer_interrupt_test(void){
   timerStop(timer);
   timerRestart(timer);
   alarm_flag = false;
-  timerAlarmDisable(timer);
+  timerDetachInterrupt(timer);
   timerStart(timer);
 
   delay(2000);
@@ -58,10 +65,9 @@ void timer_divider_test(void){
   time_val = timerRead(timer);
 
   // compare divider  16 and 8, value should be double
-  timerStop(timer);
-  timerSetDivider(timer,8);
+  timerEnd(timer);
+  timer = timerBegin(2 * TIMER_FREQUENCY, true);
   timerRestart(timer);
-  timerStart(timer);
 
   delay(1000);        
   comp_time_val = timerRead(timer);
@@ -70,14 +76,13 @@ void timer_divider_test(void){
   TEST_ASSERT_INT_WITHIN(10000, 10000000, comp_time_val);
 
   // divider is 256, value should be 2^4
-  timerStop(timer);
-  timerSetDivider(timer,256);
+  timerEnd(timer);
+  timer = timerBegin(TIMER_FREQUENCY / 16, true);
   timerRestart(timer);
-  timerStart(timer);
   delay(1000);       
   comp_time_val = timerRead(timer);
   TEST_ASSERT_INT_WITHIN(5000, 5000000, time_val);
-  TEST_ASSERT_INT_WITHIN(3126, 312500, comp_time_val);
+  TEST_ASSERT_INT_WITHIN(3125, 312500, comp_time_val);
 }
 
 void timer_read_test(void){
@@ -85,10 +90,20 @@ void timer_read_test(void){
   uint64_t set_timer_val = 0xFF;
   uint64_t get_timer_val = 0;
 
-  timerWrite(timer,set_timer_val);
+  timerWrite(timer, set_timer_val);
   get_timer_val = timerRead(timer);
 
   TEST_ASSERT_EQUAL(set_timer_val, get_timer_val);
+}
+
+void timer_clock_select_test(void){
+  // Set timer frequency that can be achieved using XTAL clock source (autoselected)
+  timer_XTAL = timerBegin(TIMER_FREQUENCY_XTAL_CLK, true);
+  
+  uint32_t resolution = timerGetResolution(timer_XTAL);
+  TEST_ASSERT_EQUAL(TIMER_FREQUENCY_XTAL_CLK,resolution);
+  
+  timerEnd(timer_XTAL);
 }
 
 void setup(){
@@ -103,6 +118,9 @@ void setup(){
   RUN_TEST(timer_read_test);
   RUN_TEST(timer_interrupt_test);
   RUN_TEST(timer_divider_test);
+  #if !CONFIG_IDF_TARGET_ESP32
+  RUN_TEST(timer_clock_select_test);
+  #endif
   UNITY_END();
 }
 
