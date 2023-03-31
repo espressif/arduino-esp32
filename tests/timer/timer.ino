73d2862863b5261457ffeaf6bd6438c39bbd2a76
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-03-31 14:15:11 +0200

    TIMER refactoring (#7904)
    * refactor using GPtimer
    
    * Updated timer HW test
    
    * fix examples
    
    * Add v2.0.7 in issue template (#7871)
    
    * refactor using GPtimer
    
    * Updated timer HW test
    
    * fix examples
    
    * Revert "Add v2.0.7 in issue template (#7871)"
    
    This reverts commit fcc3b17d62ff57861f8913ca1f142fd5163b7457.
    
    * Update upload-artifact to v3 in HW CI
    
    * Revert "Update upload-artifact to v3 in HW CI"
    
    This reverts commit 1ba228071718ba37c4e26d98db22f77b2a7364a6.
    
    * replace resolution with frequency
    
    * remove count_down option
    
    * countup removed from examples + header
    
    * Refactored timer object
    
    * code cleanup + examples and tests fixes
    
    * TimerAlarm fix
    
    ---------
    
    Co-authored-by: Vojtěch Bartoška <76958047+VojtechBartoska@users.noreply.github.com>

--- tests/timer/timer.ino
+++ tests/timer/timer.ino
@@ -1,21 +1,30 @@
 /* HW Timer test */
 #include <unity.h>
 
-#define TIMER_DIVIDER  16
-#define TIMER_SCALE    (APB_CLK_FREQ / TIMER_DIVIDER)
+#define TIMER_FREQUENCY           5000000
+#define TIMER_FREQUENCY_XTAL_CLK  1000
+
+/*
+ * ESP32 - APB clk only (1kHz not possible)
+ * C3 - APB + XTAL clk
+ * S2 - APB + XTAL clk
+ * S3 - APB + XTAL clk
+ */
 
 hw_timer_t * timer = NULL;
+hw_timer_t * timer_XTAL = NULL;
 static volatile bool alarm_flag;
 
 /* These functions are intended to be called before and after each test. */
 void setUp(void) {
-  timer = timerBegin(0, TIMER_DIVIDER, true);
+  timer = timerBegin(TIMER_FREQUENCY);
   timerStop(timer);
   timerRestart(timer);
 }
 
 void tearDown(void){
   timerEnd(timer);
+  timer = NULL;
 }
 
 
@@ -28,9 +37,8 @@ void ARDUINO_ISR_ATTR onTimer(){
 void timer_interrupt_test(void){
   
   alarm_flag = false;
-  timerAttachInterrupt(timer, &onTimer, true);
-  timerAlarmWrite(timer, (1.2 * TIMER_SCALE), true);
-  timerAlarmEnable(timer);
+  timerAttachInterrupt(timer, &onTimer);
+  timerAlarm(timer, (1.2 * TIMER_FREQUENCY), true, 0);
   timerStart(timer);
   
   delay(2000);
@@ -40,7 +48,7 @@ void timer_interrupt_test(void){
   timerStop(timer);
   timerRestart(timer);
   alarm_flag = false;
-  timerAlarmDisable(timer);
+  timerDetachInterrupt(timer);
   timerStart(timer);
 
   delay(2000);
@@ -58,11 +66,11 @@ void timer_divider_test(void){
   time_val = timerRead(timer);
 
   // compare divider  16 and 8, value should be double
-  timerStop(timer);
-  timerSetDivider(timer,8);
-  timerRestart(timer);
-  timerStart(timer);
+  timerEnd(timer);
+  timer = NULL;
 
+  timer = timerBegin(2 * TIMER_FREQUENCY);
+  timerRestart(timer);
   delay(1000);        
   comp_time_val = timerRead(timer);
     
@@ -70,14 +78,16 @@ void timer_divider_test(void){
   TEST_ASSERT_INT_WITHIN(10000, 10000000, comp_time_val);
 
   // divider is 256, value should be 2^4
-  timerStop(timer);
-  timerSetDivider(timer,256);
+  timerEnd(timer);
+  timer = NULL;
+
+  timer = timerBegin(TIMER_FREQUENCY / 16);
   timerRestart(timer);
-  timerStart(timer);
   delay(1000);       
   comp_time_val = timerRead(timer);
+
   TEST_ASSERT_INT_WITHIN(5000, 5000000, time_val);
-  TEST_ASSERT_INT_WITHIN(3126, 312500, comp_time_val);
+  TEST_ASSERT_INT_WITHIN(3125, 312500, comp_time_val);
 }
 
 void timer_read_test(void){
@@ -85,10 +95,21 @@ void timer_read_test(void){
   uint64_t set_timer_val = 0xFF;
   uint64_t get_timer_val = 0;
 
-  timerWrite(timer,set_timer_val);
+  timerWrite(timer, set_timer_val);
   get_timer_val = timerRead(timer);
 
   TEST_ASSERT_EQUAL(set_timer_val, get_timer_val);
+}
+
+void timer_clock_select_test(void){
+  // Set timer frequency that can be achieved using XTAL clock source (autoselected)
+  timer_XTAL = timerBegin(TIMER_FREQUENCY_XTAL_CLK);
+  
+  uint32_t resolution = timerGetFrequency(timer_XTAL);
+  TEST_ASSERT_EQUAL(TIMER_FREQUENCY_XTAL_CLK,resolution);
+  
+  timerEnd(timer_XTAL);
+  timer_XTAL = NULL;
 }
 
 void setup(){
@@ -103,6 +124,9 @@ void setup(){
   RUN_TEST(timer_read_test);
   RUN_TEST(timer_interrupt_test);
   RUN_TEST(timer_divider_test);
+  #if !CONFIG_IDF_TARGET_ESP32
+  RUN_TEST(timer_clock_select_test);
+  #endif
   UNITY_END();
 }
 
