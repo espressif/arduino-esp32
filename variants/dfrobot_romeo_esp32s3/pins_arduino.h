08463516ff3a17f2e881d65ae44c35d04a6c676c
fary99 <fary_young@outlook.com>
2023-05-12 16:35:52 +0800

    add dfrobot romeo esp32s3

--- variants/dfrobot_romeo_esp32s3/pins_arduino.h
+++ variants/dfrobot_romeo_esp32s3/pins_arduino.h
@@ -25,8 +25,6 @@ static const uint8_t MOSI  = 15;
 static const uint8_t MISO  = 16;
 static const uint8_t SCK   = 17;
 
-
-
 #define GDI_DISPLAY_FPC_INTERFACE
 #ifdef  GDI_DISPLAY_FPC_INTERFACE
 
@@ -44,6 +42,40 @@ static const uint8_t SCK   = 17;
 #define GDI_SDA       SDA
 #define GDI_INT       13
 #define GDI_BUSY_TE   14
+
+// CAM
+#define CAM_DVP_INTERFACE
+#ifdef  CAM_DVP_INTERFACE
+
+#define CAM_D5         4
+#define CAM_PCLK       5
+#define CAM_VSYNC      6
+#define CAM_D6         7
+#define CAM_D7         8
+#define CAM_D8         46
+#define CAM_D9         48
+#define CAM_XMCLK      45
+#define CAM_D2         39
+#define CAM_D3         40
+#define CAM_D4         41
+#define CAM_HREF       42
+#define CAM_SCL        SCL
+#define CAM_SDA        SDA
+
+#endif /* CAM_DVP_INTERFACE */
+
+// Motor
+#define MOTOR_INTERFACE
+#ifdef  MOTOR_INTERFACE
+
+#define M1_EN          12
+#define M1_PH          13
+#define M2_EN          14
+#define M2_PH          21
+#define M3_EN          9
+#define M3_PH          10
+#define M4_EN          47
+#define M4_PH          11
 
 #endif
 
