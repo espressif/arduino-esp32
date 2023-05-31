c38226587073adcb6c4d4ded08b46e616121d73c
杨峰 <71751335+fary99@users.noreply.github.com>
2023-05-31 19:32:41 +0800

    I have added support for the DFRobot Romeo ESP32-S3 board. (#8195)
    * add dfrobot romeo esp32S3
    
    * add dfrobot romeo esp32s3
    
    * Update pins_arduino.h
    
    * Update pins_arduino.h
    
    * Update pins_arduino.h
    
    * Update pins_arduino.h

--- libraries/ESP32/examples/Camera/CameraWebServer/camera_pins.h
+++ libraries/ESP32/examples/Camera/CameraWebServer/camera_pins.h
@@ -293,6 +293,25 @@
 #define HREF_GPIO_NUM 7
 #define PCLK_GPIO_NUM 13
 
+#elif defined(CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3) || defined(CAMERA_MODEL_DFRobot_Romeo_ESP32S3)
+#define PWDN_GPIO_NUM     -1
+#define RESET_GPIO_NUM    -1
+#define XCLK_GPIO_NUM     45
+#define SIOD_GPIO_NUM     1
+#define SIOC_GPIO_NUM     2
+
+#define Y9_GPIO_NUM       48
+#define Y8_GPIO_NUM       46
+#define Y7_GPIO_NUM       8
+#define Y6_GPIO_NUM       7
+#define Y5_GPIO_NUM       4
+#define Y4_GPIO_NUM       41
+#define Y3_GPIO_NUM       40
+#define Y2_GPIO_NUM       39
+#define VSYNC_GPIO_NUM    6
+#define HREF_GPIO_NUM     42
+#define PCLK_GPIO_NUM     5
+
 #else
 #error "Camera model not selected"
 #endif
