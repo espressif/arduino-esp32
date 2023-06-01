7e9e4756285611466a8f2d805c5a2d23ade4f8bd
me-no-dev <hristo@espressif.com>
2023-06-02 00:27:33 +0300

    Print full chip report when log level is sufficient

---
+++ cores/esp32/chip-debug-report.h
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: 2019-2023 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+#pragma once
+
+void printBeforeSetupInfo(void);
+void printAfterSetupInfo(void);
