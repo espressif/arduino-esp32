7b9d0e8ed43a04a23aa238875ec1088a08c239f3
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-06-26 11:38:12 +0200

    fix i2c compilation error

--- cores/esp32/esp32-hal-i2c-slave.c
+++ cores/esp32/esp32-hal-i2c-slave.c
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "esp32-hal-i2c-slave.h"
+#include "soc/soc_caps.h"
 
 #if SOC_I2C_SUPPORT_SLAVE
 #include <stdint.h>
@@ -44,6 +44,7 @@
 #include "hal/i2c_ll.h"
 #include "hal/clk_gate_ll.h"
 #include "esp32-hal-log.h"
+#include "esp32-hal-i2c-slave.h"
 #include "esp32-hal-periman.h"
 
 #define I2C_SLAVE_USE_RX_QUEUE 0 // 1: Queue, 0: RingBuffer
