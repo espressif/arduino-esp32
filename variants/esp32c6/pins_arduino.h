fe27a1f326bd4113d482ccbc1ecc0ecd3200ea1d
me-no-dev <hristo@espressif.com>
2023-07-03 12:06:38 +0300

    Fix GPIO Configs for ESP32-C6 and ESP32-H2

--- variants/esp32c6/pins_arduino.h
+++ variants/esp32c6/pins_arduino.h
@@ -4,9 +4,9 @@
 #include <stdint.h>
 #include "soc/soc_caps.h"
 
-#define EXTERNAL_NUM_INTERRUPTS 22
-#define NUM_DIGITAL_PINS        22
-#define NUM_ANALOG_INPUTS       6
+#define EXTERNAL_NUM_INTERRUPTS 24
+#define NUM_DIGITAL_PINS        24
+#define NUM_ANALOG_INPUTS       7
 
 static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT+8;
 #define BUILTIN_LED  LED_BUILTIN // backward compatibility
@@ -18,16 +18,16 @@ static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT+8;
 #define digitalPinToInterrupt(p)    (((p)<NUM_DIGITAL_PINS)?(p):-1)
 #define digitalPinHasPWM(p)         (p < EXTERNAL_NUM_INTERRUPTS)
 
-static const uint8_t TX = 21;
-static const uint8_t RX = 20;
+static const uint8_t TX = 16;
+static const uint8_t RX = 17;
 
-static const uint8_t SDA = 8;
-static const uint8_t SCL = 9;
+static const uint8_t SDA = 23;
+static const uint8_t SCL = 22;
 
-static const uint8_t SS    = 7;
-static const uint8_t MOSI  = 6;
-static const uint8_t MISO  = 5;
-static const uint8_t SCK   = 4;
+static const uint8_t SS    = 18;
+static const uint8_t MOSI  = 19;
+static const uint8_t MISO  = 20;
+static const uint8_t SCK   = 21;
 
 static const uint8_t A0 = 0;
 static const uint8_t A1 = 1;
@@ -35,5 +35,6 @@ static const uint8_t A2 = 2;
 static const uint8_t A3 = 3;
 static const uint8_t A4 = 4;
 static const uint8_t A5 = 5;
+static const uint8_t A6 = 6;
 
 #endif /* Pins_Arduino_h */
--- variants/esp32h2/pins_arduino.h
+++ variants/esp32h2/pins_arduino.h
@@ -4,9 +4,9 @@
 #include <stdint.h>
 #include "soc/soc_caps.h"
 
-#define EXTERNAL_NUM_INTERRUPTS 22
-#define NUM_DIGITAL_PINS        22
-#define NUM_ANALOG_INPUTS       6
+#define EXTERNAL_NUM_INTERRUPTS 28
+#define NUM_DIGITAL_PINS        28
+#define NUM_ANALOG_INPUTS       5
 
 static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT+8;
 #define BUILTIN_LED  LED_BUILTIN // backward compatibility
@@ -18,22 +18,21 @@ static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT+8;
 #define digitalPinToInterrupt(p)    (((p)<NUM_DIGITAL_PINS)?(p):-1)
 #define digitalPinHasPWM(p)         (p < EXTERNAL_NUM_INTERRUPTS)
 
-static const uint8_t TX = 21;
-static const uint8_t RX = 20;
+static const uint8_t TX = 24;
+static const uint8_t RX = 23;
 
-static const uint8_t SDA = 8;
-static const uint8_t SCL = 9;
+static const uint8_t SDA = 12;
+static const uint8_t SCL = 22;
 
-static const uint8_t SS    = 7;
-static const uint8_t MOSI  = 6;
-static const uint8_t MISO  = 5;
-static const uint8_t SCK   = 4;
+static const uint8_t SS    = 0;
+static const uint8_t MOSI  = 25;
+static const uint8_t MISO  = 11;
+static const uint8_t SCK   = 10;
 
-static const uint8_t A0 = 0;
-static const uint8_t A1 = 1;
-static const uint8_t A2 = 2;
-static const uint8_t A3 = 3;
-static const uint8_t A4 = 4;
-static const uint8_t A5 = 5;
+static const uint8_t A0 = 1;
+static const uint8_t A1 = 2;
+static const uint8_t A2 = 3;
+static const uint8_t A3 = 4;
+static const uint8_t A4 = 5;
 
 #endif /* Pins_Arduino_h */
