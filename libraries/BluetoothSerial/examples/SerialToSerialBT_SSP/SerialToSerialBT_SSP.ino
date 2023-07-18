e2ec74db4c9da1ec722f3524b76e45c9c8247b57
Tomas Pilny <tomas.pilny@espressif.com>
2023-07-18 10:24:11 +0200

    Renamed SerialToSerialBT_SSP_pairing

--- libraries/BluetoothSerial/examples/SerialToSerialBT_SSP/SerialToSerialBT_SSP.ino
+++ libraries/BluetoothSerial/examples/SerialToSerialBT_SSP/SerialToSerialBT_SSP.ino
@@ -1,8 +1,11 @@
-//This example code is in the Public Domain (or CC0 licensed, at your option.)
-//By Richard Li - 2020
+// This example code is in the Public Domain (or CC0 licensed, at your option.)
+// By Richard Li - 2020
 //
-//This example creates a bridge between Serial and Classical Bluetooth (SPP with authentication)
-//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+// This example creates a bridge between Serial and Classical Bluetooth (SPP with authentication)
+// and also demonstrate that SerialBT have the same functionalities of a normal Serial
+// SSP - Simple Secure Pairing - The device (ESP32) will display random number and the user is responsible of comparing it to the number
+// displayed on the other device (for example phone).
+// If the numbers match the user authenticates the pairing.
 
 #include "BluetoothSerial.h"
 
@@ -14,13 +17,15 @@
 #error Serial Bluetooth not available or not enabled. It is only available for the ESP32 chip.
 #endif
 
+const char * deviceName = "ESP32_SSP_example";
+
 BluetoothSerial SerialBT;
 boolean confirmRequestPending = true;
 
 void BTConfirmRequestCallback(uint32_t numVal)
 {
   confirmRequestPending = true;
-  Serial.println(numVal);
+  Serial.printf("The PIN is: %lu\n", numVal);
 }
 
 void BTAuthCompleteCallback(boolean success)
@@ -43,8 +48,8 @@ void setup()
   SerialBT.enableSSP();
   SerialBT.onConfirmRequest(BTConfirmRequestCallback);
   SerialBT.onAuthComplete(BTAuthCompleteCallback);
-  SerialBT.begin("ESP32test"); //Bluetooth device name
-  Serial.println("The device started, now you can pair it with bluetooth!");
+  SerialBT.begin(deviceName); //Bluetooth device name
+  Serial.printf("The device started with name \"%s\", now you can pair it with Bluetooth!\n", deviceName);
 }
 
 void loop()
