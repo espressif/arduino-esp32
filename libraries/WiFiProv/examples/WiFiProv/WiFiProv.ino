886a74ce60d488da54fde6a1b9dc328690cc48b5
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2023-08-03 11:50:25 -0300

    fixes preprocessor test
    When using `#define USE_SOFT_AP` 
    Change
    `&& not USE_SOFT_AP` ==> `&& !defined(USE_SOFT_AP)`

--- libraries/WiFiProv/examples/WiFiProv/WiFiProv.ino
+++ libraries/WiFiProv/examples/WiFiProv/WiFiProv.ino
@@ -62,7 +62,7 @@ void setup() {
   Serial.begin(115200);
   WiFi.onEvent(SysProvEvent);
 
-#if CONFIG_IDF_TARGET_ESP32 && CONFIG_BLUEDROID_ENABLED && not USE_SOFT_AP
+#if CONFIG_IDF_TARGET_ESP32 && CONFIG_BLUEDROID_ENABLED && !defined(USE_SOFT_AP)
     Serial.println("Begin Provisioning using BLE");
     // Sample uuid that user can pass during provisioning using BLE
     uint8_t uuid[16] = {0xb4, 0xdf, 0x5a, 0x1c, 0x3f, 0x6b, 0xf4, 0xbf,
@@ -73,7 +73,7 @@ void setup() {
     WiFiProv.beginProvision(WIFI_PROV_SCHEME_SOFTAP, WIFI_PROV_SCHEME_HANDLER_NONE, WIFI_PROV_SECURITY_1, pop, service_name, service_key);
 #endif
 
-  #if CONFIG_BLUEDROID_ENABLED && not USE_SOFT_AP
+  #if CONFIG_BLUEDROID_ENABLED && !defined(USE_SOFT_AP)
     log_d("ble qr");
     WiFiProv.printQR(service_name, pop, "ble");
   #else
