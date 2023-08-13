c8cdbd2040125cb64a542476e827681f8710d544
Holger Lembke <lembke@gmail.com>
2023-08-13 20:52:24 +0200

    Fixing indention/tabs

--- libraries/SD/src/SD.cpp
+++ libraries/SD/src/SD.cpp
@@ -95,9 +95,9 @@ uint64_t SDFS::totalBytes()
 {
 	FATFS* fsinfo;
 	DWORD fre_clust;
-    char drv[3] = {(char)(48+_pdrv), ':', 0};
+	char drv[3] = {(char)(48+_pdrv), ':', 0};
 	if(f_getfree(drv,&fre_clust,&fsinfo)!= 0) return 0;
-    uint64_t size = ((uint64_t)(fsinfo->csize))*(fsinfo->n_fatent - 2)
+	uint64_t size = ((uint64_t)(fsinfo->csize))*(fsinfo->n_fatent - 2)
 #if _MAX_SS != 512
         *(fsinfo->ssize);
 #else
@@ -110,7 +110,7 @@ uint64_t SDFS::usedBytes()
 {
 	FATFS* fsinfo;
 	DWORD fre_clust;
-    char drv[3] = {(char)(48+_pdrv), ':', 0};
+	char drv[3] = {(char)(48+_pdrv), ':', 0};
 	if(f_getfree(drv,&fre_clust,&fsinfo)!= 0) return 0;
 	uint64_t size = ((uint64_t)(fsinfo->csize))*((fsinfo->n_fatent - 2) - (fsinfo->free_clst))
 #if _MAX_SS != 512
