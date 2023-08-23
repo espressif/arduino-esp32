3da0cc1cd02fec6b2be69dbead33f1ef7d52700e
me-no-dev <me-no-dev@users.noreply.github.com>
2023-08-23 12:59:55 +0000

    IDF release/v4.4 5f0ac58e1b

--- tools/platformio-build-esp32s3.py
+++ tools/platformio-build-esp32s3.py
@@ -327,7 +327,7 @@ env.Append(
         "UNITY_INCLUDE_CONFIG_H",
         "WITH_POSIX",
         "_GNU_SOURCE",
-        ("IDF_VER", '\\"v4.4.5-300-ga49e0180ee\\"'),
+        ("IDF_VER", '\\"v4.4.5-314-g5f0ac58e1b\\"'),
         "ESP_PLATFORM",
         "_POSIX_READER_WRITER_LOCKS",
         "ARDUINO_ARCH_ESP32",
--- tools/sdk/esp32/include/esp_littlefs/include/esp_littlefs.h
+++ tools/sdk/esp32/include/esp_littlefs/include/esp_littlefs.h
@@ -4,20 +4,20 @@
 #include "esp_err.h"
 #include "esp_idf_version.h"
 #include <stdbool.h>
+#include "esp_partition.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-#define ESP_LITTLEFS_VERSION_NUMBER "1.7.0"
+#define ESP_LITTLEFS_VERSION_NUMBER "1.8.0"
 #define ESP_LITTLEFS_VERSION_MAJOR 1
-#define ESP_LITTLEFS_VERSION_MINOR 7
+#define ESP_LITTLEFS_VERSION_MINOR 8
 #define ESP_LITTLEFS_VERSION_PATCH 0
 
 #if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 2) && CONFIG_VFS_SUPPORT_DIR
 #define ESP_LITTLEFS_ENABLE_FTRUNCATE
 #endif // ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(4, 4, 2)
-
 
 /**
  *Configuration structure for esp_vfs_littlefs_register.
@@ -25,7 +25,9 @@ extern "C" {
 typedef struct {
     const char *base_path;            /**< Mounting point. */
     const char *partition_label;      /**< Label of partition to use. */
+    const esp_partition_t* partition; /**< partition to use if partition_label is NULL */
     uint8_t format_if_mount_failed:1; /**< Format the file system if it fails to mount. */
+    uint8_t read_only : 1;            /**< Mount the partition as read-only. */
     uint8_t dont_mount:1;             /**< Don't attempt to mount or format. Overrides format_if_mount_failed */
 } esp_vfs_littlefs_conf_t;
 
@@ -55,6 +57,17 @@ esp_err_t esp_vfs_littlefs_register(const esp_vfs_littlefs_conf_t * conf);
 esp_err_t esp_vfs_littlefs_unregister(const char* partition_label);
 
 /**
+ * Unregister and unmount littlefs from VFS
+ *
+ * @param partition  partition to unregister.
+ *
+ * @return  
+ *          - ESP_OK if successful
+ *          - ESP_ERR_INVALID_STATE already unregistered
+ */
+esp_err_t esp_vfs_littlefs_unregister_partition(const esp_partition_t* partition);
+
+/**
  * Check if littlefs is mounted
  *
  * @param partition_label  Label of the partition to check.
@@ -64,6 +77,17 @@ esp_err_t esp_vfs_littlefs_unregister(const char* partition_label);
  *          - false   if not mounted
  */
 bool esp_littlefs_mounted(const char* partition_label);
+
+/**
+ * Check if littlefs is mounted
+ *
+ * @param partition  partition to check.
+ *
+ * @return  
+ *          - true    if mounted
+ *          - false   if not mounted
+ */
+bool esp_littlefs_partition_mounted(const esp_partition_t* partition);
 
 /**
  * Format the littlefs partition
@@ -76,9 +100,32 @@ bool esp_littlefs_mounted(const char* partition_label);
 esp_err_t esp_littlefs_format(const char* partition_label);
 
 /**
+ * Format the littlefs partition
+ *
+ * @param partition  partition to format.
+ * @return
+ *          - ESP_OK      if successful
+ *          - ESP_FAIL    on error
+ */
+esp_err_t esp_littlefs_format_partition(const esp_partition_t* partition);
+
+/**
  * Get information for littlefs
  *
  * @param partition_label           Optional, label of the partition to get info for.
+ * @param[out] total_bytes          Size of the file system
+ * @param[out] used_bytes           Current used bytes in the file system
+ *
+ * @return
+ *          - ESP_OK                  if success
+ *          - ESP_ERR_INVALID_STATE   if not mounted
+ */
+esp_err_t esp_littlefs_info(const char* partition_label, size_t* total_bytes, size_t* used_bytes);
+
+/**
+ * Get information for littlefs
+ *
+ * @param parition                  the partition to get info for.
  * @param[out] total_bytes          Size of the file system
  * @param[out] used_bytes           Current used bytes in the file system
  *
@@ -86,7 +133,7 @@ esp_err_t esp_littlefs_format(const char* partition_label);
  *          - ESP_OK                  if success
  *          - ESP_ERR_INVALID_STATE   if not mounted
  */
-esp_err_t esp_littlefs_info(const char* partition_label, size_t *total_bytes, size_t *used_bytes);
+esp_err_t esp_littlefs_partition_info(const esp_partition_t* partition, size_t *total_bytes, size_t *used_bytes);
 
 #ifdef __cplusplus
 } // extern "C"
