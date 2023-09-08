5da67c14100cfd9ce446203b996c08ff277aeebd
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2023-09-08 12:21:56 -0300

    Update cores/esp32/HardwareSerial.cpp
    Co-authored-by: Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>

--- cores/esp32/HardwareSerial.cpp
+++ cores/esp32/HardwareSerial.cpp
@@ -569,10 +569,10 @@ bool HardwareSerial::setPins(int8_t rxPin, int8_t txPin, int8_t ctsPin, int8_t r
     bool retCode = uartSetPins(_uart, rxPin, txPin, ctsPin, rtsPin);
     if (retCode) {
 	// detach previous attached UART pins if not set as same as before
-	if (_rxPin > 0 && _rxPin != rxPin) uartDetachPins(_uart, _rxPin, -1, -1, -1);
-	if (_txPin > 0 && _txPin != txPin) uartDetachPins(_uart, -1, _txPin, -1, -1);
-	if (_ctsPin > 0 && _ctsPin != ctsPin) uartDetachPins(_uart, -1, -1, _ctsPin, -1);
-	if (_rtsPin > 0 && _rtsPin != rtsPin) uartDetachPins(_uart, -1, -1, -1, _rtsPin);
+	if (_rxPin >= 0 && _rxPin != rxPin) uartDetachPins(_uart, _rxPin, -1, -1, -1);
+	if (_txPin >= 0 && _txPin != txPin) uartDetachPins(_uart, -1, _txPin, -1, -1);
+	if (_ctsPin >= 0 && _ctsPin != ctsPin) uartDetachPins(_uart, -1, -1, _ctsPin, -1);
+	if (_rtsPin >= 0 && _rtsPin != rtsPin) uartDetachPins(_uart, -1, -1, -1, _rtsPin);
 	// set new pins for a future end() or a setPins()    
         _txPin = txPin >= 0 ? txPin : _txPin;
         _rxPin = rxPin >= 0 ? rxPin : _rxPin;
