a3d05da71b40eb2800706a2c012172e6d3b1ee96
pedrominatel <pminatel@gmail.com>
2023-09-19 15:22:44 +0100

    Added CI for deploy docs

--- docs/_static/docs_version.js
+++ docs/_static/docs_version.js
@@ -11,6 +11,6 @@ var DOCUMENTATION_VERSIONS = {
        { text: "ESP32-S3", value: "esp32s3"},
        { text: "ESP32-C3", value: "esp32c3"},
        { text: "ESP32-H2", value: "esp32h2"},
-       { text: "ESP32C6", value: "esp32c6"},
+       { text: "ESP32-C6", value: "esp32c6"},
     ]
 };
