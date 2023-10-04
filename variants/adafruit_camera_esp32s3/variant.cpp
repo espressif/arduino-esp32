a0b5a0936e6a6c6dbee2aa281c611e98c23539d2
Melissa LeBlanc-Williams <melissa@adafruit.com>
2023-10-04 10:01:41 -0700

    Add Additional Adafruit boards including the new Qualia ESP32-S3 and Camera ESP32-S3 (#8696)
    * Add MatrixPortal ESP32-S3 (WIP)
    
    * Update bootloader files, etc.
    
    * Add MatrixPortal S3 PSRAM setup to boards.txt
    
    * fix neopixel pin
    
    * initialize bsp
    
    * ?
    
    * new variant for tft board with RGB666 40-pin display contact
    
    * fix menu name
    
    * fix neopixel pins
    
    * more pin defs
    
    * second bootloader file
    
    * fix partition
    
    * Update variants/adafruit_camera_esp32s3/pins_arduino.h
    
    Co-authored-by: Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
    
    * Update variants/adafruit_qualia_s3_rgb666/pins_arduino.h
    
    Co-authored-by: Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
    
    * add some #'s
    
    * hardware design mistakes? in this economy? now we can use PSRAM and SPI at the same time :)
    
    ---------
    
    Co-authored-by: Phillip Burgess <paintyourdragon@dslextreme.com>
    Co-authored-by: Limor "Ladyada" Fried <limor@ladyada.net>
    Co-authored-by: Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>

--- variants/adafruit_camera_esp32s3/variant.cpp
+++ variants/adafruit_camera_esp32s3/variant.cpp
@@ -29,10 +29,8 @@
 extern "C" {
 
 // Initialize variant/board, called before setup()
-void initVariant(void) {
-  // default SD_CS to input pullup (we cannot have built in pullup since its
-  // a strapping pin!)
-  pinMode(SS, INPUT_PULLUP);
-}
+void initVariant(void)
+{
 
+}
 }
