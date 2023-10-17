26f5909d7e83ae3736ef93cd3028cac2dd3a3a6b
uPesy-computer <alexis@upesy.com>
2023-10-17 14:51:16 +0200

    tweak uPesy C3 Mini pinout
    tweak uPesy C3 Mini pinout
    
    tweak uPesy C3 basic pinout
    
    Add uPesy ESP32S3 pin variant
    
    Add uPesy ESP32S3 pin variant
    
    Tweak uPesy ESP32s3/C3 pinout variant

--- variants/uPesy_esp32c3_basic/pins_arduino.h
+++ variants/uPesy_esp32c3_basic/pins_arduino.h
@@ -3,6 +3,12 @@
 
 #include <stdint.h>
 
+#define USB_VID 0x303A
+#define USB_PID 0x8195
+#define USB_MANUFACTURER "uPesy Electronics"
+#define USB_PRODUCT "uPesy ESP32C3 Basic"
+#define USB_SERIAL ""
+
 #define EXTERNAL_NUM_INTERRUPTS 22
 #define NUM_DIGITAL_PINS        22
 #define NUM_ANALOG_INPUTS       6
@@ -14,8 +20,8 @@
 static const uint8_t TX = 21;
 static const uint8_t RX = 20;
 
-static const uint8_t SDA = 8;
-static const uint8_t SCL = 9;
+static const uint8_t SDA = 3;
+static const uint8_t SCL = 10;
 
 static const uint8_t SS    = 7;
 static const uint8_t MOSI  = 6;
@@ -28,5 +34,7 @@ static const uint8_t A2 = 2;
 static const uint8_t A3 = 3;
 static const uint8_t A4 = 4;
 static const uint8_t A5 = 5;
+
+static const uint8_t VBAT_SENSE = 0;
 
 #endif /* Pins_Arduino_h */
--- variants/uPesy_esp32c3_mini/pins_arduino.h
+++ variants/uPesy_esp32c3_mini/pins_arduino.h
@@ -3,6 +3,12 @@
 
 #include <stdint.h>
 
+#define USB_VID 0x303A
+#define USB_PID 0x819B
+#define USB_MANUFACTURER "uPesy Electronics"
+#define USB_PRODUCT "uPesy ESP32C3 Mini"
+#define USB_SERIAL ""
+
 #define EXTERNAL_NUM_INTERRUPTS 22
 #define NUM_DIGITAL_PINS        22
 #define NUM_ANALOG_INPUTS       6
@@ -14,8 +20,8 @@
 static const uint8_t TX = 21;
 static const uint8_t RX = 20;
 
-static const uint8_t SDA = 21;
-static const uint8_t SCL = 20;
+static const uint8_t SDA = 3;
+static const uint8_t SCL = 10;
 
 static const uint8_t SS    = 7;
 static const uint8_t MOSI  = 6;
@@ -24,9 +30,10 @@ static const uint8_t SCK   = 4;
 
 static const uint8_t A0 = 0;
 static const uint8_t A1 = 1;
-static const uint8_t A2 = 2;
 static const uint8_t A3 = 3;
 static const uint8_t A4 = 4;
 static const uint8_t A5 = 5;
+
+static const uint8_t VBAT_SENSE = 0;
 
 #endif /* Pins_Arduino_h */
