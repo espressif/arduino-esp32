8ca507f3fb0fefedc184b55f18d605beee8945f8
powerfeatherdev <dev@powerfeather.dev>
2023-11-16 19:44:36 +0900

    Replace GPIO_NUM_X with just the pin number

--- variants/esp32s3_powerfeather/pins_arduino.h
+++ variants/esp32s3_powerfeather/pins_arduino.h
@@ -17,37 +17,37 @@
 #define digitalPinToInterrupt(p)    (((p)<48)?(p):-1)
 #define digitalPinHasPWM(p)         (p < 46)
 
-static const uint8_t ALARM = GPIO_NUM_7;
-static const uint8_t INT =   GPIO_NUM_5;
+static const uint8_t ALARM = 7;
+static const uint8_t INT =   5;
 
-static const uint8_t LED =   GPIO_NUM_46;
-static const uint8_t BTN =   GPIO_NUM_0;
-static const uint8_t EN =    GPIO_NUM_13;
+static const uint8_t LED =   46;
+static const uint8_t BTN =   0;
+static const uint8_t EN =    13;
 
-static const uint8_t TX =    GPIO_NUM_44;
-static const uint8_t RX =    GPIO_NUM_42;
-static const uint8_t TX0 =   GPIO_NUM_43;
+static const uint8_t TX =    44;
+static const uint8_t RX =    42;
+static const uint8_t TX0 =   43;
 
-static const uint8_t MISO =  GPIO_NUM_41;
-static const uint8_t MOSI =  GPIO_NUM_40;
-static const uint8_t SCK =   GPIO_NUM_39;
+static const uint8_t MISO =  41;
+static const uint8_t MOSI =  40;
+static const uint8_t SCK =   39;
 
-static const uint8_t SCL =   GPIO_NUM_36;
-static const uint8_t SDA =   GPIO_NUM_35;
+static const uint8_t SCL =   36;
+static const uint8_t SDA =   35;
 
-static const uint8_t A0 =    GPIO_NUM_10;
-static const uint8_t A1 =    GPIO_NUM_9;
-static const uint8_t A2 =    GPIO_NUM_8;
-static const uint8_t A3 =    GPIO_NUM_3;
-static const uint8_t A4 =    GPIO_NUM_2;
-static const uint8_t A5 =    GPIO_NUM_1;
+static const uint8_t A0 =    10;
+static const uint8_t A1 =    9;
+static const uint8_t A2 =    8;
+static const uint8_t A3 =    3;
+static const uint8_t A4 =    2;
+static const uint8_t A5 =    1;
 
-static const uint8_t D5 =    GPIO_NUM_15;
-static const uint8_t D6 =    GPIO_NUM_16;
-static const uint8_t D9 =    GPIO_NUM_17;
-static const uint8_t D10 =   GPIO_NUM_18;
-static const uint8_t D11 =   GPIO_NUM_45;
-static const uint8_t D12 =   GPIO_NUM_12;
-static const uint8_t D13 =   GPIO_NUM_11;
+static const uint8_t D5 =    15;
+static const uint8_t D6 =    16;
+static const uint8_t D9 =    17;
+static const uint8_t D10 =   18;
+static const uint8_t D11 =   45;
+static const uint8_t D12 =   12;
+static const uint8_t D13 =   11;
 
 #endif /* Pins_Arduino_h */
