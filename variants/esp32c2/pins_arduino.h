1acbec20d7cb1d2750839e1f3662c78f073d367d
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2023-11-20 22:06:03 +0100

    Change SPI default pins

--- variants/esp32c2/pins_arduino.h
+++ variants/esp32c2/pins_arduino.h
@@ -15,10 +15,10 @@ static const uint8_t RX = 19;
 static const uint8_t SDA = 8;
 static const uint8_t SCL = 9;
 
-static const uint8_t SS    = 4;
-static const uint8_t MOSI  = 7;
+static const uint8_t SS    = 7;
+static const uint8_t MOSI  = 6;
 static const uint8_t MISO  = 5;
-static const uint8_t SCK   = 6;
+static const uint8_t SCK   = 4;
 
 static const uint8_t A0 = 1;
 static const uint8_t A1 = 2;
