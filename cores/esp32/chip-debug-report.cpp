973ef24a88bc26405a86a0b1d86136fc4d1dc038
Jan Prochazka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-11-21 16:27:08 +0100

    Update print format

--- cores/esp32/chip-debug-report.cpp
+++ cores/esp32/chip-debug-report.cpp
@@ -241,8 +241,9 @@ static void printBoardInfo(void){
 
 static void printPerimanInfo(void){
   chip_report_printf("GPIO Info:\n");
-  chip_report_printf("PIN number : BUS_TYPE[bus_num/unit][channel]\n");
   chip_report_printf("------------------------------------------\n");
+  chip_report_printf("  GPIO : BUS_TYPE[bus/unit][chan]\n");
+  chip_report_printf("  --------------------------------------  \n");
   for(uint8_t i = 0; i < SOC_GPIO_PIN_COUNT; i++){
     if(!perimanPinIsValid(i)){
       continue;//invalid pin
@@ -252,7 +253,7 @@ static void printPerimanInfo(void){
       continue;//unused pin
     }
     const char* extra_type = perimanGetPinBusExtraType(i);
-    chip_report_printf("  %17u : ", i);
+    chip_report_printf("  %4u : ", i);
     if(extra_type){
       chip_report_printf("%s", extra_type);
     }
