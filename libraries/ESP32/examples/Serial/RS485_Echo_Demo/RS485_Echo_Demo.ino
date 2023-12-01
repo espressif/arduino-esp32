57061e659b4bdc0331933ce89eb51413261fe43a
Neale Petrillo <82894027+econeale@users.noreply.github.com>
2023-12-01 09:06:08 -0500

    Update libraries/ESP32/examples/Serial/RS485_Echo_Demo/RS485_Echo_Demo.ino
    Co-authored-by: Lucas Saavedra Vaz <lucassvaz@yahoo.com.br>

--- libraries/ESP32/examples/Serial/RS485_Echo_Demo/RS485_Echo_Demo.ino
+++ libraries/ESP32/examples/Serial/RS485_Echo_Demo/RS485_Echo_Demo.ino
@@ -23,7 +23,7 @@
 
 void setup() {
   Serial.begin(9600);
-  delay(2000);
+  while(!Serial) { delay(10); }
 
   RS485.begin(9600, SERIAL_8N1, RS485_RX_PIN, RS485_TX_PIN);
   if(!RS485.setPins(-1, -1, -1, RS485_RTS_PIN)){
