98bdb98a9e06d00cd1837d644faf33c2ca0cb392
Jan Prochazka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2023-12-05 10:05:00 +0100

    set _target_md5 to lowercase

--- libraries/Update/src/Updater.cpp
+++ libraries/Update/src/Updater.cpp
@@ -277,7 +277,7 @@ bool UpdateClass::setMD5(const char * expected_md5){
     {
         return false;
     }
-    _target_md5 = expected_md5;
+    _target_md5 = expected_md5.toLowerCase();
     return true;
 }
 
