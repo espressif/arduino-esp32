e9ee9c118bc75a5d094492aab4e96b40ef809c68
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2023-12-19 17:12:40 -0300

    Improve Log Messages in GPIO HAL (#9011)
    * Improve Log Messages
    
    * uses pin# in log messages
    
    * Uses IO # in messages

--- cores/esp32/esp32-hal-gpio.c
+++ cores/esp32/esp32-hal-gpio.c
@@ -104,14 +104,14 @@ extern void ARDUINO_ISR_ATTR __pinMode(uint8_t pin, uint8_t mode)
 #endif
 
     if (pin >= SOC_GPIO_PIN_COUNT) {
-        log_e("Invalid pin selected");
+        log_e("Invalid IO %i selected", pin);
         return;
     }
 
     if(perimanGetPinBus(pin, ESP32_BUS_TYPE_GPIO) == NULL){
         perimanSetBusDeinit(ESP32_BUS_TYPE_GPIO, gpioDetachBus);
         if(!perimanClearPinBus(pin)){
-            log_e("Deinit of previous bus failed");
+            log_e("Deinit of previous bus from IO %i failed", pin);
             return;
         }
     }
@@ -140,7 +140,7 @@ extern void ARDUINO_ISR_ATTR __pinMode(uint8_t pin, uint8_t mode)
     }
     if(gpio_config(&conf) != ESP_OK)
     {
-        log_e("GPIO config failed");
+        log_e("IO %i config failed", pin);
         return;
     }
     if(perimanGetPinBus(pin, ESP32_BUS_TYPE_GPIO) == NULL){
@@ -164,7 +164,7 @@ extern void ARDUINO_ISR_ATTR __digitalWrite(uint8_t pin, uint8_t val)
         if(perimanGetPinBus(pin, ESP32_BUS_TYPE_GPIO) != NULL){
             gpio_set_level((gpio_num_t)pin, val);
         } else {
-            log_e("Pin is not set as GPIO.");
+            log_e("IO %i is not set as GPIO.", pin);
         }
 }
 
@@ -174,7 +174,7 @@ extern int ARDUINO_ISR_ATTR __digitalRead(uint8_t pin)
         return gpio_get_level((gpio_num_t)pin);
     }
     else {
-        log_e("Pin is not set as GPIO.");
+        log_e("IO %i is not set as GPIO.", pin);
         return 0;
     }
 }
@@ -204,7 +204,7 @@ extern void __attachInterruptFunctionalArg(uint8_t pin, voidFuncPtrArg userFunc,
     	interrupt_initialized = (err == ESP_OK) || (err == ESP_ERR_INVALID_STATE);
     }
     if(!interrupt_initialized) {
-    	log_e("GPIO ISR Service Failed To Start");
+    	log_e("IO %i ISR Service Failed To Start", pin);
     	return;
     }
 
