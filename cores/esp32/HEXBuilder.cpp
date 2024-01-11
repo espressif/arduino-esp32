dc1a920231369a6c46c347e17175661b35e60e0a
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-01-11 18:11:39 -0300

    Fix indentation

--- cores/esp32/HEXBuilder.cpp
+++ cores/esp32/HEXBuilder.cpp
@@ -1,7 +1,7 @@
-/* 
+/*
   Copyright (c) 2015 Hristo Gochkov. All rights reserved.
-  This file is part of the esp8266 core for Arduino environment.
- 
+  This file is part of the esp32 core for Arduino environment.
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
@@ -16,6 +16,7 @@
   License along with this library; if not, write to the Free Software
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
 #include <Arduino.h>
 #include <HEXBuilder.h>
 
@@ -27,43 +28,44 @@ static uint8_t hex_char_to_byte(uint8_t c)
 }
 
 size_t HEXBuilder::hex2bytes(unsigned char * out, size_t maxlen, String &in) {
-	return hex2bytes(out, maxlen, in.c_str());
+    return hex2bytes(out, maxlen, in.c_str());
 }
 
 size_t HEXBuilder::hex2bytes(unsigned char * out, size_t maxlen, const char * in) {
-	size_t len = 0;
-	for(;*in;in++) {
-		uint8_t c = hex_char_to_byte(*in);
-		// Silently skip anything unknown.
-		if (c > 15) 
-			continue;
+    size_t len = 0;
+    for(;*in;in++) {
+        uint8_t c = hex_char_to_byte(*in);
+        // Silently skip anything unknown.
+        if (c > 15)
+            continue;
 
-		if (len & 1) {
-			if (len/2 < maxlen) 
-				out[len/2] |= c;
-		} else {
-			if (len/2 < maxlen) 
-		 		out[len/2] = c<<4;
-		}
-		len++;
-	}
-	return (len + 1)/2;
+        if (len & 1) {
+            if (len/2 < maxlen)
+                out[len/2] |= c;
+        } else {
+            if (len/2 < maxlen)
+                 out[len/2] = c<<4;
+        }
+        len++;
+    }
+    return (len + 1)/2;
 }
 
 size_t HEXBuilder::bytes2hex(char * out, size_t maxlen, const unsigned char * in, size_t len) {
-	for(size_t i = 0; i < len; i++) 
-	    if (i*2 + 1 < maxlen)
-	        sprintf(out + (i * 2), "%02x", in[i]);
-
-        return len * 2 + 1;
+    for(size_t i = 0; i < len; i++) {
+        if (i*2 + 1 < maxlen) {
+            sprintf(out + (i * 2), "%02x", in[i]);
+        }
+    }
+    return len * 2 + 1;
 }
 
 String HEXBuilder::bytes2hex(const unsigned char * in, size_t len) {
-        size_t maxlen = len * 2 + 1;
-        char * out = (char *) malloc(maxlen);
-        if (!out) return String();
-        bytes2hex(out, maxlen, in, len);
-        String ret = String(out);
-        free(out);
-        return ret;
+    size_t maxlen = len * 2 + 1;
+    char * out = (char *) malloc(maxlen);
+    if (!out) return String();
+    bytes2hex(out, maxlen, in, len);
+    String ret = String(out);
+    free(out);
+    return ret;
 }
--- cores/esp32/HEXBuilder.h
+++ cores/esp32/HEXBuilder.h
@@ -1,7 +1,7 @@
-/* 
+/*
   Copyright (c) 2015 Hristo Gochkov. All rights reserved.
   This file is part of the esp8266 core for Arduino environment.
- 
+
   This library is free software; you can redistribute it and/or
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
