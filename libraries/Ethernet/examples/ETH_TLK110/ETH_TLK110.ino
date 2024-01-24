ea1717bc0fd497099e2881017fe2ccbda56ea983
me-no-dev <hristo@espressif.com>
2024-01-24 18:13:18 +0200

    more fixes

--- libraries/Ethernet/examples/ETH_TLK110/ETH_TLK110.ino
+++ libraries/Ethernet/examples/ETH_TLK110/ETH_TLK110.ino
@@ -15,7 +15,7 @@
 static bool eth_connected = false;
 
 // WARNING: WiFiEvent is called from a separate FreeRTOS task (thread)!
-void onEvent(WiFiEvent_t event)
+void onEvent(arduino_event_id_t event)
 {
   switch (event) {
     case ARDUINO_EVENT_ETH_START:
