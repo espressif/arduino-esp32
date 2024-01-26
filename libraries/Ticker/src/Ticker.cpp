bcf0e7b387dff164fc396b48e4f765adafffef68
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-01-26 10:55:25 -0300

    Revert formatting changes

--- libraries/Ticker/src/Ticker.cpp
+++ libraries/Ticker/src/Ticker.cpp
@@ -34,42 +34,41 @@ Ticker::~Ticker()
 
 void Ticker::_attach_us(uint64_t micros, bool repeat, callback_with_arg_t callback, void* arg)
 {
-    esp_timer_create_args_t _timerConfig;
-    _timerConfig.arg = reinterpret_cast<void*>(arg);
-    _timerConfig.callback = callback;
-    _timerConfig.dispatch_method = ESP_TIMER_TASK;
-    _timerConfig.name = "Ticker";
-    if (_timer) {
-        esp_timer_stop(_timer);
-        esp_timer_delete(_timer);
-    }
-    esp_timer_create(&_timerConfig, &_timer);
-    if (repeat) {
-        esp_timer_start_periodic(_timer, micros);
-    }
-    else {
-        esp_timer_start_once(_timer, micros);
-    }
+  esp_timer_create_args_t _timerConfig;
+  _timerConfig.arg = reinterpret_cast<void*>(arg);
+  _timerConfig.callback = callback;
+  _timerConfig.dispatch_method = ESP_TIMER_TASK;
+  _timerConfig.name = "Ticker";
+  if (_timer) {
+    esp_timer_stop(_timer);
+    esp_timer_delete(_timer);
+  }
+  esp_timer_create(&_timerConfig, &_timer);
+  if (repeat) {
+    esp_timer_start_periodic(_timer, micros);
+  } else {
+    esp_timer_start_once(_timer, micros);
+  }
 }
 
 void Ticker::detach() {
-    if (_timer) {
-        esp_timer_stop(_timer);
-        esp_timer_delete(_timer);
-        _timer = nullptr;
-        _callback_function = nullptr;
-    }
+  if (_timer) {
+    esp_timer_stop(_timer);
+    esp_timer_delete(_timer);
+    _timer = nullptr;
+    _callback_function = nullptr;
+  }
 }
 
 bool Ticker::active() const {
-    if (!_timer) return false;
-    return esp_timer_is_active(_timer);
+  if (!_timer) return false;
+  return esp_timer_is_active(_timer);
 }
 
 void Ticker::_static_callback(void* arg)
 {
-    Ticker* _this = reinterpret_cast<Ticker*>(arg);
-    if (_this && _this->_callback_function)
-        _this->_callback_function();
+  Ticker* _this = reinterpret_cast<Ticker*>(arg);
+  if (_this && _this->_callback_function)
+    _this->_callback_function();
 }
 
--- libraries/Ticker/src/Ticker.h
+++ libraries/Ticker/src/Ticker.h
@@ -33,106 +33,106 @@ extern "C" {
 class Ticker
 {
 public:
-    Ticker();
-    ~Ticker();
+  Ticker();
+  ~Ticker();
 
-    typedef void (*callback_with_arg_t)(void*);
-    typedef std::function<void(void)> callback_function_t;
+  typedef void (*callback_with_arg_t)(void*);
+  typedef std::function<void(void)> callback_function_t;
 
-    void attach(float seconds, callback_function_t callback)
-    {
-        _callback_function = std::move(callback);
-        _attach_us(1000000ULL * seconds, true, _static_callback, this);
-    }
+  void attach(float seconds, callback_function_t callback)
+  {
+    _callback_function = std::move(callback);
+    _attach_us(1000000ULL * seconds, true, _static_callback, this);
+  }
 
-    void attach_ms(uint64_t milliseconds, callback_function_t callback)
-    {
-        _callback_function = std::move(callback);
-        _attach_us(1000ULL * milliseconds, true, _static_callback, this);
-    }
+  void attach_ms(uint64_t milliseconds, callback_function_t callback)
+  {
+    _callback_function = std::move(callback);
+    _attach_us(1000ULL * milliseconds, true, _static_callback, this);
+  }
 
-    void attach_us(uint64_t micros, callback_function_t callback)
-    {
-        _callback_function = std::move(callback);
-        _attach_us(micros, true, _static_callback, this);
-    }
+  void attach_us(uint64_t micros, callback_function_t callback)
+  {
+    _callback_function = std::move(callback);
+    _attach_us(micros, true, _static_callback, this);
+  }
 
-    template<typename TArg>
-    void attach(float seconds, void (*callback)(TArg), TArg arg)
-    {
-        static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
-        // C-cast serves two purposes:
-        // static_cast for smaller integer types,
-        // reinterpret_cast + const_cast for pointer types
-        _attach_us(1000000ULL * seconds, true, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
-    }
+  template<typename TArg>
+  void attach(float seconds, void (*callback)(TArg), TArg arg)
+  {
+    static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
+    // C-cast serves two purposes:
+    // static_cast for smaller integer types,
+    // reinterpret_cast + const_cast for pointer types
+    _attach_us(1000000ULL * seconds, true, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
+  }
 
-    template<typename TArg>
-    void attach_ms(uint64_t milliseconds, void (*callback)(TArg), TArg arg)
-    {
-        static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
-        _attach_us(1000ULL * milliseconds, true, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
-    }
+  template<typename TArg>
+  void attach_ms(uint64_t milliseconds, void (*callback)(TArg), TArg arg)
+  {
+    static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
+    _attach_us(1000ULL * milliseconds, true, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
+  }
 
-    template<typename TArg>
-    void attach_us(uint64_t micros, void (*callback)(TArg), TArg arg)
-    {
-        static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
-        _attach_us(micros, true, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
-    }
+  template<typename TArg>
+  void attach_us(uint64_t micros, void (*callback)(TArg), TArg arg)
+  {
+    static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
+    _attach_us(micros, true, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
+  }
 
-    void once(float seconds, callback_function_t callback)
-    {
-        _callback_function = std::move(callback);
-        _attach_us(1000000ULL * seconds, false, _static_callback, this);
-    }
+  void once(float seconds, callback_function_t callback)
+  {
+    _callback_function = std::move(callback);
+    _attach_us(1000000ULL * seconds, false, _static_callback, this);
+  }
 
-    void once_ms(uint64_t milliseconds, callback_function_t callback)
-    {
-        _callback_function = std::move(callback);
-        _attach_us(1000ULL * milliseconds, false, _static_callback, this);
-    }
+  void once_ms(uint64_t milliseconds, callback_function_t callback)
+  {
+    _callback_function = std::move(callback);
+    _attach_us(1000ULL * milliseconds, false, _static_callback, this);
+  }
 
-    void once_us(uint64_t micros, callback_function_t callback)
-    {
-        _callback_function = std::move(callback);
-        _attach_us(micros, false, _static_callback, this);
-    }
+  void once_us(uint64_t micros, callback_function_t callback)
+  {
+    _callback_function = std::move(callback);
+    _attach_us(micros, false, _static_callback, this);
+  }
 
-    template<typename TArg>
-    void once(float seconds, void (*callback)(TArg), TArg arg)
-    {
-        static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
-        _attach_us(1000000ULL * seconds, false, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
-    }
+  template<typename TArg>
+  void once(float seconds, void (*callback)(TArg), TArg arg)
+  {
+    static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
+    _attach_us(1000000ULL * seconds, false, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
+  }
 
-    template<typename TArg>
-    void once_ms(uint64_t milliseconds, void (*callback)(TArg), TArg arg)
-    {
-        static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
-        _attach_us(1000ULL * milliseconds, false, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
-    }
+  template<typename TArg>
+  void once_ms(uint64_t milliseconds, void (*callback)(TArg), TArg arg)
+  {
+    static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
+    _attach_us(1000ULL * milliseconds, false, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
+  }
 
-    template<typename TArg>
-    void once_us(uint64_t micros, void (*callback)(TArg), TArg arg)
-    {
-        static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
-        _attach_us(micros, false, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
-    }
+  template<typename TArg>
+  void once_us(uint64_t micros, void (*callback)(TArg), TArg arg)
+  {
+    static_assert(sizeof(TArg) <= sizeof(void*), "attach() callback argument size must be <= sizeof(void*)");
+    _attach_us(micros, false, reinterpret_cast<callback_with_arg_t>(callback), reinterpret_cast<void*>(arg));
+  }
 
-    void detach();
-    bool active() const;
+  void detach();
+  bool active() const;
 
-protected:
-    static void _static_callback(void* arg);
+protected: 
+  static void _static_callback(void* arg);
 
-    callback_function_t _callback_function = nullptr;
+  callback_function_t _callback_function = nullptr;
 
-    esp_timer_handle_t _timer;
+  esp_timer_handle_t _timer;
 
 private:
-    void _attach_us(uint64_t micros, bool repeat, callback_with_arg_t callback, void* arg);
-};
+  void _attach_us(uint64_t micros, bool repeat, callback_with_arg_t callback, void* arg);
+  };
 
 
-#endif//TICKER_H
+#endif  // TICKER_H
