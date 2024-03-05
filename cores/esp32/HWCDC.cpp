631b7b76a5f4b9c92ad2eb37334af5e0e4ee0e0f
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-03-05 08:09:49 -0300

    Update cores/esp32/HWCDC.cpp

--- cores/esp32/HWCDC.cpp
+++ cores/esp32/HWCDC.cpp
@@ -247,8 +247,11 @@ void HWCDC::begin(unsigned long baud)
     }
     // Setting USB D+ D- pins
     uint8_t pin = ESP32_BUS_TYPE_USB_DM;
-    if(perimanGetPinBusType(pin) != ESP32_BUS_TYPE_INIT)
-        if(!perimanClearPinBus(pin)) goto err;
+    if(perimanGetPinBusType(pin) != ESP32_BUS_TYPE_INIT){
+        if(!perimanClearPinBus(pin)){
+            goto err;
+        }
+    }
     if(!perimanSetPinBus(pin, ESP32_BUS_TYPE_USB_DM, (void *) this, -1, -1)) goto err;
     pin = ESP32_BUS_TYPE_USB_DP;
     if(perimanGetPinBusType(pin) != ESP32_BUS_TYPE_INIT)
