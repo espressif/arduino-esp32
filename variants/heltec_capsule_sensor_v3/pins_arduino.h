3d8b58658f82d5fc98c7c13c5b4187922029eda1
Aaron.Lee <32860565+Heltec-Aaron-Lee@users.noreply.github.com>
2024-03-15 14:57:57 +0800

    Update variants/heltec_capsule_sensor_v3/pins_arduino.h
    Co-authored-by: Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>

--- variants/heltec_capsule_sensor_v3/pins_arduino.h
+++ variants/heltec_capsule_sensor_v3/pins_arduino.h
@@ -19,9 +19,6 @@ static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT+48;
 #define RGB_BUILTIN LED_BUILTIN
 #define RGB_BRIGHTNESS 64
 
-#define analogInputToDigitalPin(p)  (((p)<20)?(analogChannelToDigitalPin(p)):-1)
-#define digitalPinToInterrupt(p)    (((p)<48)?(p):-1)
-#define digitalPinHasPWM(p)         (p < 46)
 
 static const uint8_t TX = 43;
 static const uint8_t RX = 44;
