2c68fdc977785189de77b2bfa58706d5c23d324d
Jan Prochazka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2024-03-20 13:22:37 +0100

    feat(libs): Add ESP-NOW Arduino library

---
+++ libraries/ESP_NOW/src/ESP32_NOW.h
@@ -0,0 +1,65 @@
+#pragma once
+
+#include "esp_wifi_types.h"
+#include "Print.h"
+#include "esp_now.h"
+
+class ESP_NOW_Peer {
+private:
+  uint8_t mac[6];
+  uint8_t chan;
+  wifi_interface_t ifc;
+  bool encrypt;
+  uint8_t key[16];
+
+protected:
+  bool added;
+  bool add();
+  bool remove();
+  size_t send(const uint8_t * data, int len);
+
+public:
+  ESP_NOW_Peer(const uint8_t *mac_addr, uint8_t channel=0, wifi_interface_t iface=WIFI_IF_AP, const uint8_t *lmk=NULL);
+  virtual ~ESP_NOW_Peer() {}
+
+  const uint8_t * addr() const;
+  bool addr(const uint8_t *mac_addr);
+
+  uint8_t getChannel() const;
+  bool setChannel(uint8_t channel);
+
+  wifi_interface_t getInterface() const;
+  bool setInterface(wifi_interface_t iface);
+
+  bool isEncrypted() const;
+  bool setKey(const uint8_t *lmk);
+
+  operator bool() const;
+
+  //must be implemented by the upper class
+  virtual void _onReceive(const uint8_t * data, size_t len) = 0;
+  virtual void _onSent(bool success) = 0;
+};
+
+class ESP_NOW_Class : public Print {
+public:
+  ESP_NOW_Class();
+  ~ESP_NOW_Class();
+
+  bool begin(const uint8_t *pmk=NULL /* 16 bytes */);
+  bool end();
+
+  int getTotalPeerCount();
+  int getEncryptedPeerCount();
+
+  int availableForWrite();
+  size_t write(const uint8_t * data, size_t len);
+  size_t write(uint8_t data){ return write(&data, 1); }
+
+  void onNewPeer(void (*cb)(const esp_now_recv_info_t *info, const uint8_t * data, int len, void * arg), void * arg);
+
+  //TODO: Add function to set peer rate - esp_now_set_peer_rate_config()
+};
+
+extern ESP_NOW_Class ESP_NOW;
+
