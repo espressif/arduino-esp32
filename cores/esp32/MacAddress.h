d2e47916dcd4668f97aa6aa362ea93abaa40baaf
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-03-21 13:42:05 -0300

    Add MacAddress string constructor

--- cores/esp32/MacAddress.h
+++ cores/esp32/MacAddress.h
@@ -54,6 +54,7 @@ public:
     MacAddress(const uint8_t *macbytearray) : MacAddress(MAC6, macbytearray) {}
 
     MacAddress(const char *macstr);
+    MacAddress(const String &macstr) : MacAddress(macstr.c_str()) {}
 
     virtual ~MacAddress() {}
 
