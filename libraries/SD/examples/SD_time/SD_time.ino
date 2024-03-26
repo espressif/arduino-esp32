b4999721924881cd17346577a6b5f81d5dd70551
Larry Bernstone <lbernstone@gmail.com>
2024-03-26 09:50:13 -1000

    Non-breaking dashes

--- libraries/SD/examples/SD_time/SD_time.ino
+++ libraries/SD/examples/SD_time/SD_time.ino
@@ -23,7 +23,7 @@
  *        Alternatively you can change only the CS pin with `SD.begin(CSpin)` and keep the default settings for other pins.
  *
 +----------+
- * | SPI Pin Name | ESP8266 | ESP32 | ESP32-S2 | ESP32-S3 | ESP32-C3 | ESP32-C6 | ESP32-H2 |
+ * | SPI Pin Name | ESP8266 | ESP32 | ESP32‑S2 | ESP32‑S3 | ESP32‑C3 | ESP32‑C6 | ESP32‑H2 |
  * +==============+=========+=======+==========+==========+==========+==========+==========+
  * | CS (SS)      | GPIO15  | GPIO5 | GPIO34   | GPIO10   | GPIO7    | GPIO18   | GPIO0    |
  * +--------------+---------+-------+----------+----------+----------+----------+----------+
