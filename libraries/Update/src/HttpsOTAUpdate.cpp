04abd8948fd30735d32997eadebc0f74bf32f36e
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-04-02 10:10:41 -0300

    feat: update license

--- libraries/Update/src/HttpsOTAUpdate.cpp
+++ libraries/Update/src/HttpsOTAUpdate.cpp
@@ -1,6 +1,5 @@
-/* OTA task
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
+/* 
+   This code is in the Public Domain (or CC0 licensed, at your option.)
 
    Unless required by applicable law or agreed to in writing, this
    software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
