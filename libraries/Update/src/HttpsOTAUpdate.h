c2a8e25aba22ad8cb14a19e9e7c7179f95d52ff2
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-04-02 16:08:29 -0300

    Adjust Update Library license (#9448)
    * Adjust Update Library license
    
    * Update Update.h
    
    * Update Updater.cpp

--- libraries/Update/src/HttpsOTAUpdate.h
+++ libraries/Update/src/HttpsOTAUpdate.h
@@ -1,3 +1,9 @@
+/* OTA task
+   This example code is in the Public Domain (or CC0 licensed, at your option.)
+   Unless required by applicable law or agreed to in writing, this
+   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+   CONDITIONS OF ANY KIND, either express or implied.
+*/
 #ifndef HTTPSOTAUPDATE_H
 #define HTTPSOTAUPDATE_H
 #include "esp_http_client.h"
