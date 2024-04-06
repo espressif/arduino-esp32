5102d32cfd5403c30c84afcceb15cf5d5e29dace
Nathan Nau <nathannau@gmail.com>
2024-04-06 19:14:16 +0200

    Remove unuse function printDirectory

--- libraries/WebServer/examples/UploadHugeFile/UploadHugeFile.ino
+++ libraries/WebServer/examples/UploadHugeFile/UploadHugeFile.ino
@@ -42,50 +42,6 @@ void returnFail(String msg) {
   server.send(500, "text/plain", msg + "\r\n");
 }
 
-void printDirectory() {
-  if (!server.hasArg("dir")) {
-    return returnFail("BAD ARGS");
-  }
-  String path = server.arg("dir");
-  if (path != "/" && !SD.exists((char *)path.c_str())) {
-    return returnFail("BAD PATH");
-  }
-  File dir = SD.open((char *)path.c_str());
-  path = String();
-  if (!dir.isDirectory()) {
-    dir.close();
-    return returnFail("NOT DIR");
-  }
-  dir.rewindDirectory();
-  server.setContentLength(CONTENT_LENGTH_UNKNOWN);
-  server.send(200, "text/json", "");
-  WiFiClient client = server.client();
-
-  server.sendContent("[");
-  for (int cnt = 0; true; ++cnt) {
-    File entry = dir.openNextFile();
-    if (!entry) {
-      break;
-    }
-
-    String output;
-    if (cnt > 0) {
-      output = ',';
-    }
-
-    output += "{\"type\":\"";
-    output += (entry.isDirectory()) ? "dir" : "file";
-    output += "\",\"name\":\"";
-    output += entry.path();
-    output += "\"";
-    output += "}";
-    server.sendContent(output);
-    entry.close();
-  }
-  server.sendContent("]");
-  dir.close();
-}
-
 void handleNotFound() {
   String message = "File Not Found\n\n";
   message += "URI: ";
