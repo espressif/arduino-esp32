5f48d9a3f2281c1327f8135a1cd30c90ad41dd6c
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-04-08 00:15:11 -0300

    feat: backports HWCDC

--- cores/esp32/HWCDC.h
+++ cores/esp32/HWCDC.h
@@ -42,6 +42,10 @@ typedef union {
 
 class HWCDC: public Stream
 {
+private:
+    static void deinit();
+    static bool isCDC_Connected();
+
 public:
     HWCDC();
     ~HWCDC();
@@ -64,6 +68,11 @@ public:
     size_t write(const uint8_t *buffer, size_t size);
     void flush(void);
     
+    static bool isPlugged(void);
+    inline static bool isConnected(void)
+    {
+        return isCDC_Connected();
+    }
     inline size_t read(char * buffer, size_t size)
     {
         return read((uint8_t*) buffer, size);
