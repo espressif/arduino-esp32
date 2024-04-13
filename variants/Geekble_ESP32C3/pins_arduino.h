45736950118f8f9eb635b738c5718f664b366e5d
SooDragon <soodragon@geekble.kr>
2024-04-13 20:38:37 +0900

    fix: add variable for support internal built in switch
    fix: add variable for support internal built in switch

--- variants/Geekble_ESP32C3/pins_arduino.h
+++ variants/Geekble_ESP32C3/pins_arduino.h
@@ -3,9 +3,13 @@
 
 #include <stdint.h>
 
-static const uint8_t LED_BUILTIN = 10;
-#define BUILTIN_LED  LED_BUILTIN // backward compatibility
-#define LED_BUILTIN LED_BUILTIN  // allow testing #ifdef LED_BUILTIN
+static const uint8_t    LED_BUILTIN = 10;
+#define BUILTIN_LED     LED_BUILTIN     // backward compatibility
+#define LED_BUILTIN     LED_BUILTIN     // allow testing #ifdef LED_BUILTIN
+
+static const uint8_t    SW_BUILTIN = 9;
+#define BUILTIN_SW      SW_BUILTIN      // backward compatibility
+#define SW_BUILTIN      SW_BUILTIN      // allow testing #ifdef SW_BUILTIN
 
 static const uint8_t TX = 21;
 static const uint8_t RX = 20;
