9a9e7194f5bda27ae535f87185e98b7944e77db7
Juraj Andrássy <juraj.andrassy@gmail.com>
2024-04-17 14:05:07 +0200

    fix: NetworkClient - close the connection in stop() method
    for all copies referring it

--- libraries/Network/src/NetworkClient.cpp
+++ libraries/Network/src/NetworkClient.cpp
@@ -162,7 +162,14 @@ public:
   NetworkClientSocketHandle(int fd) : sockfd(fd) {}
 
   ~NetworkClientSocketHandle() {
-    close(sockfd);
+    close();
+  }
+
+  void close() {
+    if (sockfd >= 0) {
+      ::close(sockfd);
+      sockfd = -1;
+    }
   }
 
   int fd() {
@@ -178,10 +185,12 @@ NetworkClient::NetworkClient(int fd) : _connected(true), _timeout(WIFI_CLIENT_DE
 }
 
 NetworkClient::~NetworkClient() {
-  stop();
 }
 
 void NetworkClient::stop() {
+  if (clientSocketHandle) {
+  	clientSocketHandle->close();
+  }
   clientSocketHandle = NULL;
   _rxBuffer = NULL;
   _connected = false;
@@ -473,7 +482,7 @@ int NetworkClient::read(uint8_t *buf, size_t size) {
 
 int NetworkClient::peek() {
   int res = -1;
-  if (_rxBuffer) {
+  if (fd() >= 0 && _rxBuffer) {
     res = _rxBuffer->peek();
     if (_rxBuffer->failed()) {
       log_e("fail on fd %d, errno: %d, \"%s\"", fd(), errno, strerror(errno));
@@ -484,7 +493,7 @@ int NetworkClient::peek() {
 }
 
 int NetworkClient::available() {
-  if (!_rxBuffer) {
+  if (fd() < 0 || !_rxBuffer) {
     return 0;
   }
   int res = _rxBuffer->available();
@@ -502,6 +511,9 @@ void NetworkClient::clear() {
 }
 
 uint8_t NetworkClient::connected() {
+  if (fd() == -1 && _connected) {
+    stop();
+  }
   if (_connected) {
     uint8_t dummy;
     int res = recv(fd(), &dummy, 0, MSG_DONTWAIT);
