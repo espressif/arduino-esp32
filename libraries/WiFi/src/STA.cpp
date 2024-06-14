fe6e747aa47065fdd1938f9006b59d120ddab82e
me-no-dev <hristo@espressif.com>
2024-06-14 09:46:09 +0300

    fix(wifi): Fix WiFi setTxPower and getTxPower
    We need to wait for the interface to be started in order to be able to set/get TX Power. Code was returning too early, so calling the functions after `begin()` resulted in failure.

--- libraries/WiFi/src/STA.cpp
+++ libraries/WiFi/src/STA.cpp
@@ -298,6 +298,10 @@ bool STAClass::begin(bool tryConnect) {
     log_e("STA enable failed!");
     return false;
   }
+  if(!waitStatusBits(ESP_NETIF_STARTED_BIT, 1000)){
+    log_e("Failed to start STA!");
+    return false;
+  }
   if (tryConnect) {
     return connect();
   }
