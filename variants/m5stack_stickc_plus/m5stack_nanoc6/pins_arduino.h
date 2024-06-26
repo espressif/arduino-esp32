4b72aad29c52e40180371fc3cc28981f653a380c
Ludovic BOUÉ <lboue@users.noreply.github.com>
2024-06-26 22:51:42 +0200

    add(board): Add M5Stack NanoC6 Dev Kit

--- variants/m5stack_stickc_plus/m5stack_nanoc6/pins_arduino.h
+++ variants/m5stack_stickc_plus/m5stack_nanoc6/pins_arduino.h
@@ -20,6 +20,11 @@ static const uint8_t RX = 17;
 static const uint8_t SDA = 2;
 static const uint8_t SCL = 1;
 
+static const uint8_t SS = 21;
+static const uint8_t MOSI = 18;
+static const uint8_t MISO = 20;
+static const uint8_t SCK = 19;
+
 static const uint8_t A1 = 1;
 static const uint8_t A2 = 2;
 static const uint8_t A3 = 2;
