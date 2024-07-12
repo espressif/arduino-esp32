c49794f35aad3c8bdc67f87526cd630ad3c6bd40
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2024-07-12 21:37:16 +0200

    Backport timeout fix

--- libraries/WebServer/src/WebServer.cpp
+++ libraries/WebServer/src/WebServer.cpp
@@ -299,10 +299,10 @@ void WebServer::handleClient() {
     case HC_WAIT_READ:
       // Wait for data from client to become available
       if (_currentClient.available()) {
+	// because HTTP_MAX_SEND_WAIT is expressed in milliseconds,
+        // it must be divided by 1000
+	currentClient.setTimeout(HTTP_MAX_SEND_WAIT / 1000);
         if (_parseRequest(_currentClient)) {
-          // because HTTP_MAX_SEND_WAIT is expressed in milliseconds,
-          // it must be divided by 1000
-          _currentClient.setTimeout(HTTP_MAX_SEND_WAIT / 1000);
           _contentLength = CONTENT_LENGTH_NOT_SET;
           _handleRequest();
 
