0f1f947a72202a686e712b22899d7e995e9a7901
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-07-29 06:49:18 -0300

    ci(tests): Fix workflow errors (#10067)

--- .github/scripts/install-arduino-cli.sh
+++ .github/scripts/install-arduino-cli.sh
@@ -41,6 +41,12 @@ fi
 if [ ! -d "$ARDUINO_IDE_PATH" ] || [ ! -f "$ARDUINO_IDE_PATH/arduino-cli" ]; then
     echo "Installing Arduino CLI on $OS_NAME ..."
     mkdir -p "$ARDUINO_IDE_PATH"
-    curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR="$ARDUINO_IDE_PATH" sh
+    if [ "$OS_IS_WINDOWS" == "1" ]; then
+        curl -fsSL https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Windows_64bit.zip -o arduino-cli.zip
+        unzip -q arduino-cli.zip -d "$ARDUINO_IDE_PATH"
+        rm arduino-cli.zip
+    else
+        curl -fsSL https://raw.githubusercontent.com/arduino/arduino-cli/master/install.sh | BINDIR="$ARDUINO_IDE_PATH" sh
+    fi
 fi
 
