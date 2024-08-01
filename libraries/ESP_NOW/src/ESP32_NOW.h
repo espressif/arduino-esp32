fad439145898cbadcade65948d46126fe08dbead
Jan Prochazka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2024-08-01 15:31:34 +0200

    fix(esp-now): Remove all peers on ESP_NOW.end()

--- libraries/ESP_NOW/src/ESP32_NOW.h
+++ libraries/ESP_NOW/src/ESP32_NOW.h
@@ -6,6 +6,31 @@
 #include "esp32-hal-log.h"
 #include "esp_mac.h"
 
+class ESP_NOW_Peer; //forward declaration for friend function
+
+class ESP_NOW_Class : public Print {
+public:
+  const uint8_t BROADCAST_ADDR[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+
+  ESP_NOW_Class();
+  ~ESP_NOW_Class();
+
+  bool begin(const uint8_t *pmk = NULL /* 16 bytes */);
+  bool end();
+
+  int getTotalPeerCount();
+  int getEncryptedPeerCount();
+
+  int availableForWrite();
+  size_t write(const uint8_t *data, size_t len);
+  size_t write(uint8_t data) {
+    return write(&data, 1);
+  }
+
+  void onNewPeer(void (*cb)(const esp_now_recv_info_t *info, const uint8_t *data, int len, void *arg), void *arg);
+  bool removePeer(ESP_NOW_Peer &peer);
+};
+
 class ESP_NOW_Peer {
 private:
   uint8_t mac[6];
@@ -47,28 +72,8 @@ public:
   virtual void onSent(bool success) {
     log_i("Message transmission to peer " MACSTR " %s", MAC2STR(mac), success ? "successful" : "failed");
   }
-};
 
-class ESP_NOW_Class : public Print {
-public:
-  const uint8_t BROADCAST_ADDR[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
-
-  ESP_NOW_Class();
-  ~ESP_NOW_Class();
-
-  bool begin(const uint8_t *pmk = NULL /* 16 bytes */);
-  bool end();
-
-  int getTotalPeerCount();
-  int getEncryptedPeerCount();
-
-  int availableForWrite();
-  size_t write(const uint8_t *data, size_t len);
-  size_t write(uint8_t data) {
-    return write(&data, 1);
-  }
-
-  void onNewPeer(void (*cb)(const esp_now_recv_info_t *info, const uint8_t *data, int len, void *arg), void *arg);
+  friend bool ESP_NOW_Class::removePeer(ESP_NOW_Peer&);
 };
 
 extern ESP_NOW_Class ESP_NOW;
