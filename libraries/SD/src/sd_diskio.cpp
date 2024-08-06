3a717246b0f887b9128d1a1736d982c559a56dfe
Patrik Andersson <patrik.s.andersson@gmail.com>
2024-08-06 12:23:10 +0200

    fix(sd): Fix use after free

--- libraries/SD/src/sd_diskio.cpp
+++ libraries/SD/src/sd_diskio.cpp
@@ -671,8 +671,10 @@ uint8_t sdcard_uninit(uint8_t pdrv) {
   if (pdrv >= FF_VOLUMES || card == NULL) {
     return 1;
   }
-  AcquireSPI lock(card);
-  sdTransaction(pdrv, GO_IDLE_STATE, 0, NULL);
+  {
+    AcquireSPI lock(card);
+    sdTransaction(pdrv, GO_IDLE_STATE, 0, NULL);
+  } // lock is destructed here
   ff_diskio_register(pdrv, NULL);
   s_cards[pdrv] = NULL;
   esp_err_t err = ESP_OK;
