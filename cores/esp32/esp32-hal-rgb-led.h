84b20f0101faa7702511de1755d9cfcecaf9eb7b
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-08-14 11:25:43 -0300

    feat(rgbled): add color order enum

--- cores/esp32/esp32-hal-rgb-led.h
+++ cores/esp32/esp32-hal-rgb-led.h
@@ -11,6 +11,15 @@ extern "C" {
 #define RGB_BRIGHTNESS 64
 #endif
 
+typedef enum {
+  LED_COLOR_ORDER_RGB,
+  LED_COLOR_ORDER_BGR,
+  LED_COLOR_ORDER_BRG,
+  LED_COLOR_ORDER_RBG,
+  LED_COLOR_ORDER_GBR,
+  LED_COLOR_ORDER_GRB
+} rgb_led_color_order_t;
+
 void neopixelWrite(uint8_t pin, uint8_t red_val, uint8_t green_val, uint8_t blue_val);
 
 #ifdef __cplusplus
