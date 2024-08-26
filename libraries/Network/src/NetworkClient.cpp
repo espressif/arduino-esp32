espressif/arduino-esp32

commit 9b32541c0c10b7df91b666ca2975461d823f0449

WiFiClient - rename flush() to clear() (breaking) (#9453)
Co-authored-by: Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>

libraries/Network/src/NetworkClient.cpp
@@ -159,7 +159,7 @@ public:
         return _fill - _pos + r_available();
     }
 
-    void flush(){
+    void clear(){
         if(r_available()){
             fillBuffer();
         }
@@ -391,6 +391,11 @@ int NetworkClient::read()
     return data;
 }
 
+void NetworkClient::flush()
+{
+
+}
+
 size_t NetworkClient::write(const uint8_t *buf, size_t size)
 {
     int res =0;
@@ -534,11 +539,9 @@ int NetworkClient::available()
     return res;
 }
 
-// Though flushing means to send all pending data,
-// seems that in Arduino it also means to clear RX
-void NetworkClient::flush() {
+void NetworkClient::clear() {
     if (_rxBuffer != nullptr) {
-        _rxBuffer->flush();
+        _rxBuffer->clear();
     }
 }
 

libraries/Network/src/NetworkClient.h
@@ -59,11 +59,12 @@ public:
     size_t write(const uint8_t *buf, size_t size);
     size_t write_P(PGM_P buf, size_t size);
     size_t write(Stream &stream);
+    void flush(); // Print::flush tx
     int available();
     int read();
     int read(uint8_t *buf, size_t size);
     int peek();
-    void flush();
+    void clear(); // clear rx
     void stop();
     uint8_t connected();
     void setSSE(bool sse);

libraries/Network/src/NetworkUdp.cpp
@@ -288,6 +288,11 @@ size_t NetworkUDP::write(const uint8_t *buffer, size_t size){
   return i;
 }
 
+void NetworkUDP::flush()
+{
+
+}
+
 int NetworkUDP::parsePacket(){
   if(rx_buffer)
     return 0;
@@ -374,7 +379,7 @@ int NetworkUDP::peek(){
   return rx_buffer->peek();
 }
 
-void NetworkUDP::flush(){
+void NetworkUDP::clear(){
   if(!rx_buffer) return;
   cbuf *b = rx_buffer;
   rx_buffer = 0;

libraries/Network/src/NetworkUdp.h
@@ -63,13 +63,14 @@ public:
   int endPacket();
   size_t write(uint8_t);
   size_t write(const uint8_t *buffer, size_t size);
+  void flush(); // Print::flush tx
   int parsePacket();
   int available();
   int read();
   int read(unsigned char* buffer, size_t len);
   int read(char* buffer, size_t len);
   int peek();
-  void flush();
+  void clear(); // clear rx
   IPAddress remoteIP();
   uint16_t remotePort();
 };

libraries/WiFi/examples/WiFiIPv6/WiFiIPv6.ino
@@ -50,7 +50,7 @@ void wifiConnectedLoop(){
     if(packetLength >= NTP_PACKET_SIZE){
       ntpClient.read(ntpPacketBuffer, NTP_PACKET_SIZE);
     }
-    ntpClient.flush();
+    ntpClient.clear();
     uint32_t secsSince1900 = (uint32_t)ntpPacketBuffer[40] << 24 | (uint32_t)ntpPacketBuffer[41] << 16 | (uint32_t)ntpPacketBuffer[42] << 8 | ntpPacketBuffer[43];
     //Serial.printf("Seconds since Jan 1 1900: %u\n", secsSince1900);
     uint32_t epoch = secsSince1900 - 2208988800UL;

commit 9cb5dd54f09675dab9e13b292ad58b740e2d3ccd

fix(client): Make flush() call clear() and deprecate it
This is a compromise for issues caused by https://github.com/espressif/arduino-esp32/pull/9453

libraries/Network/src/NetworkClient.cpp
@@ -369,7 +369,9 @@ int NetworkClient::read() {
   return data;
 }
 
-void NetworkClient::flush() {}
+void NetworkClient::flush() {
+  clear();
+}
 
 size_t NetworkClient::write(const uint8_t *buf, size_t size) {
   int res = 0;

libraries/Network/src/NetworkClient.h
@@ -56,6 +56,7 @@ public:
   size_t write(const uint8_t *buf, size_t size);
   size_t write_P(PGM_P buf, size_t size);
   size_t write(Stream &stream);
+  [[deprecated("Use clear() instead.")]]
   void flush();  // Print::flush tx
   int available();
   int read();

libraries/Network/src/NetworkUdp.cpp
@@ -288,7 +288,9 @@ size_t NetworkUDP::write(const uint8_t *buffer, size_t size) {
   return i;
 }
 
-void NetworkUDP::flush() {}
+void NetworkUDP::flush() {
+  clear();
+}
 
 int NetworkUDP::parsePacket() {
   if (rx_buffer) {

libraries/Network/src/NetworkUdp.h
@@ -64,6 +64,7 @@ public:
   int endPacket();
   size_t write(uint8_t);
   size_t write(const uint8_t *buffer, size_t size);
+  [[deprecated("Use clear() instead.")]]
   void flush();  // Print::flush tx
   int parsePacket();
   int available();