f949cd8a24b006743c260578a5e594a3231be0dd
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-08-29 11:35:55 -0300

    feat(hw_cdc): update main.cpp
    adds code to wait for the user to open the Serial Monitor

--- idf_component_examples/hw_cdc_hello_world/main/main.cpp
+++ idf_component_examples/hw_cdc_hello_world/main/main.cpp
@@ -2,6 +2,8 @@
 
 void setup() {
   Serial.begin(); // USB CDC doens't need a baud rate
+  while(!Serial) delay(100); // wait for the Serial Monitor to be open
+  Serial.println("\r\nStarting...\r\n");
 }
 
 void loop() {
