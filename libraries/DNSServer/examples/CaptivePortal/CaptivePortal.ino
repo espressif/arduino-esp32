espressif/arduino-esp32

commit f2026f1e345970801084821d6e5f8f95a11fba47

3.0.0 Network Refactoring (#8760)
* Create ESP_NetworkInterface class and have Ethernet extending it

* Update CMakeLists.txt

* Split networking from WiFi (H2 can now use Ethernet)

Now all libs have been checked yet. More to do on WiFi side

* Fix build errors

* Guard WiFi classes and fix RMII ETH examples

* Decouple network related libraries from WiFi

* Fix examples and WiFiUpdate

* Guard WiFiProv lib to compile only on WiFi chips

* Add periman string for network and "fix" mdns on the first ETH

* Revert back location of Client/Server/Udp in order to accept some PRs

* Fix periman

* Some fixes from merging master

* Fix web server missing fs.h

* Move Client, Server and Udp out of WiFi

* More fixes

* more fixes

* Fix CMakekLists and rework lib menu dependencies

* Fix CMake issues

* move back WiFiClient to rebase with master

* Update ETH_TLK110.ino

* Move back WiFiClient

* Update progress

* Update WiFiGeneric.cpp

* More fixes

* Switch AP to the new interface

* Cleanup

* Rename AP methods

* Add extra interface info for Printable

* Rename IPv6 getters to clarify that they are returning LinkLocal address

cc @sgryphon

* Rename network classes

cc @sgryphon

* Update NetworkManager.h

* Rename WiFi Server and UDP

* Rename WiFiClient and WiFiClientSecure

* Update CMakeLists.txt

* Update on-push.sh

* Rename Network library

* Remove unnecessary guard

* Get the correct interface MAC address for mDND Workstation service

* Apply suggestions from code review

Co-authored-by: Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>

---------

Co-authored-by: Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>

libraries/DNSServer/src/DNSServer.cpp
@@ -20,9 +20,13 @@ DNSServer::DNSServer(const String &domainName) : _port(DNS_DEFAULT_PORT), _ttl(h
 
 bool DNSServer::start(){
   if (_resolvedIP.operator uint32_t() == 0){    // no address is set, try to obtain AP interface's IP
+#if SOC_WIFI_SUPPORTED
     if (WiFi.getMode() & WIFI_AP){
       _resolvedIP = WiFi.softAPIP();
-    } else return false;              // won't run if WiFi is not in AP mode
+      return true;
+    }
+#endif
+    return false;              // won't run if WiFi is not in AP mode
   } 
 
   _udp.close();

commit b05f18dad55609ae2a569be81c7535021b880cf3

fix: DNSServer Lib - improper startup code in WiFi mode (#10366)
* DNSServer: fix improper startup code in WiFi mode

When running on WiFi-AP mode server's start() method returned true while in fact UDP listening socket was never created
Regression introduced in #8760
Closes #10330

* ci(pre-commit): Apply automatic fixes

---------

Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

libraries/DNSServer/examples/CaptivePortal/CaptivePortal.ino
@@ -39,7 +39,11 @@ void setup() {
 
   // by default DNSServer is started serving any "*" domain name. It will reply
   // AccessPoint's IP to all DNS request (this is required for Captive Portal detection)
-  dnsServer.start();
+  if (dnsServer.start()) {
+    Serial.println("Started DNS server in captive portal-mode");
+  } else {
+    Serial.println("Err: Can't start DNS server!");
+  }
 
   // serve a simple root page
   server.on("/", handleRoot);

libraries/DNSServer/src/DNSServer.cpp
@@ -22,10 +22,13 @@ bool DNSServer::start() {
 #if SOC_WIFI_SUPPORTED
     if (WiFi.getMode() & WIFI_AP) {
       _resolvedIP = WiFi.softAPIP();
-      return true;
+    } else {
+      return false;  // won't run if WiFi is not in AP mode, or no WiFi
     }
+#else
+    return false;  // for other non WiFi-AP networking an overloaded method must be used to get device's IP
+                   // start(uint16_t port, const String &domainName, const IPAddress &resolvedIP)
 #endif
-    return false;  // won't run if WiFi is not in AP mode
   }
 
   _udp.close();