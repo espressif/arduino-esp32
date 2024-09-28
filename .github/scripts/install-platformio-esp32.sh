66e74eddbcde60b0aeab0def0f9c7fb5a3c129b2
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2024-09-28 11:37:27 -0300

    fix(json): Ignore comments when searching requirements

--- .github/scripts/install-platformio-esp32.sh
+++ .github/scripts/install-platformio-esp32.sh
@@ -100,7 +100,7 @@ function count_sketches(){ # count_sketches <examples-path>
             requirements=$(jq -r '.requires[]? // empty' $sketchdir/ci.json)
             if [[ "$requirements" != "null" ]] || [[ "$requirements" != "" ]]; then
                 for requirement in $requirements; do
-                    found_line=$(grep "$requirement" $LIBS_DIR/esp32/sdkconfig)
+                    found_line=$(grep "^$requirement" $LIBS_DIR/esp32/sdkconfig)
                     if [[ "$found_line" == "" ]]; then
                         continue 2
                     fi
@@ -190,7 +190,7 @@ function build_pio_sketches(){ # build_pio_sketches <board> <options> <examples-
             requirements=$(jq -r '.requires[]? // empty' $sketchdir/ci.json)
             if [[ "$requirements" != "null" ]] || [[ "$requirements" != "" ]]; then
                 for requirement in $requirements; do
-                    found_line=$(grep "$requirement" $LIBS_DIR/esp32/sdkconfig)
+                    found_line=$(grep "^$requirement" $LIBS_DIR/esp32/sdkconfig)
                     if [[ "$found_line" == "" ]]; then
                         continue 2
                     fi
