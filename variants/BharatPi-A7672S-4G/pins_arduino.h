a55abe0baa7fbd091f6a8431ec830a72e5ccd7fe
Bharat Pi <133974527+Bharat-Pi@users.noreply.github.com>
2024-10-02 14:36:19 +0000

    fix(variant): Fixed review comments for SPI and I2C Pins

--- variants/BharatPi-A7672S-4G/pins_arduino.h
+++ variants/BharatPi-A7672S-4G/pins_arduino.h
@@ -20,4 +20,13 @@ static const uint8_t RX = 3;
 static const uint8_t TX_4G = 17;
 static const uint8_t RX_4G = 16;
 
+static const uint8_t SDA = 21;
+static const uint8_t SCL = 22;
+
+static const uint8_t SS = 5;
+static const uint8_t MOSI = 23;
+static const uint8_t MISO = 19;
+static const uint8_t SCK = 18;
+
+
 #endif /* Pins_Arduino_h */
--- variants/BharatPi-LoRa/pins_arduino.h
+++ variants/BharatPi-LoRa/pins_arduino.h
@@ -24,4 +24,12 @@ static const uint8_t SS = 4;
 static const uint8_t RST = 14;
 static const uint8_t DIO0 = 2;
 
+static const uint8_t SDA = 21;
+static const uint8_t SCL = 22;
+
+static const uint8_t SS = -1;
+static const uint8_t MOSI = -1;
+static const uint8_t MISO = -1;
+static const uint8_t SCK = -1;
+
 #endif /* Pins_Arduino_h */
