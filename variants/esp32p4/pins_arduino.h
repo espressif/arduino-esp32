8940b7d99b5160b8afb529cea3d6783d63832a1b
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2024-10-14 12:57:41 +0200

    fix(board): Remove builtin led

--- variants/esp32p4/pins_arduino.h
+++ variants/esp32p4/pins_arduino.h
@@ -4,15 +4,6 @@
 #include <stdint.h>
 #include "soc/soc_caps.h"
 
-#define PIN_NEOPIXEL 44
-// BUILTIN_LED can be used in new Arduino API digitalWrite() like in Blink.ino
-static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT + PIN_NEOPIXEL;
-#define BUILTIN_LED LED_BUILTIN  // backward compatibility
-#define LED_BUILTIN LED_BUILTIN  // allow testing #ifdef LED_BUILTIN
-// RGB_BUILTIN and RGB_BRIGHTNESS can be used in new Arduino API rgbLedWrite()
-#define RGB_BUILTIN    LED_BUILTIN
-#define RGB_BRIGHTNESS 64
-
 // BOOT_MODE 35
 // BOOT_MODE2 36 pullup
 
