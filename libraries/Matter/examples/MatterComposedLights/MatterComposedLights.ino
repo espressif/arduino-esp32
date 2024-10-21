c40444ab338e6c33c5160ee12dfe36246c482025
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-10-21 10:42:43 -0300

    feat(matter): initial commit with arduino matter lib (#10467)
    * feat(matter): initial commit with arduino matter lib
    
    * feat(matter): add matter library to cmakelists.txt
    
    * fix(matter): add correct guard for ci
    
    * fix(matter): using correct ci requirements in ci.json
    
    * fix(matter): using correct ci requirements in header files
    
    * fix(matter): using correct ci requirements header and examples
    
    * fix(typo): typo and commentaries
    
    * fix(typo): typo and commentaries
    
    * fix(typo): typo and commentaries
    
    * fix(commentary): longer explanation
    
    * feat(matter): api simplification with begin
    
    * feat(matter): testing flashmode=qio in CI
    
    * feat(matter): testing flashmode=qio in CI
    
    * fix(matter): changes CI FQBN
    
    * fix(matte): include all fqbn in ci.json using qio
    
    * fix(matter): revert ci and guard changes
    
    * fix(matter): typo and commentaties
    
    * feat(matter): adds a light toggle switch button
    
    * feat(matter): improved the button control
    
    * feat(matter): using switch instead of if() for attibute change
    
    * fix(matter): switch/case scope
    
    * fix(matter): problems found after pressing reset
    
    * feat(matter): improve example using preferences
    
    * fix(pre-commit): Fix and apply pre-commit hooks
    
    ---------
    
    Co-authored-by: Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>

---
+++ libraries/Matter/examples/MatterComposedLights/MatterComposedLights.ino
@@ -0,0 +1,94 @@
+// Matter Manager
+#include <Matter.h>
+#include <WiFi.h>
+
+// List of Matter Endpoints for this Node
+// There will be 3 On/Off Light Endpoints in the same Node
+#include <MatterOnOffLight.h>
+MatterOnOffLight OnOffLight1;
+MatterOnOffLight OnOffLight2;
+MatterOnOffLight OnOffLight3;
+
+// Matter Protocol Endpoint Callback for each Light Accessory
+bool setLightOnOff1(bool state) {
+  Serial.printf("CB-Light1 changed state to: %s\r\n", state ? "ON" : "OFF");
+  return true;
+}
+
+bool setLightOnOff2(bool state) {
+  Serial.printf("CB-Light2 changed state to: %s\r\n", state ? "ON" : "OFF");
+  return true;
+}
+
+bool setLightOnOff3(bool state) {
+  Serial.printf("CB-Light3 changed state to: %s\r\n", state ? "ON" : "OFF");
+  return true;
+}
+
+// WiFi is manually set and started
+
+const char *ssid = "your-ssid";          // Change this to your WiFi SSID
+const char *password = "your-password";  // Change this to your WiFi password
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(100);
+  }
+
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  // enable IPv6
+  WiFi.enableIPv6(true);
+  // Manually connect to WiFi
+  WiFi.begin(ssid, password);
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\r\nWiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(500);
+
+  // Initialize all 3 Matter EndPoints
+  OnOffLight1.begin();
+  OnOffLight2.begin();
+  OnOffLight3.begin();
+  OnOffLight1.onChangeOnOff(setLightOnOff1);
+  OnOffLight2.onChangeOnOff(setLightOnOff2);
+  OnOffLight3.onChangeOnOff(setLightOnOff3);
+
+  // Matter beginning - Last step, after all EndPoints are initialized
+  Matter.begin();
+}
+
+void loop() {
+  // Check Matter Light Commissioning state
+  if (!Matter.isDeviceCommissioned()) {
+    Serial.println("");
+    Serial.println("Matter Node is not commissioned yet.");
+    Serial.println("Initiate the device discovery in your Matter environment.");
+    Serial.println("Commission it to your Matter hub with the manual pairing code or QR code");
+    Serial.printf("Manual pairing code: %s\r\n", Matter.getManualPairingCode().c_str());
+    Serial.printf("QR code URL: %s\r\n", Matter.getOnboardingQRCodeUrl().c_str());
+    // waits for Matter Light Commissioning.
+    uint32_t timeCount = 0;
+    while (!Matter.isDeviceCommissioned()) {
+      delay(100);
+      if ((timeCount++ % 50) == 0) {  // 50*100ms = 5 sec
+        Serial.println("Matter Node not commissioned yet. Waiting for commissioning.");
+      }
+    }
+    Serial.println("Matter Node is commissioned and connected to Wi-Fi. Ready for use.");
+  }
+
+  //displays the Light state every 3 seconds
+  Serial.println("======================");
+  Serial.printf("Matter Light #1 is %s\r\n", OnOffLight1.getOnOff() ? "ON" : "OFF");
+  Serial.printf("Matter Light #2 is %s\r\n", OnOffLight2.getOnOff() ? "ON" : "OFF");
+  Serial.printf("Matter Light #3 is %s\r\n", OnOffLight3.getOnOff() ? "ON" : "OFF");
+  delay(3000);
+}
---
+++ libraries/Matter/examples/MatterOnOffLight/MatterOnOffLight.ino
@@ -0,0 +1,138 @@
+// Matter Manager
+#include <Matter.h>
+#include <WiFi.h>
+#include <Preferences.h>
+
+// List of Matter Endpoints for this Node
+// On/Off Light Endpoint
+#include <MatterOnOffLight.h>
+MatterOnOffLight OnOffLight;
+
+// it will keep last OnOff state stored, using Preferences
+Preferences lastStatePref;
+
+// set your board LED pin here
+#ifdef LED_BUILTIN
+const uint8_t ledPin = LED_BUILTIN;
+#else
+const uint8_t ledPin = 2;  // Set your pin here if your board has not defined LED_BUILTIN
+#warning "Do not forget to set the LED pin"
+#endif
+
+// set your board USER BUTTON pin here
+const uint8_t buttonPin = 0;  // Set your pin here. Using BOOT Button. C6/C3 use GPIO9.
+
+// Matter Protocol Endpoint Callback
+bool setLightOnOff(bool state) {
+  Serial.printf("User Callback :: New Light State = %s\r\n", state ? "ON" : "OFF");
+  if (state) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+  // store last OnOff state for when the Light is restarted / power goes off
+  lastStatePref.putBool("lastOnOffState", state);
+  // This callback must return the success state to Matter core
+  return true;
+}
+
+// WiFi is manually set and started
+
+const char *ssid = "Apartment B15";       // Change this to your WiFi SSID
+const char *password = "flat-pony-body";  // Change this to your WiFi password
+
+void setup() {
+  // Initialize the USER BUTTON (Boot button) GPIO that will act as a toggle switch
+  pinMode(buttonPin, INPUT_PULLUP);
+  // Initialize the LED (light) GPIO and Matter End Point
+  pinMode(ledPin, OUTPUT);
+
+  Serial.begin(115200);
+  while (!Serial) {
+    delay(100);
+  }
+
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+  // enable IPv6
+  WiFi.enableIPv6(true);
+  // Manually connect to WiFi
+  WiFi.begin(ssid, password);
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\r\nWiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+  delay(500);
+
+  // Initialize Matter EndPoint
+  lastStatePref.begin("matterLight", false);
+  bool lastOnOffState = lastStatePref.getBool("lastOnOffState", true);
+  OnOffLight.begin(lastOnOffState);
+  OnOffLight.onChangeOnOff(setLightOnOff);
+
+  // Matter beginning - Last step, after all EndPoints are initialized
+  Matter.begin();
+  // This may be a restart of a already commissioned Matter accessory
+  if (Matter.isDeviceCommissioned()) {
+    Serial.println("Matter Node is commissioned and connected to Wi-Fi. Ready for use.");
+    Serial.printf("Initial state: %s\r\n", OnOffLight.getOnOff() ? "ON" : "OFF");
+    setLightOnOff(OnOffLight.getOnOff());  // configure the Light based on initial state
+  }
+}
+// Button control
+uint32_t button_time_stamp = 0;                 // debouncing control
+bool button_state = false;                      // false = released | true = pressed
+const uint32_t debouceTime = 250;               // button debouncing time (ms)
+const uint32_t decommissioningTimeout = 10000;  // keep the button pressed for 10s to decommission the light
+
+void loop() {
+  // Check Matter Light Commissioning state, which may change during execution of loop()
+  if (!Matter.isDeviceCommissioned()) {
+    Serial.println("");
+    Serial.println("Matter Node is not commissioned yet.");
+    Serial.println("Initiate the device discovery in your Matter environment.");
+    Serial.println("Commission it to your Matter hub with the manual pairing code or QR code");
+    Serial.printf("Manual pairing code: %s\r\n", Matter.getManualPairingCode().c_str());
+    Serial.printf("QR code URL: %s\r\n", Matter.getOnboardingQRCodeUrl().c_str());
+    // waits for Matter Light Commissioning.
+    uint32_t timeCount = 0;
+    while (!Matter.isDeviceCommissioned()) {
+      delay(100);
+      if ((timeCount++ % 50) == 0) {  // 50*100ms = 5 sec
+        Serial.println("Matter Node not commissioned yet. Waiting for commissioning.");
+      }
+    }
+    Serial.printf("Initial state: %s\r\n", OnOffLight.getOnOff() ? "ON" : "OFF");
+    setLightOnOff(OnOffLight.getOnOff());  // configure the Light based on initial state
+    Serial.println("Matter Node is commissioned and connected to Wi-Fi. Ready for use.");
+  }
+
+  // A button is also used to control the light
+  // Check if the button has been pressed
+  if (digitalRead(buttonPin) == LOW && !button_state) {
+    // deals with button debouncing
+    button_time_stamp = millis();  // record the time while the button is pressed.
+    button_state = true;           // pressed.
+  }
+
+  // Onboard User Button is used as a Light toggle switch or to decommission it
+  uint32_t time_diff = millis() - button_time_stamp;
+  if (button_state && time_diff > debouceTime && digitalRead(buttonPin) == HIGH) {
+    button_state = false;  // released
+    // Toggle button is released - toggle the light
+    Serial.println("User button released. Toggling Light!");
+    OnOffLight.toggle();  // Matter Controller also can see the change
+
+    // Factory reset is triggered if the button is pressed longer than 10 seconds
+    if (time_diff > decommissioningTimeout) {
+      Serial.println("Decommissioning the Light Matter Accessory. It shall be commissioned again.");
+      OnOffLight.setOnOff(false);  // turn the light off
+      Matter.decommission();
+    }
+  }
+}
