a14824feffb263c41f90950582763731fc1ae8a4
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2024-10-21 13:42:39 +0200

    fix(ci): codespell fix

--- variants/esp32p4/pins_arduino.h
+++ variants/esp32p4/pins_arduino.h
@@ -49,7 +49,7 @@ static const uint8_t T11 = 13;
 static const uint8_t T12 = 14;
 static const uint8_t T13 = 15;
 
-/* ESP32-P4 EV Function board specific definitons */
+/* ESP32-P4 EV Function board specific definitions */
 //ETH
 #define ETH_PHY_TYPE    ETH_PHY_TLK110
 #define ETH_PHY_ADDR    1
