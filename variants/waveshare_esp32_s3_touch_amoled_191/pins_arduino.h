e2b007a9c72579bf871b739e761233302e7165e3
wuronmin <2578034394@qq.com>
2024-10-25 18:34:22 +0800

    feat(waveshare): add added 4 items esp32 Waveshare boards

--- variants/waveshare_esp32_s3_touch_amoled_191/pins_arduino.h
+++ variants/waveshare_esp32_s3_touch_amoled_191/pins_arduino.h
@@ -6,12 +6,45 @@
 
 // BN: ESP32 Family Device
 #define USB_VID 0x303a
-#define USB_PID 0x8242
+#define USB_PID 0x824B
 
 #define USB_MANUFACTURER "Waveshare"
 #define USB_PRODUCT      "ESP32-S3-Touch-AMOLED-1.91"
 #define USB_SERIAL       ""
 
+// display QSPI SPI2
+#define QSPI_CS       6
+#define QSPI_SCK      47
+#define QSPI_D0       18
+#define QSPI_D1       7
+#define QSPI_D2       48
+#define QSPI_D3       5
+#define AMOLED_RESET  17
+#define AMOLED_TE     -1
+#define AMOLED_PWR_EN -1
+// Touch I2C
+#define TP_SCL 39
+#define TP_SDA 40
+#define TP_RST -1
+#define TP_INT -1
+
+
+// Partial voltage measurement method
+#define BAT_ADC 1
+// Onboard  QMI8658 IMU
+#define QMI_INT1   45
+#define QMI_INT1   46
+
+//SD 
+#define SD_CS 9
+#define SD_MISO 8
+#define SD_MOSI 42
+#define SD_CLK 47
+
+//i2c
+
+static const uint8_t SDA = 40;
+static const uint8_t SCL = 39;
 
 // UART0 pins
 static const uint8_t TX = 43;
