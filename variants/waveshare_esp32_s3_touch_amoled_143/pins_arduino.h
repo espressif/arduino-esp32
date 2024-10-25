22ce416e4dbc7b9d50a960164f22bfb46ebd9d7e
wuronmin <2578034394@qq.com>
2024-10-25 17:21:39 +0800

    feat(waveshare): added 4 items esp32 waveshare boards

---
+++ variants/waveshare_esp32_s3_touch_amoled_143/pins_arduino.h
@@ -0,0 +1,25 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+#include "soc/soc_caps.h"
+
+// BN: ESP32 Family Device
+#define USB_VID 0x303a
+#define USB_PID 0x8242
+
+#define USB_MANUFACTURER "Waveshare"
+#define USB_PRODUCT      "ESP32-S3-Touch-AMOLED-1.43"
+#define USB_SERIAL       ""
+
+
+// UART0 pins
+static const uint8_t TX = 43;
+static const uint8_t RX = 44;
+
+//esp32s3-PSFlash   SPI1/SPI0
+static const uint8_t SS = 34;    // FSPICS0
+static const uint8_t MOSI = 35;  // FSPID
+static const uint8_t MISO = 37;  // FSPIQ
+static const uint8_t SCK = 36;   // FSPICLK
+#endif                           /* Pins_Arduino_h */
