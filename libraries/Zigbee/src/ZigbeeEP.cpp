a3c3153cda27deed6753867157180589897c9def
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2024-11-19 11:10:22 +0100

    fix(Zigbee): Add missing direction to report commands

--- libraries/Zigbee/src/ZigbeeEP.cpp
+++ libraries/Zigbee/src/ZigbeeEP.cpp
@@ -104,6 +104,7 @@ void ZigbeeEP::reportBatteryPercentage() {
   esp_zb_zcl_report_attr_cmd_t report_attr_cmd;
   report_attr_cmd.address_mode = ESP_ZB_APS_ADDR_MODE_DST_ADDR_ENDP_NOT_PRESENT;
   report_attr_cmd.attributeID = ESP_ZB_ZCL_ATTR_POWER_CONFIG_BATTERY_PERCENTAGE_REMAINING_ID;
+  report_attr_cmd.direction = ESP_ZB_ZCL_CMD_DIRECTION_TO_CLI;
   report_attr_cmd.clusterID = ESP_ZB_ZCL_CLUSTER_ID_POWER_CONFIG;
   report_attr_cmd.zcl_basic_cmd.src_endpoint = _endpoint;
 
