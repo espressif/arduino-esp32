157b7842c4dbb76bc48e6f90201145c7332a4e42
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-11-22 14:31:16 -0300

    feat(matter): better commentary about the GPIO.

--- libraries/Matter/examples/MatterMinimum/MatterMinimum.ino
+++ libraries/Matter/examples/MatterMinimum/MatterMinimum.ino
@@ -15,7 +15,7 @@
 /*
  * This example is the smallest code that will create a Matter Device which can be
  * commissioned and controlled from a Matter Environment APP.
- * It controls a GPIO that may be used as LED, additionally the ESP32 will send debug messages indicating the Matter activity.
+ * It controls a GPIO, that could be attached to a LED. Additionally the ESP32 will send debug messages indicating the Matter activity.
  * Turning DEBUG Level may be useful to follow Matter Accessory and Controller messages.
  */
 
