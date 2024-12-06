bc1c55f499e7e35e09af1a489915fc7927d5c700
Rodrigo Garcia <rodrigo.garcia@espressif.com>
2024-12-06 18:19:22 -0300

    feat(matter): creates new matter fan controller endpoint

--- libraries/Matter/src/MatterEndPoint.h
+++ libraries/Matter/src/MatterEndPoint.h
@@ -19,15 +19,87 @@
 #include <Matter.h>
 #include <functional>
 
+using namespace esp_matter;
+
 // Matter Endpoint Base Class. Controls the endpoint ID and allows the child class to overwrite attribute change call
 class MatterEndPoint {
 public:
+
+  enum attrOperation_t {
+    ATTR_SET    = false,
+    ATTR_UPDATE = true
+  };
+
   uint16_t getEndPointId() {
     return endpoint_id;
   }
+  
   void setEndPointId(uint16_t ep) {
     endpoint_id = ep;
   }
+
+  // helper functions for attribute manipulation
+  attribute_t * getAttribute(uint32_t cluster_id, uint32_t attribute_id){
+    if (endpoint_id == 0) {
+      log_e("Endpoint ID is not set");
+      return NULL;
+    }
+    endpoint_t *endpoint = endpoint::get(node::get(), endpoint_id);
+    if (endpoint == NULL) {
+      log_e("Endpoint [%d] not found", endpoint_id);
+      return NULL;
+    }
+    cluster_t *cluster = cluster::get(endpoint, cluster_id);
+    if (cluster == NULL) {
+      log_e("Cluster [%d] not found", cluster_id);
+      return NULL;
+    }
+    attribute_t *attribute = attribute::get(cluster, attribute_id);
+    if (attribute == NULL) {
+      log_e("Attribute [%d] not found", attribute_id);
+      return NULL;
+    }
+    return attribute;
+  }
+
+  // get the value of an attribute from its cluster id and attribute it
+  bool getAttributeVal(uint32_t cluster_id, uint32_t attribute_id, esp_matter_attr_val_t *attrVal){
+    attribute_t *attribute = getAttribute(cluster_id, attribute_id);
+    if (attribute == NULL) {
+      return false;
+    }
+    if (attribute::get_val(attribute, attrVal) == ESP_OK) {
+      log_v("GET_VAL Suceess for cluster %d, attribute %d with value %d", cluster_id, attribute_id, attrVal->val.u32);
+      return true;
+    }
+    log_e("GET_VAL FAILED! for cluster %d, attribute %d with value %d", cluster_id, attribute_id, attrVal->val.u32);
+    return false;
+  }
+
+  // set the value of an attribute from its cluster id and attribute it
+  bool setAttributeVal(uint32_t cluster_id, uint32_t attribute_id, esp_matter_attr_val_t *attrVal){
+    attribute_t *attribute = getAttribute(cluster_id, attribute_id);
+    if (attribute == NULL) {
+      return false;
+    }
+    if (attribute::set_val(attribute, attrVal) == ESP_OK) {
+      log_v("SET_VAL Suceess for cluster %d, attribute %d with value %d", cluster_id, attribute_id, attrVal->val.u32);
+      return true;
+    }
+    log_e("SET_VAL FAILED! for cluster %d, attribute %d with value %d", cluster_id, attribute_id, attrVal->val.u32);
+    return false;
+  }
+
+  // update the value of an attribute from its cluster id and attribute it
+  bool updateAttributeVal(uint32_t cluster_id, uint32_t attribute_id, esp_matter_attr_val_t *attrVal){
+    if (attribute::update(endpoint_id, cluster_id, attribute_id, attrVal) == ESP_OK) {
+      log_v("Update Suceess for cluster %d, attribute %d with value %d", cluster_id, attribute_id, attrVal->val.u32);
+      return true;
+    }
+    log_e("Update FAILED! for cluster %d, attribute %d with value %d", cluster_id, attribute_id, attrVal->val.u32);
+    return false;
+  }
+
   // this function is called by Matter internal event processor. It could be overwritten by the application, if necessary.
   virtual bool attributeChangeCB(uint16_t endpoint_id, uint32_t cluster_id, uint32_t attribute_id, esp_matter_attr_val_t *val) = 0;
 
