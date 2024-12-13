aa0c3da729dae253e6a93e6518900e80a80fd712
FaBjE <5355001+FaBjE@users.noreply.github.com>
2024-12-13 17:13:55 +0100

    feat(Zigbee): Add Zigbee Dimmable light example
    Add example for a dimmable light. Based on a copy of color dimmable light example.

---
+++ libraries/Zigbee/examples/Zigbee_Dimmable_Light/Zigbee_Dimmable_Light.ino
@@ -0,0 +1,110 @@
+// Copyright 2024 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * @brief This example demonstrates Zigbee Dimmable light bulb.
+ *
+ * The example demonstrates how to use Zigbee library to create an end device with
+ * dimmable light end point.
+ * The light bulb is a Zigbee end device, which is controlled by a Zigbee coordinator.
+ *
+ * Proper Zigbee mode must be selected in Tools->Zigbee mode
+ * and also the correct partition scheme must be selected in Tools->Partition Scheme.
+ *
+ * Please check the README.md for instructions and more detailed description.
+ *
+ * Created by Jan Procházka (https://github.com/P-R-O-C-H-Y/)
+ */
+
+#ifndef ZIGBEE_MODE_ED
+#error "Zigbee end device mode is not selected in Tools->Zigbee mode"
+#endif
+
+#include "Zigbee.h"
+
+#define LED_PIN RGB_BUILTIN
+#define BUTTON_PIN 9 // C6/H2 Boot button
+#define ZIGBEE_LIGHT_ENDPOINT 10
+
+ZigbeeDimmableLight zbDimmableLight = ZigbeeDimmableLight(ZIGBEE_LIGHT_ENDPOINT);
+
+/********************* LED functions **************************/
+void setLight(bool state, uint8_t level)
+{
+  rgbLedWrite(LED_PIN, level, level, level);
+}
+
+// Create a task on identify call to handle the identify function
+void identify(uint16_t time)
+{
+  static uint8_t blink = 1;
+  log_d("Identify called for %d seconds", time);
+  if (time == 0)
+  {
+    // If identify time is 0, stop blinking and restore light as it was used for identify
+    zbDimmableLight.restoreLight();
+    return;
+  }
+  rgbLedWrite(LED_PIN, 255 * blink, 255 * blink, 255 * blink);
+  blink = !blink;
+}
+
+/********************* Arduino functions **************************/
+void setup()
+{
+  // Init RMT and leave light OFF
+  rgbLedWrite(LED_PIN, 0, 0, 0);
+
+  // Init button for factory reset
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  // Set callback function for light change
+  zbDimmableLight.onLightChange(setLight);
+
+  // Optional: Set callback function for device identify
+  zbDimmableLight.onIdentify(identify);
+
+  // Optional: Set Zigbee device name and model
+  zbDimmableLight.setManufacturerAndModel("Espressif", "ZBLightBulb");
+
+  // Add endpoint to Zigbee Core
+  log_d("Adding ZigbeeLight endpoint to Zigbee Core");
+  Zigbee.addEndpoint(&zbDimmableLight);
+
+  // When all EPs are registered, start Zigbee. By default acts as ZIGBEE_END_DEVICE
+  log_d("Calling Zigbee.begin()");
+  Zigbee.begin();
+}
+
+void loop()
+{
+  // Checking button for factory reset
+  if (digitalRead(BUTTON_PIN) == LOW)
+  { // Push button pressed
+    // Key debounce handling
+    delay(100);
+    int startTime = millis();
+    while (digitalRead(BUTTON_PIN) == LOW)
+    {
+      delay(50);
+      if ((millis() - startTime) > 3000)
+      {
+        // If key pressed for more than 3secs, factory reset Zigbee and reboot
+        Serial.printf("Resetting Zigbee to factory settings, reboot.\n");
+        Zigbee.factoryReset();
+      }
+    }
+  }
+  delay(100);
+}
