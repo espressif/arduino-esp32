2803eac1f3e5bbabf8b7dac301be293211ba7769
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-03-04 18:20:45 +0100

    c5 flash base address is 0x2000

--- cores/esp32/Esp.cpp
+++ cores/esp32/Esp.cpp
@@ -66,7 +66,7 @@ extern "C" {
 #define ESP_FLASH_IMAGE_BASE 0x2000  // Esp32p4 is located at 0x2000
 #elif CONFIG_IDF_TARGET_ESP32C5
 #include "esp32c5/rom/spi_flash.h"
-#define ESP_FLASH_IMAGE_BASE 0x0000  // Esp32c5 is located at 0x0000
+#define ESP_FLASH_IMAGE_BASE 0x2000  // Esp32c5 is located at 0x2000
 #else
 #error Target CONFIG_IDF_TARGET is not supported
 #endif
