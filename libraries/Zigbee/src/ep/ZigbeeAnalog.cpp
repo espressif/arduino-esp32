2276f0b79401ffd63f7719975461dd0c5f68155a
Jan Procházka <90197375+P-R-O-C-H-Y@users.noreply.github.com>
2025-03-10 20:21:00 +0100

    fix(zigbee): Add manuf_code to report attribure commands (#11066)

--- libraries/Zigbee/src/ep/ZigbeeAnalog.cpp
+++ libraries/Zigbee/src/ep/ZigbeeAnalog.cpp
@@ -93,6 +93,7 @@ void ZigbeeAnalog::reportAnalogInput() {
   report_attr_cmd.direction = ESP_ZB_ZCL_CMD_DIRECTION_TO_CLI;
   report_attr_cmd.clusterID = ESP_ZB_ZCL_CLUSTER_ID_ANALOG_INPUT;
   report_attr_cmd.zcl_basic_cmd.src_endpoint = _endpoint;
+  report_attr_cmd.manuf_code = ESP_ZB_ZCL_ATTR_NON_MANUFACTURER_SPECIFIC;
 
   esp_zb_lock_acquire(portMAX_DELAY);
   esp_zb_zcl_report_attr_cmd_req(&report_attr_cmd);
