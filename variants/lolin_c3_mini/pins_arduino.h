1ca9dab521fd4252e2c33799e79f1929e93bbd02
Sugar Glider <rodrigo.garcia@espressif.com>
2025-03-27 07:32:36 -0300

    fix(arduino_pins): fixes Lolin-C3-Pico and C3-Mini RGB LED pin + C3-Pico SPI SCK pin definition (#11186)
    * fix(arduino_pins): fixes RGB LED pin and SPI SCK pin definition
    
    * fix(arduino_pins): lolin-c3-mini rgb led pin

--- variants/lolin_c3_mini/pins_arduino.h
+++ variants/lolin_c3_mini/pins_arduino.h
@@ -2,10 +2,18 @@
 #define Pins_Arduino_h
 
 #include <stdint.h>
+#include "soc/soc_caps.h"
 
-static const uint8_t LED_BUILTIN = 7;
+// based on https://www.wemos.cc/en/latest/c3/c3_mini.html
+// WS2812 RGB LED on pin 7
+#define PIN_RGB_LED 7
+// BUILTIN_LED can be used in new Arduino API digitalWrite() like in Blink.ino
+static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT + PIN_RGB_LED;
 #define BUILTIN_LED LED_BUILTIN  // backward compatibility
 #define LED_BUILTIN LED_BUILTIN  // allow testing #ifdef LED_BUILTIN
+// RGB_BUILTIN and RGB_BRIGHTNESS can be used in new Arduino API rgbLedWrite()
+#define RGB_BUILTIN    LED_BUILTIN
+#define RGB_BRIGHTNESS 64
 
 static const uint8_t TX = 21;
 static const uint8_t RX = 20;
