720747f001fcec5f3f6c80abe44bec0f05140398
Sugar Glider <rodrigo.garcia@espressif.com>
2025-04-02 13:11:59 -0300

    fix(uart): missing class qualifier declaration

--- cores/esp32/HardwareSerial.cpp
+++ cores/esp32/HardwareSerial.cpp
@@ -617,7 +617,7 @@ bool HardwareSerial::setMode(SerialMode mode) {
 // UART_CLK_SRC_REF_TICK     :: ESP32 and ESP32-S2
 // Note: CLK_SRC_PLL Freq depends on the SoC - ESP32-C2 has 40MHz, ESP32-H2 has 48MHz and ESP32-C5, C6, C61 and P4 has 80MHz
 // Note: ESP32-C6, C61, ESP32-P4 and ESP32-C5 have LP UART that will use only RTC_FAST or XTAL/2 as Clock Source
-bool setClockSource(SerialClkSrc clkSrc) {
+bool HardwareSerial::setClockSource(SerialClkSrc clkSrc) {
   return uartSetClockSource(_uart, (uart_sclk_t) clkSrc);
 }
 
