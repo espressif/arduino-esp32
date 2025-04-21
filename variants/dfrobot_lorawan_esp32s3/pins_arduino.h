feeb0b0828dc6fa932ad01dc405c6ec7414f8bec
ouki.wang <ouki.wang@dfrobot.com>
2025-04-21 14:47:24 +0800

    add dfrobot_lorawan_esp32s3 board

---
+++ variants/dfrobot_lorawan_esp32s3/pins_arduino.h
@@ -0,0 +1,55 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+// DFROBOT  DFR1195
+#define EXTERNAL_NUM_INTERRUPTS 16
+#define NUM_DIGITAL_PINS 40
+#define NUM_ANALOG_INPUTS 16
+
+#define analogInputToDigitalPin(p) (((p) < 20) ? (esp32_adc2gpio[(p)]) : -1)
+#define digitalPinToInterrupt(p) (((p) < 40) ? (p) : -1)
+#define digitalPinHasPWM(p) (p < 34)
+
+#define LED_GREEN -1
+#define LED_BLUE 2
+
+#define LED_BUILTIN LED_GREEN
+
+static const uint8_t TX = 43;
+static const uint8_t RX = 44;
+
+static const uint8_t SDA = 8;
+static const uint8_t SCL = 9;
+
+#define WB_IO1 1
+#define WB_IO2 1
+#define WB_SW1 34
+#define WB_A0 36 
+#define WB_LED1 -1
+#define WB_LED2 2
+
+#define LORA_ANTPWR  42     //RXEN
+#define LORA_RST     41     //RST
+#define LORA_BUSY    40     //BUSY
+#define LORA_DIO1    4      //DIO
+
+
+static const uint8_t LORA_SS    = 10;
+static const uint8_t LORA_MOSI  = 6;
+static const uint8_t LORA_MISO  = 5;
+static const uint8_t LORA_SCK   = 7;
+
+static const uint8_t SS         = 17;
+static const uint8_t MOSI       = 11;
+static const uint8_t MISO       = 13;
+static const uint8_t SCK        = 12;
+
+// 屏幕
+#define TFT_DC 14
+#define TFT_CS 17
+#define TFT_RST 15
+#define TFT_BL 16       // Backlight pin
+
+#endif /* Pins_Arduino_h */
