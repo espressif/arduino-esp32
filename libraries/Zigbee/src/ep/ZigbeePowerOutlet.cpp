8f71c44ef48a0fa27ec19702858cac62fd9a4f28
Ludovic BOUÉ <lboue@users.noreply.github.com>
2025-04-25 08:27:14 +0200

    feat(esp32): Zigbee power outlet example
    Remove old comment

--- libraries/Zigbee/src/ep/ZigbeePowerOutlet.cpp
+++ libraries/Zigbee/src/ep/ZigbeePowerOutlet.cpp
@@ -5,7 +5,7 @@ ZigbeePowerOutlet::ZigbeePowerOutlet(uint8_t endpoint) : ZigbeeEP(endpoint) {
   _device_id = ESP_ZB_HA_MAINS_POWER_OUTLET_DEVICE_ID;
 
   esp_zb_mains_power_outlet_cfg_t outlet_cfg = ESP_ZB_DEFAULT_ON_OFF_SWITCH_CONFIG();
-  _cluster_list = esp_zb_mains_power_outlet_clusters_create(&outlet_cfg);  // use esp_zb_zcl_cluster_list_create() instead of esp_zb_mains_power_outlet_clusters_create()
+  _cluster_list = esp_zb_mains_power_outlet_clusters_create(&outlet_cfg);
   _ep_config = {.endpoint = endpoint, .app_profile_id = ESP_ZB_AF_HA_PROFILE_ID, .app_device_id = ESP_ZB_HA_MAINS_POWER_OUTLET_DEVICE_ID, .app_device_version = 0};
   log_v("Outlet endpoint created %d", _endpoint);
 }
