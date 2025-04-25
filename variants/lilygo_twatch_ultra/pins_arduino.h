4047829d1e4e3200522f9d22d656f23867a06a0c
lewisxhe <lewishe@outlook.com>
2025-04-25 15:20:03 +0800

    Update T-Watch-S3-Ultra variants

--- variants/lilygo_twatch_ultra/pins_arduino.h
+++ variants/lilygo_twatch_ultra/pins_arduino.h
@@ -77,7 +77,8 @@ static const uint8_t SCK = 35;
 // External expansion chip IO definition
 #define EXPANDS_DRV_EN    (6)
 #define EXPANDS_DISP_EN   (7)
-#define EXPANDS_SD_DET  (10)
+#define EXPANDS_TOUCH_RST (8)
+#define EXPANDS_SD_DET   (10)
 
 // Peripheral definition exists
 #define USING_XL9555_EXPANDS
