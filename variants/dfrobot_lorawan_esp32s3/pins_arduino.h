a11aad2e89ba1144a45e78596072be0a67246063
qhddz <ouki.wang@dfrobot.com>
2025-05-06 09:26:23 +0800

    Update pins_arduino.h

--- variants/dfrobot_lorawan_esp32s3/pins_arduino.h
+++ variants/dfrobot_lorawan_esp32s3/pins_arduino.h
@@ -11,13 +11,6 @@ static const uint8_t RX = 44;
 static const uint8_t SDA = 8;
 static const uint8_t SCL = 9;
 
-#define WB_IO1 1
-#define WB_IO2 1
-#define WB_SW1 34
-#define WB_A0 36 
-#define WB_LED1 -1
-#define WB_LED2 2
-
 #define LORA_ANTPWR  42     //RXEN
 #define LORA_RST     41     //RST
 #define LORA_BUSY    40     //BUSY
