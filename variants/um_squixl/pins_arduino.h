2be618a589b94d07242337bca3f4e82724b9bb65
Seon Rozenblum <seonr@3sprockets.com>
2025-05-11 17:54:46 +1000

    Added new Unexpected Maker SQUiXL and EDGES3[D] boards.
    Signed-off-by: Seon Rozenblum <seonr@3sprockets.com>

---
+++ variants/um_squixl/pins_arduino.h
@@ -0,0 +1,16 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+#include "soc/soc_caps.h"
+
+#define USB_VID          0x303A
+#define USB_PID          0x82DF
+#define USB_MANUFACTURER "Unexpected Maker"
+#define USB_PRODUCT      "SQUiXL"
+#define USB_SERIAL       ""
+
+static const uint8_t SDA = 1;
+static const uint8_t SCL = 2;
+
+#endif /* Pins_Arduino_h */
