7f95ebf0ec7ab6f36b25c267d03a098a775ea1bd
qhddz <ouki.wang@dfrobot.com>
2025-05-16 13:32:12 +0800

    Update pins_arduino.h

--- variants/dfrobot_lorawan_esp32s3/pins_arduino.h
+++ variants/dfrobot_lorawan_esp32s3/pins_arduino.h
@@ -11,26 +11,24 @@ static const uint8_t RX = 44;
 static const uint8_t SDA = 8;
 static const uint8_t SCL = 9;
 
-#define LORA_ANTPWR  42     //RXEN
-#define LORA_RST     41     //RST
-#define LORA_BUSY    40     //BUSY
-#define LORA_DIO1    4      //DIO
+#define LORA_ANTPWR 42  //RXEN
+#define LORA_RST    41  //RST
+#define LORA_BUSY   40  //BUSY
+#define LORA_DIO1   4   //DIO
 
+static const uint8_t LORA_SS = 10;
+static const uint8_t LORA_MOSI = 6;
+static const uint8_t LORA_MISO = 5;
+static const uint8_t LORA_SCK = 7;
 
-static const uint8_t LORA_SS    = 10;
-static const uint8_t LORA_MOSI  = 6;
-static const uint8_t LORA_MISO  = 5;
-static const uint8_t LORA_SCK   = 7;
+static const uint8_t SS = 17;
+static const uint8_t MOSI = 11;
+static const uint8_t MISO = 13;
+static const uint8_t SCK = 12;
 
-static const uint8_t SS         = 17;
-static const uint8_t MOSI       = 11;
-static const uint8_t MISO       = 13;
-static const uint8_t SCK        = 12;
-
-
-#define TFT_DC 14
-#define TFT_CS 17
-#define TFT_RST 15
-#define TFT_BL 16       // Backlight pin
+#define TFT_DC  14
+#define TFT_CS  17
+#define TFT_RST  15
+#define TFT_BL  16  // Backlight pin
 
 #endif /* Pins_Arduino_h */
