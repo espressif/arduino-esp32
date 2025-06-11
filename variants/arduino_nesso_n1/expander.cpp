0756741a93a9f5a8816d5d070a4b898d56c0fd71
Martino Facchin <m.facchin@arduino.cc>
2025-06-11 18:48:56 +0200

    feat: add Arduino Nesso N1
    Also implement an oveload for digital* operation to abstract
    the various gpio expanders.
    
    TODO: change WireInternal into default Wire (will require the
    user to explicitly include "Wire.h" (as in
    https://github.com/arduino/ArduinoCore-mbed/blob/main/variants/NANO_RP2040_CONNECT/nina_pins.h#L14-L18
    )

---
+++ variants/arduino_nesso_n1/expander.cpp
@@ -0,0 +1,90 @@
+#define TwoWire TwoWireInternal
+#define Wire WireInternal
+#define Wire1 WireInternal1
+
+#include "pins_arduino.h"
+#include "../../libraries/Wire/src/Wire.h"
+#include "../../libraries/Wire/src/Wire.cpp"
+
+static bool wireInitialized = false;
+
+// From https://www.diodes.com/datasheet/download/PI4IOE5V6408.pdf
+static void writeRegister(uint8_t address, uint8_t reg, uint8_t value) {
+    WireInternal.beginTransmission(address);
+    WireInternal.write(reg);
+    WireInternal.write(value);
+    WireInternal.endTransmission();
+}
+
+static uint8_t readRegister(uint8_t address, uint8_t reg) {
+    WireInternal.beginTransmission(address);
+    WireInternal.write(reg);
+    WireInternal.endTransmission(false);
+    WireInternal.requestFrom(address, 1);
+    return WireInternal.read();
+}
+
+static void writeBitRegister(uint8_t address, uint8_t reg, uint8_t bit, uint8_t value) {
+    uint8_t val = readRegister(address, reg);
+    if (value) {
+        writeRegister(address, reg, val | (1 << bit));
+    } else {
+        writeRegister(address, reg, val & ~(1 << bit));
+    }
+}
+
+static bool readBitRegister(uint8_t address, uint8_t reg, uint8_t bit) {
+    uint8_t val = readRegister(address, reg);
+    return ((val & (1 << bit)) > 0);
+}
+
+void pinMode(ExpanderPin pin, uint8_t mode) {
+    if (!wireInitialized) {
+        WireInternal.begin(SDA, SCL);
+        wireInitialized = true;
+        // reset all registers to default state
+        writeRegister(pin.address, 0x1, 0x1);
+        // set all pins as high as default state
+        writeRegister(pin.address, 0x9, 0xFF);
+        // interrupt mask to all pins
+        writeRegister(pin.address, 0x11, 0xFF);
+        // all input
+        writeRegister(pin.address, 0x3, 0);
+    }
+    writeBitRegister(pin.address, 0x3, pin.pin, mode == OUTPUT);
+    if (mode == OUTPUT) {
+        // remove high impedence
+        writeBitRegister(pin.address, 0x7, pin.pin, false);
+    } else if (mode == INPUT_PULLUP) {
+        // set pull-up resistor
+        writeBitRegister(pin.address, 0xB, pin.pin, true);
+        writeBitRegister(pin.address, 0xD, pin.pin, true);
+    } else if (mode == INPUT_PULLDOWN) {
+        // disable pull-up resistor
+        writeBitRegister(pin.address, 0xB, pin.pin, true);
+        writeBitRegister(pin.address, 0xD, pin.pin, false);
+    } else if (mode == INPUT) {
+        // disable pull selector resistor
+        writeBitRegister(pin.address, 0xB, pin.pin, false);
+    }
+}
+
+void digitalWrite(ExpanderPin pin, uint8_t val) {
+    writeBitRegister(pin.address, 0x5, pin.pin, val == HIGH);
+}
+
+int digitalRead(ExpanderPin pin) {
+    return readBitRegister(pin.address, 0xF, pin.pin);
+}
+
+ExpanderPin LORA_LNA_ENABLE(5);
+ExpanderPin LORA_ANTENNA_SWITCH(6);
+ExpanderPin LORA_ENABLE(7);
+ExpanderPin KEY1(0);
+ExpanderPin KEY2(1);
+ExpanderPin POWEROFF((1 << 8) | 0);
+ExpanderPin LCD_RESET((1 << 8) | 1);
+ExpanderPin GROVE_POWER_EN((1 << 8) | 2);
+ExpanderPin VIN_DETECT((1 << 8) | 5);
+ExpanderPin LCD_BACKLIGHT((1 << 8) | 6);
+ExpanderPin LED_BUILTIN((1 << 8) | 7);
