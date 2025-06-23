f7004ef46f4c38180a3f1cfa8f444e2fca77f3a0
HighDoping <highdoping@gmail.com>
2025-06-23 22:53:30 +0800

    fix(example): add header guard for board_config.h

--- libraries/ESP32/examples/Camera/CameraWebServer/board_config.h
+++ libraries/ESP32/examples/Camera/CameraWebServer/board_config.h
@@ -1,8 +1,13 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+//
 // WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
 //            Ensure ESP32 Wrover Module or other board with PSRAM is selected
 //            Partial images will be transmitted if image exceeds buffer size
 //
 //            You must select partition scheme from the board menu that has at least 3MB APP space.
+
 // ===================
 // Select camera model
 // ===================
@@ -25,3 +30,5 @@
 //#define CAMERA_MODEL_DFRobot_FireBeetle2_ESP32S3 // Has PSRAM
 //#define CAMERA_MODEL_DFRobot_Romeo_ESP32S3 // Has PSRAM
 #include "camera_pins.h"
+
+#endif  // BOARD_CONFIG_H
