1426927c837dfa0c10835eb3c769f7000db64c26
Me No Dev <me-no-dev@users.noreply.github.com>
2025-07-04 18:37:56 +0300

    fix(matter): Fix MatterSmartButon.ino when CHIPOBLE is on

--- libraries/Matter/examples/MatterSmartButon/MatterSmartButon.ino
+++ libraries/Matter/examples/MatterSmartButon/MatterSmartButon.ino
@@ -45,12 +45,12 @@ void setup() {
 
   Serial.begin(115200);
 
+// CONFIG_ENABLE_CHIPOBLE is enabled when BLE is used to commission the Matter Network
+#if !CONFIG_ENABLE_CHIPOBLE
   // We start by connecting to a WiFi network
   Serial.print("Connecting to ");
   Serial.println(ssid);
 
-// CONFIG_ENABLE_CHIPOBLE is enabled when BLE is used to commission the Matter Network
-#if !CONFIG_ENABLE_CHIPOBLE
   // Manually connect to WiFi
   WiFi.begin(ssid, password);
   // Wait for connection
