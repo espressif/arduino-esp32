000336bad0446d3b8ded72b1d92e5edc407f8f69
me-no-dev <hristo@espressif.com>
2025-07-18 11:25:36 +0300

    fix(build): Enable I2C FIFO mode only on IDF 5.4.2+

--- cores/esp32/esp32-hal-i2c-slave.c
+++ cores/esp32/esp32-hal-i2c-slave.c
@@ -343,7 +343,11 @@ esp_err_t i2cSlaveInit(uint8_t num, int sda, int scl, uint16_t slaveID, uint32_t
   || (ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 3, 3) && ESP_IDF_VERSION < ESP_IDF_VERSION_VAL(5, 4, 0))
   i2c_ll_set_mode(i2c->dev, I2C_BUS_MODE_SLAVE);
   i2c_ll_enable_pins_open_drain(i2c->dev, true);
+#if ESP_IDF_VERSION >= ESP_IDF_VERSION_VAL(5, 4, 2)
   i2c_ll_enable_fifo_mode(i2c->dev, true);
+#else
+  i2c_ll_slave_set_fifo_mode(i2c->dev, true);
+#endif
 #else
   i2c_ll_slave_init(i2c->dev);
   i2c_ll_slave_set_fifo_mode(i2c->dev, true);
