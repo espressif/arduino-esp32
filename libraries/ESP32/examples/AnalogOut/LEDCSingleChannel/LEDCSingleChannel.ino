11dcd5c645a692c523fd7df77e8a4d9145f525f8
Sugar Glider <rodrigo.garcia@espressif.com>
2025-11-03 01:17:44 -0300

    feat(ledc): Change LED fade amount and brightness limits

--- libraries/ESP32/examples/AnalogOut/LEDCSingleChannel/LEDCSingleChannel.ino
+++ libraries/ESP32/examples/AnalogOut/LEDCSingleChannel/LEDCSingleChannel.ino
@@ -26,7 +26,7 @@
 #define LEDC_CHANNEL 0
 
 int brightness = 0;  // how bright the LED is
-int fadeAmount = 5;  // how many points to fade the LED by
+int fadeAmount = 4;  // how many points to fade the LED by
 
 void setup() {
   // Use single LEDC channel 0 for both pins
@@ -42,7 +42,7 @@ void loop() {
   brightness = brightness + fadeAmount;
 
   // reverse the direction of the fading at the ends of the fade:
-  if (brightness <= 0 || brightness >= 255) {
+  if (brightness <= 0 || brightness >= 256) {
     fadeAmount = -fadeAmount;
   }
   // wait for 30 milliseconds to see the dimming effect
