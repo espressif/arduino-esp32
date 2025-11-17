2a2179d69dfc3a1ec9cdf63c39eb215c411723c8
Jason2866 <24528715+Jason2866@users.noreply.github.com>
2025-11-17 12:22:26 +0100

    Fix chip_variant retrieval with default value

--- tools/pioarduino-build.py
+++ tools/pioarduino-build.py
@@ -32,7 +32,7 @@ env = DefaultEnvironment()
 platform = env.PioPlatform()
 board_config = env.BoardConfig()
 build_mcu = board_config.get("build.mcu", "").lower()
-chip_variant = board_config.get("build.chip_variant").lower()
+chip_variant = board_config.get("build.chip_variant", "").lower()
 chip_variant = chip_variant if chip_variant else build_mcu
 partitions_name = board_config.get("build.partitions", board_config.get("build.arduino.partitions", ""))
 
