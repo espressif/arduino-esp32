e540af21de9505f4bd1616a6b31f5a1cf634a94f
Sugar Glider <rodrigo.garcia@espressif.com>
2025-12-04 10:26:09 -0300

    feat(matter): adds rain sensor matter endpoint (#12100)
    * feat(matter): adds rain sensor matter endpoint
    
    * feat(matter): adds source code to CMakeLists.txt
    
    * ci(pre-commit): Apply automatic fixes
    
    ---------
    
    Co-authored-by: pre-commit-ci-lite[bot] <117423508+pre-commit-ci-lite[bot]@users.noreply.github.com>

--- libraries/Matter/src/Matter.h
+++ libraries/Matter/src/Matter.h
@@ -29,6 +29,7 @@
 #include <MatterEndpoints/MatterTemperatureSensor.h>
 #include <MatterEndpoints/MatterHumiditySensor.h>
 #include <MatterEndpoints/MatterContactSensor.h>
+#include <MatterEndpoints/MatterRainSensor.h>
 #include <MatterEndpoints/MatterPressureSensor.h>
 #include <MatterEndpoints/MatterOccupancySensor.h>
 #include <MatterEndpoints/MatterOnOffPlugin.h>
@@ -194,6 +195,7 @@ public:
   friend class MatterTemperatureSensor;
   friend class MatterHumiditySensor;
   friend class MatterContactSensor;
+  friend class MatterRainSensor;
   friend class MatterPressureSensor;
   friend class MatterOccupancySensor;
   friend class MatterOnOffPlugin;
