d6585873a190f2f6d13a35544b87c7424c7c4324
SuGlider <rodrigo.garcia@espressif.com>
2026-06-10 01:08:56 -0300

    fix(othread): fix copyright message

--- libraries/OpenThread/src/OThreadUDP.h
+++ libraries/OpenThread/src/OThreadUDP.h
@@ -1,4 +1,4 @@
-// Copyright 2024 Espressif Systems (Shanghai) PTE LTD
+// Copyright 2026 Espressif Systems (Shanghai) PTE LTD
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
