eb1b8b149b4122500a40b2730dd4340fdee283d5
Lucas Saavedra Vaz <32426024+lucasssvaz@users.noreply.github.com>
2026-09-25 07:30:03 -0300

    boards(waveshare): Change C5 Zero LED color order to GRB (#12941)

--- variants/waveshare_esp32_c5_zero/pins_arduino.h
+++ variants/waveshare_esp32_c5_zero/pins_arduino.h
@@ -22,7 +22,7 @@ static const uint8_t LED_BUILTIN = SOC_GPIO_PIN_COUNT + PIN_RGB_LED;
 // RGB_BUILTIN and RGB_BRIGHTNESS can be used in new Arduino API rgbLedWrite()
 #define RGB_BUILTIN                 LED_BUILTIN
 #define RGB_BRIGHTNESS              64
-#define RGB_BUILTIN_LED_COLOR_ORDER LED_COLOR_ORDER_RGB
+#define RGB_BUILTIN_LED_COLOR_ORDER LED_COLOR_ORDER_GRB
 
 static const uint8_t TX = 11;
 static const uint8_t RX = 12;
